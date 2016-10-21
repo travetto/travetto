334a235da5518dd269592d2149d3ddd9d2439cae
Timothy Soehnlin <tim@eaiti.com>
2016-10-21 09:46:18 -0500

    Initial commit

---
+++ module/asset-express/src/lib/decorate/upload.ts
@@ -0,0 +1,30 @@
+import {Request,Response} from 'express';
+import {AssetService} from '../service';
+import {filterAdder} from '../../express';
+import {nodeToPromise} from '../../util';
+import {File} from '../model';
+
+let multipart = require('connect-multiparty')({
+    hash : 'sha256'
+});
+
+function doUpload(after?:(req:Request)=>Promise<any>) {
+  return (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => {
+    let clz = target.constructor;    
+    filterAdder(async (req:Request, res:Response) => {
+      await nodeToPromise<void>(null, multipart, req, res);
+            
+      Object.keys(req.files).forEach(f =>		
+        req.files[f] = AssetService.fromUpload(req.files[f] as any as Express.MultipartyUpload, (clz as any).basePath +'/'));
+      
+      if (after) {
+        await after(req);
+      }
+        
+    })(target, propertyKey, descriptor);
+  };
+} 
+
+export function Upload() {
+  return doUpload();
+}
---
+++ module/asset-express/src/lib/model/file.ts
@@ -0,0 +1,55 @@
+import {Request,Response} from "express";
+import {Renderable} from '../../express';
+import {nodeToPromise} from '../../util';
+import * as fs from "fs";
+import * as mime from "mime"; 
+
+export class File extends Renderable {
+	
+	static fields = ['filename', 'length','contentType','path','metadata','stream'];
+	
+	_id:string;
+	stream:NodeJS.ReadableStream;
+	length:number;
+	filename:string;
+	contentType:string;
+	path:string;
+	metadata : {
+		name : string,
+		title : string,
+		hash : string,
+		createdDate : Date,
+		tags?: string[]
+	}
+	
+	constructor(conf:any = null) {
+		super();
+		if (conf) {
+			File.fields.forEach(k => {
+				if (conf[k]) (this as any)[k] = conf[k]
+			})
+		}
+	}
+		
+	render(res:Response) {
+    res.setHeader('Content-Type', this.contentType);
+    res.setHeader('Content-Disposition', `attachment; filename="${this.filename}"`)
+		if (this.length) {
+			res.setHeader('Content-Length', `${this.length}`);
+		}
+		if (this.stream) {
+			this.stream.pipe(res);
+		}
+		
+		return new Promise<any>((resolve, reject) => {
+			this.stream.on('end', resolve);
+			this.stream.on('error', reject);
+		});
+	}
+	
+	async read() {
+		let res = (await nodeToPromise<Buffer>(fs, fs.readFile, this.path)).toString();
+		fs.unlink(this.path);
+		return res;
+	}
+}
