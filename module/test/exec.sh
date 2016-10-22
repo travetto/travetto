7678c6b80c2840b1ebe4037b572665b325604dbd
Timothy Soehnlin <tim@eaiti.com>
2016-10-22 00:32:30 -0500

    Mock transport

--- module/test/exec.sh
+++ module/test/exec.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-function test() {
-  mocha --require node_modules/@encore/base/src/lib/require-ts.js --require src/test/  $@ 
-}
+CMD="mocha --require node_modules/@encore/base/src/lib/require-ts.js"
+
+if [[ -e "./src/test/index.js" ]]; then
+  CMD="$CMD --require src/test"
+fi
 
 if [[ "$1" == "all" ]]; then
-  test src/test/**/*.ts
+  $CMD src/test/**/*.ts
 elif [[ "$1" == "bamboo" ]]; then
-  test -r json src/test/**/*.ts > mocha.json
+  $CMD -r json src/test/**/*.ts > mocha.json
 else
-  test $@
+  $CMD $@
 fi
