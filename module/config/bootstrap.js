2123071e2403c214ad751e342932b573a39c472c
Timothy Soehnlin <tim@eaiti.com>
2016-10-22 10:40:55 -0500

    Auto bootstrapper

--- module/config/bootstrap.js
+++ module/config/bootstrap.js
@@ -1,12 +1,1 @@
-require('@encore/base/src/lib/require-ts');
-
-function init(env = null, imports = null) {
-  env = env || process.env.env || 'local';
-  require('./src/lib').configure(env);
-  if (imports) {
-    require('@encore/base').bulkRequire(imports);
-  }
-}
-const registerNamespace = require('./src/lib').registerNamespace;
-
-module.exports = { init, registerNamespace }
+require('./bootstrap').init(env || process.env.env);
