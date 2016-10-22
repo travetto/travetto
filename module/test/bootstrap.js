8944e1609fc61535854cdaf2c262aecaaaa36435
Timothy Soehnlin <tim@eaiti.com>
2016-10-21 22:16:15 -0500

    Working on testing

--- module/test/bootstrap.js
+++ module/test/bootstrap.js
@@ -1,5 +1,5 @@
-function init(name, handler) {
+function init(name, handlerFn) {
   require('@encore/base/src/lib/require-ts');
-  require('./src/lib/mocha').registerTest(name, handler);
+  require('./src/lib/mocha').registerTest(name, handlerFn());
 }
 module.exports = { init };
