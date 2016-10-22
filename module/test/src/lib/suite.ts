9185894e8a99f56ad2e6c5af4bab8835e8025edd
Timothy Soehnlin <tim@eaiti.com>
2016-10-22 00:01:41 -0500

    Moving to mocha 3

--- module/test/src/lib/suite.ts
+++ module/test/src/lib/suite.ts
@@ -5,27 +5,6 @@ let _beforeSuite: ActionFunction[] = [];
 let _afterTest: ActionFunction[] = [];
 let _afterSuite: ActionFunction[] = [];
 
-const DUMMY_ASYNC = async () => { }
-const DUMMY = process.nextTick
-
-function runOnce(fn: Function): any {
-  let cb = fn.toString().indexOf('(done)') >= 0;
-  let gen = fn.toString().indexOf('yield ') >= 0;
-  let op = fn;
-
-  if (gen) {
-    return (done: Function) => {
-      op()
-        .then(() => op = DUMMY_ASYNC, () => op = DUMMY_ASYNC)
-        .then(done, done)
-    };
-  } else if (cb) {
-    return (done: Function) => { op(done); op = DUMMY; }
-  } else {
-    return (done: Function) => { op(); op = DUMMY; done() }
-  }
-}
-
 export function suite(fn: Function) {
   for (let fn of _beforeSuite) before(fn);
   for (let fn of _beforeTest) beforeEach(fn);
@@ -35,7 +14,6 @@ export function suite(fn: Function) {
 }
 
 export const adder = <T>(arr: T[]) => (t: T) => arr.push(t);
-export const beforeAll = (fn: ActionFunction) => _beforeSuite.push(runOnce(fn));
 export const beforeSuite = adder(_beforeSuite);
 export const beforeTest = adder(_beforeTest);
 export const afterSuite = adder(_afterSuite);
