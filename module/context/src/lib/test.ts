e93df555755c0b203d4bd1be32a9017ef0a3b3b8
Timothy Soehnlin <tim@eaiti.com>
2016-10-23 18:32:30 -0500

    Adding context support for testing

---
+++ module/context/src/lib/test.ts
@@ -0,0 +1,18 @@
+import { Context } from './context';
+import { beforeTest, afterTest } from '@encore/test';
+
+let store: { [key: string]: any } = {};
+
+Context.storage = {
+  get: (key: string) => store[key],
+  set: (key: string, val: any) => store[key] = val
+}
+
+let ctx = {};
+
+beforeTest(() => Context.set(Object.assign({}, ctx)));
+afterTest(() => Context.clear());
+
+export function setDefault(obj: any) {
+  Object.assign(ctx, obj);
+}
