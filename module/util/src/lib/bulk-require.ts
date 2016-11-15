b1a054bca62d74a13096eb455e3902ff66ab8bfa
Timothy Soehnlin <tim@eaiti.com>
2016-11-14 23:24:28 -0500

    Making bulk require returning

--- module/util/src/lib/bulk-require.ts
+++ module/util/src/lib/bulk-require.ts
@@ -4,8 +4,8 @@ let glob = require('glob');
 export default function (pattern: string, base: string | null = null) {
   let search = `${base || process.cwd()}/${pattern}`;
   //console.log("Bulk Require", search);
-  glob.sync(search)
+  return glob.sync(search)
     .map((f: string) => path.resolve(f))
     //.map((x: string) => { console.log(x); return x; })
-    .forEach(require)
+    .map(require)
 }
