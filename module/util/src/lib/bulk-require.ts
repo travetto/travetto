ceb8aa4329f47b476cdec94e1f2e877c098e52a2
Timothy Soehnlin <tim@eaiti.com>
2016-11-14 23:27:20 -0500

    Cleaning up

--- module/util/src/lib/bulk-require.ts
+++ module/util/src/lib/bulk-require.ts
@@ -4,7 +4,7 @@ let glob = require('glob');
 export default function (pattern: string, base: string | null = null) {
   let search = `${base || process.cwd()}/${pattern}`;
   //console.log("Bulk Require", search);
-  return glob.sync(search)
+  return (glob.sync(search) as string[])
     .map((f: string) => path.resolve(f))
     //.map((x: string) => { console.log(x); return x; })
     .map(require);
