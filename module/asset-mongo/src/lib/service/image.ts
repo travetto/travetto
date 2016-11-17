58d9c4f8df59d7143125bf18f1e37a8cde2a175a
Timothy Soehnlin <tim@eaiti.com>
2016-11-17 15:39:12 -0600

    Adding linting in

--- module/asset-mongo/src/lib/service/image.ts
+++ module/asset-mongo/src/lib/service/image.ts
@@ -1,62 +1,62 @@
-import * as path from "path";
-import * as fs from "fs";
-import * as LRU from "lru-cache";
+import * as path from 'path';
+import * as fs from 'fs';
+import * as LRU from 'lru-cache';
 import * as gm from 'gm';
 import { AssetService } from './asset';
-import { File } from '../model'
+import { File } from '../model';
 import { nodeToPromise } from '@encore/util';
 let osTmpdir = require('os-tmpdir');
 
 let tmpDir = path.resolve(osTmpdir());
 
 export class ImageService {
-	private static imageCache = LRU<string>({
-		max: 1000,
-		dispose: (key: string, n: string) => fs.unlink(n)
-	});
+  private static imageCache = LRU<string>({
+    max: 1000,
+    dispose: (key: string, n: string) => fs.unlink(n)
+  });
 
-	static clear() {
-		ImageService.imageCache.reset();
-	}
+  static clear() {
+    ImageService.imageCache.reset();
+  }
 
-	static generateTempFile(ext: string): string {
-		let now = new Date();
-		let name = `image-${now.getFullYear()}-${now.getMonth()}-${now.getDate()}-${process.pid}-${(Math.random() * 100000000 + 1).toString(36)}.${ext}`;
-		return path.join(tmpDir, name);
-	}
+  static generateTempFile(ext: string): string {
+    let now = new Date();
+    let name = `image-${now.getFullYear()}-${now.getMonth()}-${now.getDate()}-${process.pid}-${(Math.random() * 100000000 + 1).toString(36)}.${ext}`;
+    return path.join(tmpDir, name);
+  }
 
-	static async getImage(filename: string, options: { w: number, h: number }, filter?: any): Promise<File> {
-		let info = await AssetService.get(filename, filter);
-		if (options && (options.w || options.h)) {
-			let key = `${filename}${JSON.stringify(options)}`
-			let res = ImageService.imageCache.get(key);
+  static async getImage(filename: string, options: { w: number, h: number }, filter?: any): Promise<File> {
+    let info = await AssetService.get(filename, filter);
+    if (options && (options.w || options.h)) {
+      let key = `${filename}${JSON.stringify(options)}`;
+      let res = ImageService.imageCache.get(key);
 
-			if (!res) {
-				let filePath = ImageService.generateTempFile(info.filename.split('.').pop() as string);
-				let op = gm(info.stream, info.filename)
-					.resize(options.w, options.h)
-					.autoOrient()
-				let res = await nodeToPromise<void>(op, op.write, filePath)
-				ImageService.imageCache.set(key, filePath);
-			}
+      if (!res) {
+        let filePath = ImageService.generateTempFile(info.filename.split('.').pop() as string);
+        let op = gm(info.stream, info.filename)
+          .resize(options.w, options.h)
+          .autoOrient();
+        await nodeToPromise<void>(op, op.write, filePath);
+        ImageService.imageCache.set(key, filePath);
+      }
 
-			info.stream = fs.createReadStream(ImageService.imageCache.get(key));
-			delete info.length
-		}
-		return info;
-	}
+      info.stream = fs.createReadStream(ImageService.imageCache.get(key));
+      delete info.length;
+    }
+    return info;
+  }
 
-	static onExit(exit: boolean, err: any) {
-		if (err) {
-			console.log(err.stack || err);
-		}
-		ImageService.clear();
-		if (exit) {
-			process.exit();
-		}
-	}
+  static onExit(exit: boolean, err: any) {
+    if (err) {
+      console.log(err.stack || err);
+    }
+    ImageService.clear();
+    if (exit) {
+      process.exit();
+    }
+  }
 }
 
-process.on("exit", ImageService.onExit.bind(null, false))
-process.on("SIGINT", ImageService.onExit.bind(null, true))
-process.on("uncaughtException", ImageService.onExit.bind(null, true))
+process.on('exit', ImageService.onExit.bind(null, false));
+process.on('SIGINT', ImageService.onExit.bind(null, true));
+process.on('uncaughtException', ImageService.onExit.bind(null, true));
