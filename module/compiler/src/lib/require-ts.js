cf14329ffe81bef9693d8cb0d2833455da77c281
Timothy Soehnlin <tim@eaiti.com>
2016-11-17 15:28:00 -0600

    Working on linting

--- module/compiler/src/lib/require-ts.js
+++ module/compiler/src/lib/require-ts.js
@@ -1,11 +1,11 @@
-let sourcemap = require('source-map-support')
+let sourcemap = require('source-map-support');
 let fs = require('fs');
 let path = require('path');
 let ts = require('typescript');
 let cwd = process.cwd();
 let tsOptions = getOptions(path.join(cwd, 'tsconfig.json'));
 
-let dataUriRe = /data:application\/json[^,]+base64,/
+let dataUriRe = /data:application\/json[^,]+base64,/;
 let sourceMaps = {};
 
 sourcemap.install({ retrieveSourceMap: (path) => sourceMaps[path] });
@@ -18,7 +18,7 @@ function getOptions(tsconfigFile) {
   return o;
 }
 
-//Wrap sourcemap tool
+// Wrap sourcemap tool
 let prep = Error.prepareStackTrace;
 Error.prepareStackTrace = function (a, stack) {
   let res = prep(a, stack);
@@ -29,10 +29,10 @@ Error.prepareStackTrace = function (a, stack) {
       l.indexOf('source-map-support.js') < 0 &&
       (l.indexOf('node_modules') > 0 ||
         (l.indexOf('(native)') < 0 && (l.indexOf(cwd) < 0 || l.indexOf('.js') < 0))))
-    .join('\n')
+    .join('\n');
 }
 
-//TODO: Remove once mongoose fixes it's crap
+// TODO: Remove once mongoose fixes it's crap
 let cap = Error.captureStackTrace;
 Error.captureStackTrace = function (a) {
   a.stack = new Error().stack;
@@ -44,7 +44,7 @@ require.extensions['.ts'] = function load(m, tsf) {
   let name = parts.pop();
   let folder = parts.pop();
   let content = ts.transpile(fs.readFileSync(tsf, 'utf-8'), tsOptions, `${folder}/${name}`);
-  let map = new Buffer(content.split(dataUriRe)[1], "base64").toString()
+  let map = new Buffer(content.split(dataUriRe)[1], 'base64').toString()
   sourceMaps[jsf] = { content, url: tsf, map };
   return m._compile(content, jsf);
 };
