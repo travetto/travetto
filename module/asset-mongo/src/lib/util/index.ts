d585a4318a067048b2768df953dbc6855f5a1949
Zach Roberts <zach@zachbookpro.eaiinternal.eaiti.com>
2016-12-07 10:33:10 -0500

    fixed up some spacing issues

--- module/asset-mongo/src/lib/util/index.ts
+++ module/asset-mongo/src/lib/util/index.ts
@@ -1,3 +1,6 @@
+let osTmpdir = require('os-tmpdir');
+let tmpDir = path.resolve(osTmpdir());
+  
   export function generateTempFile(ext: string): string {
     let now = new Date();
     let name = `image-${now.getFullYear()}-${now.getMonth()}-${now.getDate()}-${process.pid}-${(Math.random() * 100000000 + 1).toString(36)}.${ext}`;
