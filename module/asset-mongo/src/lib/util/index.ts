5cf65f925f11841bb490e31c2751bea6eac130f3
Zach Roberts <zach@zachbookpro.eaiinternal.eaiti.com>
2016-12-07 09:23:23 -0500

    some spring cleaning

---
+++ module/asset-mongo/src/lib/util/index.ts
@@ -0,0 +1,5 @@
+  export function generateTempFile(ext: string): string {
+    let now = new Date();
+    let name = `image-${now.getFullYear()}-${now.getMonth()}-${now.getDate()}-${process.pid}-${(Math.random() * 100000000 + 1).toString(36)}.${ext}`;
+    return path.join(tmpDir, name);
+  }
