994aa7d1d7b73bd0ad4bd1bb92da4c9bcda456d3
Zach Roberts <zach@zachbookpro.eaiinternal.eaiti.com>
2016-12-08 13:13:29 -0500

    added testing support, and moved some functions to util folder

---
+++ module/asset/src/test/service/asset.ts
@@ -0,0 +1,20 @@
+import { AssetService } from '../../lib/service/asset';
+import { AssetUtil } from '../../lib/util';
+import { expect } from 'chai';
+import * as fs from 'fs';
+
+describe('Assect Service', () => {
+  it('downloads an file from a url', async () => {
+    let filePath = await AssetUtil.downloadUrl('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')
+    expect(filePath).to.not.be.undefined;
+    expect(filePath.split('.').pop()).equals('png');
+    let file = await AssetService.uploadFromPath(filePath, undefined, undefined, true);
+    expect(file.contentType).equals('image/png');
+    expect(file.length).is.greaterThan(0);
+
+    fs.stat(filePath, (err, stats) => {
+      expect(err).to.be.instanceof(Error);
+      expect(stats).to.be.undefined;
+    })
+  });
+}) 
