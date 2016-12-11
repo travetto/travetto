dbb94a7765be7603c04ebad8fc00767202ed7740
Timothy Soehnlin <tim@eaiti.com>
2016-12-10 21:28:21 -0600

    Cleaning up structure for general usage

--- module/express/src/lib/decorate/filter.ts
+++ module/express/src/lib/decorate/filter.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { RouteService } from '../service';
+import { RouteRegistry } from '../service';
 
 
 export function RequiredParam(name: string) {
-  return RouteService.filterAdder(async (req: Request, res: Response) => {
+  return RouteRegistry.filterAdder(async (req: Request, res: Response) => {
     let param = req.query[name] || req.params[name] || (req.body || {})[name];
     let paramTypes = [
       'string',
@@ -13,6 +13,15 @@ export function RequiredParam(name: string) {
 
     if ((param !== null) && paramTypes.indexOf(typeof param) === -1) {
       throw { message: `Missing field: ${name}`, statusCode: 400 };
+    }
+  });
+}
+
+export function Accepts(contentTypes: string[]) {
+  return RouteRegistry.filterAdder(async (req: Request, res: Response) => {
+    let contentType = req.header('content-type');
+    if (contentTypes.indexOf(contentType) < 0) {
+      throw { message: `Content type ${contentType}`, statusCode: 400 };
     }
   });
 }
