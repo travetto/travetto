a681dad88ef00c787ce31dc665a0149f790a586f
Timothy Soehnlin <tim@eaiti.com>
2016-12-10 20:51:58 -0600

    Fixing logging

--- module/model/src/ext/express.ts
+++ module/model/src/ext/express.ts
@@ -8,13 +8,8 @@ import { filterAdder } from '@encore/express';
 export function ModelBody<T extends BaseModel>(cls: (new (a?: any) => T), view?: string) {
   return filterAdder(async (req: Request, res: Response) => {
     if (ObjectUtil.isPlainObject(req.body)) {
-      try {
-        let o = new cls(req.body);
-        req.body = await Validator.validate(o, view);
-      } catch (e) {
-        console.log(e);
-        throw e;
-      }
+      let o = new cls(req.body);
+      req.body = await Validator.validate(o, view);
     } else {
       throw { message: `Body is missing or wrong type: ${req.body}`, statusCode: 503 };
     }
