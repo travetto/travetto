1b6efe2952e4ce2d31b3f4336a15bdcfdef86bad
Timothy Soehnlin <tim@eaiti.com>
2016-12-10 21:36:41 -0600

    Cleaning up structure for general usage

--- module/model/src/ext/express.ts
+++ module/model/src/ext/express.ts
@@ -3,10 +3,10 @@ import { BaseModel } from '../lib/model';
 import { Validator } from '../lib/service';
 import { ObjectUtil } from '@encore/util';
 
-import { filterAdder } from '@encore/express';
+import { RouteRegistry } from '@encore/express';
 
 export function ModelBody<T extends BaseModel>(cls: (new (a?: any) => T), view?: string) {
-  return filterAdder(async (req: Request, res: Response) => {
+  return RouteRegistry.filterAdder(async (req: Request, res: Response) => {
     if (ObjectUtil.isPlainObject(req.body)) {
       let o = new cls(req.body);
       req.body = await Validator.validate(o, view);
