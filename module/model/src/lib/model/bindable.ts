ad0082a35bfdfad57747bb70608fabba0d1e4d4b
Timothy Soehnlin <tim@eaiti.com>
2016-12-11 22:52:06 -0600

    Allowing for recursive binding without hardcoding in models

---
+++ module/model/src/lib/model/bindable.ts
@@ -0,0 +1,8 @@
+import { bindData } from '../util';
+import { Cls } from '../service';
+
+export class Bindable {
+  constructor(data?: any) {
+    bindData(this.constructor as Cls, this, data);
+  }
+}
