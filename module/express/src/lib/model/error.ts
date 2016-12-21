132a27eb8dfabe2b35e8547bf79d6ce4c5387e90
Timothy Soehnlin <tim@eaiti.com>
2016-12-20 23:32:30 -0600

    Making app error render

--- module/express/src/lib/model/error.ts
+++ module/express/src/lib/model/error.ts
@@ -1,7 +1,14 @@
+import { Response } from 'express';
 import { ExtendableError } from '@encore/util';
+import { Renderable } from './renderable';
 
-export class AppError extends ExtendableError {
+export class AppError extends ExtendableError implements Renderable {
   constructor(message: string, public status: number = 500) {
     super(message);
+  }
+
+  render(res: Response) {
+    res.status(this.status);
+    res.json({ message: this.message, status: this.status });
   }
 }
