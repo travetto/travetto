b3d534c3bc5e7747ee3175447d4b87696d15e558
Timothy Soehnlin <tim@eaiti.com>
2016-12-21 18:29:26 -0600

    Working on object utils

--- module/util/src/lib/object.ts
+++ module/util/src/lib/object.ts
@@ -4,15 +4,17 @@ export class ObjectUtil {
   static merge = _.merge.bind(_) as typeof _.merge;
   static isPlainObject = _.isPlainObject.bind(_) as typeof _.isPlainObject;
 
-  static values(o: { [key: string]: any }): any[] {
+  static values<T>(o: { [key: string]: T }): T[] {
     return Object.keys(o).map(x => o[x]);
   }
+
   static fromPairs(pairs: [string, any][]): { [key: string]: any } {
     return pairs.reduce((acc: { [key: string]: any }, pair: [string, any]) => {
       acc[pair[0]] = pair[1];
       return acc;
     }, {});
   }
+
   static toPairs(o: { [key: string]: any }): [string, any][] {
     return Object.keys(o).reduce((acc: [string, any][], k: string) => {
       acc.push([k, o[k]]);
