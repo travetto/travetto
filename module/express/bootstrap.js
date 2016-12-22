b7cc66fd3ba7787c3d7de2eb8e9c7a601b371a6c
Timothy Soehnlin <tim@eaiti.com>
2016-12-22 09:40:41 -0600

    Fixing initialization

--- module/express/bootstrap.js
+++ module/express/bootstrap.js
@@ -4,7 +4,7 @@ let Configure = require('@encore/config').Configure;
 let Ready = require('@encore/lifecycle').Ready;
 let bulkRequire = require('@encore/util').bulkRequire;
 
-Configure.initialize(process.env.env || 'local');
+Configure.initialize(process.env.env);
 bulkRequire('src/app/route/**/*.ts');
 Ready.onReady(() => { console.log(JSON.stringify(Configure.data, null, 2)); });
 Ready.initialize();
