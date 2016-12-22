5cfe71e26c1208db45a5cf96852aff8d4339f579
Timothy Soehnlin <tim@eaiti.com>
2016-12-21 18:04:31 -0600

    Separting out mongo vs express pieces

--- module/asset-express/src/lib/decorate/upload.ts
+++ module/asset-express/src/lib/decorate/upload.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { AssetService } from '../service';
+import { AssetUtil } from '../util';
 import { RouteRegistry } from '@encore/express';
 import { nodeToPromise } from '@encore/util';
 
@@ -14,7 +14,7 @@ function doUpload(after?: (req: Request) => Promise<any>) {
       await nodeToPromise<void>(null, multipart, req, res);
 
       Object.keys(req.files).forEach(f =>
-        req.files[f] = AssetService.fromUpload(req.files[f] as any as Express.MultipartyUpload, (clz as any).basePath + '/'));
+        req.files[f] = AssetUtil.uploadToAsset(req.files[f] as any as Express.MultipartyUpload, (clz as any).basePath + '/'));
 
       if (after) {
         await after(req);
