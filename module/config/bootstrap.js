f82af639b921eddb052fcba446cd57492e697188
Timothy Soehnlin <tim@eaiti.com>
2016-12-21 20:31:02 -0600

    Moving configure to class

--- module/config/bootstrap.js
+++ module/config/bootstrap.js
@@ -6,7 +6,7 @@ function init(env, imports = null) {
     require('@encore/util').bulkRequire(imports);
   }
 }
-const registerNamespace = require('./src/lib').Configure.registerNamespace;
+const registerNamespace = require('./src/lib').registerNamespace;
 
 if (!!process.env.auto) {
   init(process.env.env);
--- module/config/src/lib/index.ts
+++ module/config/src/lib/index.ts
@@ -1,3 +1,5 @@
+import { Configure } from './configure';
 export * from './configure';
 export * from './ready';
 export * from './shutdown';
+export const registerNamespace = Configure.registerNamespace;
