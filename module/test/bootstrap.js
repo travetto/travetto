fdd0c0c709e09404cc9687f2a4749b2fb8eb8485
Timothy Soehnlin <tim@eaiti.com>
2016-12-23 15:35:52 -0600

    Moving to ts-node

--- module/test/bootstrap.js
+++ module/test/bootstrap.js
@@ -1,3 +1,3 @@
-require('@encore/bootstrap');
+require('ts-node/register');
 require('@encore/config').Configure.initialize(process.env.env || 'test');
 require('@encore/lifecycle').Ready.initialize();
