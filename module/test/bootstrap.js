19ee7fe39578aca30f0dbd06918524a161b01b25
Timothy Soehnlin <tim@eaiti.com>
2016-12-23 15:51:52 -0600

    Updating to custom registration

--- module/test/bootstrap.js
+++ module/test/bootstrap.js
@@ -1,3 +1,3 @@
-require('ts-node/register');
+require('@encore/base/typescript');
 require('@encore/config').Configure.initialize(process.env.env || 'test');
 require('@encore/lifecycle').Ready.initialize();
