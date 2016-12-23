a9b17af1817c4b0959416bed91d53567c2958a21
Timothy Soehnlin <tim@eaiti.com>
2016-12-23 16:49:42 -0600

    Upping base

--- module/express/bootstrap.js
+++ module/express/bootstrap.js
@@ -1,10 +1,9 @@
-require('@encore/base/require-ts');
-
-let {Configure} = require('@encore/config');
 let {Ready} = require('@encore/lifecycle');
-let {bulkRequire} = require('@encore/util');
 
-Configure.initialize(process.env.env || 'local');
-Ready.onReady(() => Configure.log());
-bulkRequire('src/app/route/**/*.ts');
-Ready.initialize();
+require('@encore/bootstrap').init({
+  defaultEnv: 'local',
+  scan: 'src/app/route/**/*.ts',
+  preInit: () => {
+    Ready.onReady(() => Configure.log());
+  }
+});
