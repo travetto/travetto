3dce8d36baca63586757d7d349fc797048c8af04
Timothy Soehnlin <tim@eaiti.com>
2016-12-23 16:09:26 -0600

    Fixing ts require

--- module/test/bootstrap.js
+++ module/test/bootstrap.js
@@ -1,3 +1,3 @@
-require('@encore/base/typescript');
+require('@encore/base/require-ts');
 require('@encore/config').Configure.initialize(process.env.env || 'test');
 require('@encore/lifecycle').Ready.initialize();
