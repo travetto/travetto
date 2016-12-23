d6f5aada4ede4efd15ee864382688a6bf20cbc77
Timothy Soehnlin <tim@eaiti.com>
2016-12-23 16:59:29 -0600

    Fixing bootstrap

--- module/express/bootstrap.js
+++ module/express/bootstrap.js
@@ -1,9 +1,8 @@
-let {Ready} = require('@encore/lifecycle');
 
 require('@encore/bootstrap').init({
   defaultEnv: 'local',
   scan: 'src/app/route/**/*.ts',
   preInit: () => {
-    Ready.onReady(() => Configure.log());
+    require('@encore/lifecycle').Ready.onReady(() => Configure.log());
   }
 });
