e793004d40116194546c0841833156b44022baff
Timothy Soehnlin <tim@eaiti.com>
2016-12-23 17:17:47 -0600

    Common patterns for bootstrap

--- module/express/bootstrap.js
+++ module/express/bootstrap.js
@@ -1,9 +1,5 @@
 require('@encore/bootstrap').init({
   defaultEnv: 'local',
-  scan: 'src/app/route/**/*.ts',
-  postConfig: () => {
-    let {Ready} = require('@encore/lifecycle');
-    let {Configure} = require('@encore/config');
-    Ready.onReady(() => Configure.log());
-  }
+  scanGlob: 'src/app/route/**/*.ts',
+  showConfig: true
 });
