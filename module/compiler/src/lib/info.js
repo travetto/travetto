bca6f363205e343c7774dddcfcb17176006b6ff3
Timothy Soehnlin <tim@eaiti.com>
2016-12-23 19:36:26 -0600

    Adding in option to get the info

---
+++ module/compiler/src/lib/info.js
@@ -0,0 +1,11 @@
+let pkg = JSON.parse(fs.readFileSync(process.cwd() + '/package.json').toString());
+let simpleName = (pkg.name as string).replace(/[@]/g, '').replace(/[\/]/g, '_');
+
+module.exports = {
+  version: pkg.version,
+  name: pkg.name,
+  simpleName,
+  license: pkg.license,
+  author: pkg.author,
+  description: pkg.author,
+}
