9067a82165057fc949fa184fc8e0f137a17c0705
Timothy Soehnlin <tim@eaiti.com>
2016-12-29 21:18:38 -0500

    Moving to Ready to Startup

--- module/asset-express/src/test/setup.ts
+++ module/asset-express/src/test/setup.ts
@@ -1,8 +1,8 @@
-import { Ready } from '@encore/lifecycle';
+import { Startup } from '@encore/lifecycle';
 import { beforeAll } from '@encore/test';
 import { MongoService } from '@encore/mongo';
 
 beforeAll(async () => {
-  await Ready.onReadyPromise();
+  await Startup.onStartupPromise();
   await MongoService.resetDatabase();
 });
