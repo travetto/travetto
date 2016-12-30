6026c7c274f29883461137d429e30ded4178993f
Timothy Soehnlin <tim@eaiti.com>
2016-12-30 00:02:43 -0500

    Fixing initialize

--- module/auth/src/lib/initialize.ts
+++ module/auth/src/lib/initialize.ts
@@ -1,17 +1,17 @@
 import * as passport from 'passport';
 import { Context } from '@encore/context';
-import { app } from '@encore/express';
+import { AppService } from '@encore/express';
 import { nodeToPromise } from '@encore/util';
 import { Request, Response } from 'express';
 
-app.use(passport.initialize(), passport.session());
+AppService.use(passport.initialize(), passport.session());
 
 async function logout(req: Request, res: Response) {
   await nodeToPromise(req.session, req.session.destroy);
   res.clearCookie('connect.sid', { path: '/' });
 }
 
-app.use((req: Request, res: Response, next?: Function) => {
+AppService.use((req: Request, res: Response, next?: Function) => {
   req.principal = req.user as any;
   Context.get().user = req.user;
   req.doLogout = () => logout(req, res);
