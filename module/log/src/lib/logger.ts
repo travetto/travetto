41b0b77b0af80c54d9ca2c4aa9d80a8e546abcb0
Timothy Soehnlin <tim@eaiti.com>
2016-12-29 23:13:44 -0500

    Working on logger

--- module/log/src/lib/logger.ts
+++ module/log/src/lib/logger.ts
@@ -1,34 +1,4 @@
 import * as log4js from 'log4js';
 
-import Config from './config';
-import { Layouts } from './layout';
-import { processAppenderConfig } from './appender';
-
-for (let key of Object.keys(Layouts)) {
-  log4js.layouts.addLayout(key, Layouts[key]);
-}
-log4js.clearAppenders();
-
-Object
-  .keys(Config)
-  .map(name => {
-    let conf = (Config as any)[name];
-    if (!conf.hasOwnProperty('enabled') || conf.enabled) {
-      return processAppenderConfig(conf);
-    }
-  })
-  .filter(x => x !== undefined)
-  .forEach(x => log4js.addAppender(x));
-
-
-if (Config.console) {
-  let override: boolean | null = Config.console.replaceConsole;
-
-  if (override === null ? process.env.env !== 'test' : !!override) {
-    log4js.replaceConsole();
-  }
-}
-
-
 export const logger = log4js.getLogger();
 export const Logger = logger;
