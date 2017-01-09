1b9439d866f107394b653aa7ae8419cea306ac57
Timothy Soehnlin <tim@eaiti.com>
2017-01-09 09:17:06 -0600

    Allowing for method name to count as differentiator

--- module/cache/src/lib/decorate/cache.ts
+++ module/cache/src/lib/decorate/cache.ts
@@ -3,9 +3,10 @@ import * as LRU from 'lru-cache';
 
 export function Cacheable(config: string | LRU.Options<any> & { name?: string }, keyFn?: (...args: any[]) => string) {
   return (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => {
+    let targetName = `${target.name}-${propertyKey}`;
     if (typeof config !== 'string') {
       if (!config['name']) {
-        config['name'] = `${target.name}-${propertyKey}`;
+        config['name'] = targetName;
       }
     }
     let cache = CacheService.getCache(config as (string | LRU.Options<any> & { name: string }));
@@ -13,6 +14,7 @@ export function Cacheable(config: string | LRU.Options<any> & { name?: string },
 
     target[propertyKey] = (...args: any[]) => {
       let key = keyFn ? keyFn(args) : JSON.stringify(args);
+      key = `${targetName}||${key}`;
       if (!cache.has(key)) {
         let res = orig.apply(target, args);
         cache.set(key, res);
