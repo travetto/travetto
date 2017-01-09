20095fd664bea90cecef9a6452b3fb97f9be97d1
Timothy Soehnlin <tim@eaiti.com>
2017-01-09 08:35:04 -0600

    Reworking caching

--- module/asset-mongo/src/lib/service/image.ts
+++ module/asset-mongo/src/lib/service/image.ts
@@ -1,42 +1,40 @@
 import * as fs from 'fs';
-import * as LRU from 'lru-cache';
 import * as gm from 'gm';
 
 import { AssetService } from './asset';
 import { Asset } from '../model';
 import { nodeToPromise } from '@encore/util';
 import { AssetUtil } from '../util';
-import { OnShutdown } from '@encore/lifecycle';
+import { Cacheable } from '@encore/cache';
 
 export class ImageService {
-  private static imageCache = LRU<string>({
+
+  @Cacheable({
     max: 1000,
     dispose: (key: string, n: string) => fs.unlink(n)
-  });
-
-  @OnShutdown()
-  static clear() {
-    ImageService.imageCache.reset();
+  })
+  static async generateAndStoreImage(filename: string, options: { w: number, h: number }, filter?: any): Promise<string> {
+    let info = await AssetService.get(filename, filter);
+    if (!info.stream) {
+      throw new Error('Stream not found');
+    }
+    if (options && (options.w || options.h)) {
+      let filePath = AssetUtil.generateTempFile(info.filename.split('.').pop() as string);
+      let op = gm(info.stream, info.filename)
+        .resize(options.w, options.h)
+        .autoOrient();
+      await nodeToPromise<void>(op, op.write, filePath);
+      return filePath;
+    } else {
+      return info.filename;
+    }
   }
 
   static async getImage(filename: string, options: { w: number, h: number }, filter?: any): Promise<Asset> {
+    let file = await ImageService.generateAndStoreImage(filename, options, filter);
     let info = await AssetService.get(filename, filter);
-    if (info.stream && options && (options.w || options.h)) {
-      let key = `${filename}${JSON.stringify(options)}`;
-      let res = ImageService.imageCache.get(key);
-
-      if (!res) {
-        let filePath = AssetUtil.generateTempFile(info.filename.split('.').pop() as string);
-        let op = gm(info.stream, info.filename)
-          .resize(options.w, options.h)
-          .autoOrient();
-        await nodeToPromise<void>(op, op.write, filePath);
-        ImageService.imageCache.set(key, filePath);
-      }
-
-      info.stream = fs.createReadStream(ImageService.imageCache.get(key));
-      delete info.length;
-    }
+    info.stream = fs.createReadStream(file);
+    delete info.length;
     return info;
   }
 }
