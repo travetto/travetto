9bb27c9cb33d19dd0b401797af871ec37b1bed94
Timothy Soehnlin <tim@eaiti.com>
2017-01-15 16:45:26 -0600

    Separating out schema from model

---
+++ module/schema/src/lib/util/bind.ts
@@ -0,0 +1,79 @@
+import { Cls, SchemaRegistry } from '../service';
+
+export class BindUtil {
+
+  static convert<T>(cls: Cls<T>, o: T): T {
+    let config = SchemaRegistry.schemas[cls.name];
+    if (config && config.subtypes && !!(o as any)._type) {
+      return new config.subtypes[(o as any)._type](o);
+    } else {
+      return new cls(o);
+    }
+  }
+
+  static coerceType<T>(type: Cls<T>, val: any): T {
+    if (val.constructor !== type) {
+      let atype = type as Cls<any>;
+      if (atype === Boolean) {
+        if (typeof val === 'string') {
+          val = val === 'true';
+        } else {
+          val = !!val;
+        }
+      } else if (atype === Number) {
+        val = parseInt(`${val}`, 10);
+      } else if (atype === String) {
+        val = `${val}`;
+      }
+    }
+    return val as T;
+  }
+
+  static bindSchema<T>(cons: Cls<any>, obj: T, data?: any, view: string = SchemaRegistry.DEFAULT_VIEW): T {
+    if (!!data) {
+      let conf = SchemaRegistry.schemas[cons.name];
+
+      // If no configuration
+      if (!conf) {
+        for (let k of Object.keys(data)) {
+          (obj as any)[k] = data[k];
+        }
+      } else {
+        let viewConf = conf && conf.views[view];
+        if (!viewConf) {
+          throw new Error(`View not found: ${view}`);
+        }
+        for (let f of viewConf.fields) {
+          if (data.hasOwnProperty(f)) {
+
+            let v = data[f];
+
+            if (v !== undefined && v !== null) {
+              let declared = viewConf.schema[f].declared;
+              // Ensure its an array
+              if (!Array.isArray(v) && declared.array) {
+                v = [v];
+              }
+
+              if (SchemaRegistry.schemas[declared.type.name]) {
+                if (declared.array) {
+                  v = v.map((x: any) => BindUtil.bindSchema(declared.type, new declared.type(), x, view));
+                } else {
+                  v = BindUtil.bindSchema(declared.type, new declared.type(), v, view);
+                }
+              } else {
+                v = declared.array ?
+                  v.map((e: any) => BindUtil.coerceType(declared.type, e)) :
+                  BindUtil.coerceType(declared.type, v);
+              }
+            }
+
+            (obj as any)[f] = v;
+          }
+        }
+      }
+    }
+
+    return obj;
+  }
+}
