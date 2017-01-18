6c1ba1023245b0a11d75953f5d30885ee585a850
Timothy Soehnlin <tim@eaiti.com>
2017-01-17 23:24:18 -0600

    Changing name

--- module/schema/src/lib/model/bound.ts
+++ module/schema/src/lib/model/bound.ts
@@ -1,7 +1,7 @@
 import { BindUtil } from '../util';
 
 export class SchemaBound {
-  static new<T>(this: new () => T, data: any): T {
+  static from<T>(this: new () => T, data: any): T {
     return BindUtil.bindSchema(this, new this(), data);
   }
 }
