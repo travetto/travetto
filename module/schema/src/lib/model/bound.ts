a35e2609df6e1825ce6d9be29643d2ad16bcb798
Timothy Soehnlin <tim@eaiti.com>
2017-01-18 17:44:13 -0600

    Allowing for views on from

--- module/schema/src/lib/model/bound.ts
+++ module/schema/src/lib/model/bound.ts
@@ -1,7 +1,8 @@
 import { BindUtil } from '../util';
+import { SchemaRegistry } from '../service';
 
 export class SchemaBound {
-  static from<T>(this: new () => T, data: any): T {
-    return BindUtil.bindSchema(this, new this(), data);
+  static from<T>(this: new () => T, data: any, view: string = SchemaRegistry.DEFAULT_VIEW): T {
+    return BindUtil.bindSchema(this, new this(), data, view);
   }
 }
