c16d60f975eb8a80da6a4e33f656d2a24a8dd148
Timothy Soehnlin <tim@eaiti.com>
2017-01-25 15:21:20 -0600

    Adding in upload support

--- module/asset/src/lib/decorate/upload.ts
+++ module/asset/src/lib/decorate/upload.ts
@@ -2,28 +2,51 @@ import { Request, Response } from 'express';
 import { AssetUtil } from '../util';
 import { RouteRegistry } from '@encore/express';
 import { nodeToPromise } from '@encore/util';
+import Config from '../config';
 
-let multipart = require('connect-multiparty')({
-  hash: 'sha256'
-});
+const match = require('mime-match');
 
-function doUpload(after?: (req: Request) => Promise<any>) {
+type UploadConfig = { allowedTypes: string[] | string, excludeTypes: string[] | string } & (typeof Config);
+
+function readTypeArr(arr: string[] | string) {
+  return Array.isArray(arr) ? arr : (arr || '').split(',');
+}
+
+function matchType(types: string[], type: string, invert: boolean = false) {
+  return types.length && (invert ? !types.filter(match(type)).length : types.filter(match(type)).length);
+}
+
+function doUpload(config: UploadConfig, after?: (req: Request) => Promise<any>) {
+  let multipart = require('connect-multiparty')({
+    hash: 'sha256',
+    maxFilesSize: Config.maxSize
+  });
+
+  let allowedTypes = readTypeArr(config.allowedTypes);
+  let excludeTypes = readTypeArr(config.excludeTypes);
+
   return (target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => {
     let clz = target.constructor;
     RouteRegistry.filterAdder(async (req: Request, res: Response) => {
       await nodeToPromise<void>(null, multipart, req, res);
 
-      Object.keys(req.files).forEach(f =>
-        req.files[f] = AssetUtil.uploadToAsset(req.files[f] as any as Express.MultipartyUpload, (clz as any).basePath + '/'));
+      for (let f of Object.keys(req.files)) {
+        let contentType = (await AssetUtil.detectFileType(req.files[f].path)).mime;
 
-      if (after) {
-        await after(req);
+        if (matchType(allowedTypes, contentType, true) || matchType(excludeTypes, contentType)) {
+          throw { message: `Content type not allowed: ${contentType}`, status: 403 };
+        }
+
+        req.files[f] = AssetUtil.uploadToAsset(req.files[f] as any as Express.MultipartyUpload, (clz as any).basePath + '/');
+
+        if (after) {
+          await after(req);
+        }
       }
-
     })(target, propertyKey, descriptor);
   };
 }
 
-export function Upload() {
-  return doUpload();
+export function Upload(config: UploadConfig = Config) {
+  return doUpload(config);
 }
