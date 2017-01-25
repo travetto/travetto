15056a3117a296bb40ad17bbe6bc1737ac886ce3
Timothy Soehnlin <tim@eaiti.com>
2017-01-25 15:24:47 -0600

    Fixing support

--- module/asset-mongo/src/lib/config.ts
+++ module/asset-mongo/src/lib/config.ts
@@ -1,7 +1,7 @@
 import { Configure } from '@encore/config';
 
 export default Configure.registerNamespace('asset', {
-  maxSize: 10000,
+  maxSize: 10 * 1024 * 1024,
   allowedTypes: '',
   excludeTypes: ''
 });
