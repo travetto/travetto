a5ca7493a52e69fdbc8679f171bad92e2347fdd7
Timothy Soehnlin <tim@eaiti.com>
2017-02-03 15:54:09 -0500

    Fixing

---
+++ module/schema/src/lib/util/messages.ts
@@ -0,0 +1,10 @@
+export class Messages {
+  static EMAIL = '{PATH} is not a valid email address';
+  static TELEPHONE = '{PATH} is not a valid phone number';
+  static URL = '{PATH} is not a valid url';
+  static SIMPLE_NAME = '{PATH} is not a proper name';
+  static POSTAL_CODE = '{PATH} is not a valid postal code';
+  static REQUIRED = '{PATH} is required';
+  static MINLENGTH = '{PATH} is not long enough ({MIN})';
+  static MAXLENGTH = '{PATH} is too long ({MAX})';
+}
