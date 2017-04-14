0103c51b0bd8f36330b536c843397ac9ff9e430f
Timothy Soehnlin <tim@eaiti.com>
2017-04-14 13:24:51 -0400

    Enabling long stack traces

---
+++ module/context/src/lib/stack.ts
@@ -0,0 +1,63 @@
+require('async-listener');
+
+
+type StackType = any;
+
+export class Stack {
+  constructor(public stack:StackType, public parent: Stack | null) {}
+
+  getStacks() {
+    let stacks = [this.stack];
+    let parent = this.parent;
+
+    while (true) {
+      if (!parent) {
+          break;
+      }
+
+      stacks.push(parent.stack);
+      parent = parent.parent;
+    }
+    return stacks;
+  }
+}
+
+/**
+ * AsyncListener object
+ */
+class AsyncListener {
+  stackSeparator = 'From previous event:';
+  currentStack:Stack|null = null;
+
+  create() {
+    // This always gets called in between before/after
+    let trace:{name?:string, stack?:StackType} = {};
+    Error.captureStackTrace(trace, this.create.bind(this));
+    trace.name = this.stackSeparator;
+    return new Stack(trace.stack, this.currentStack);
+  }
+
+  before(context:any, stack:Stack) {
+    this.currentStack = stack;
+  }
+
+  after(context:any, stack:Stack) {
+    this.currentStack = null;
+  }
+
+  error(stack:Stack, err:Error) {
+    if (stack) {
+        err.stack += '\n' + stack.getStacks().join('\n');
+    }
+    this.currentStack = null;
+  }
+}
+
+let listener:AsyncListener;
+
+export function enableLongStacktrace() {
+  if (!listener) {
+    Error.stackTraceLimit = Infinity;    
+    (process as any).addAsyncListener(listener = new AsyncListener());
+  }
+}
