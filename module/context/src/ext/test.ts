b58fa5f1cb726b5693076edde4a66b5c999b262d
Timothy Soehnlin <tim@eaiti.com>
2017-04-14 16:10:17 -0400

    Fixing context

--- module/context/src/ext/test.ts
+++ module/context/src/ext/test.ts
@@ -3,14 +3,20 @@ import { beforeTest, afterTest } from '@encore/test';
 
 let store: { [key: string]: any } = {};
 
-Context.storage = {
+let storage = {
+  run: () => {},
+  bindEmitter(e:any) {},
   get: (key: string) => store[key],
   set: (key: string, val: any) => store[key] = val
 };
 
 let ctx = {};
 
-beforeTest(() => Context.set(Object.assign({}, ctx)));
+beforeTest(() => {
+  Context.storage = storage;
+  Context.set(Object.assign({}, ctx))
+});
+
 afterTest(() => Context.clear());
 
 export function setDefault(obj: any) {
