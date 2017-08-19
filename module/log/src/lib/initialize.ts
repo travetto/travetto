0e44d87e616720703fb022cad4a146c26c735a07
Timothy Soehnlin <tim@eaiti.com>
2017-08-19 18:49:24 -0400

    Simplifying config

--- module/log/src/lib/initialize.ts
+++ module/log/src/lib/initialize.ts
@@ -3,14 +3,43 @@ import { addLayout } from 'log4js/lib/layouts';
 
 import Config from './config';
 import { Layouts } from './layout';
-import { processAppenderConfig } from './appender';
+import { isFileAppender } from './types';
+import { simpleName } from '@encore/base/info';
+import * as mkdirp from 'mkdirp';
 
 let appenders = Object
   .keys(Config.appenders)
   .reduce((acc, name) => {
     let conf = (Config.appenders as any)[name];
     if (!conf.hasOwnProperty('enabled') || conf.enabled) {
-      acc[name] = processAppenderConfig(conf)!;
+      if (isFileAppender(conf)) {
+        if (!conf.filename) {
+          conf.filename = simpleName;
+          if (conf.name) {
+            conf.filename += `-${conf.name}`;
+          }
+          conf.filename += '.log';
+        }
+        if (!conf.filename.startsWith('/')) {
+          conf.filename = `${process.cwd()}/logs/${conf.filename}`;
+          conf.absolute = true;
+        }
+
+        // Setup folder for logging
+        mkdirp.sync(conf.filename.substring(0, conf.filename.lastIndexOf('/')));
+      }
+
+      if (conf.level) {
+        acc[`_${name}`] = conf;
+        conf = {
+          type: 'logLevelFilter',
+          appender: `_${name}`,
+          level: conf.level,
+          maxLevel: conf.maxLevel || 'fatal'
+        }
+      }
+
+      acc[name] = conf!;
     }
     return acc;
   }, {} as { [key: string]: log4js.Appender });
@@ -18,12 +47,13 @@ let appenders = Object
 let categories = Object
   .keys(Config.categories)
   .reduce((acc, name) => {
-    acc[name] = categories[name];
+    acc[name] = (Config.categories as any)[name];
     if (typeof acc[name].appenders === 'string') {
       acc[name].appenders = (acc[name].appenders as any as string).split(',').map(x => x.trim())
     }
     return acc;
   }, {} as { [key: string]: log4js.Category })
+
 for (let layout of Object.keys(Layouts)) {
   addLayout(layout, Layouts[layout]);
 }
@@ -38,7 +68,7 @@ if (Config.appenders.console) {
 
   if (override === null ? process.env.env !== 'test' : !!override) {
     const logger = log4js.getLogger('console');
-    for (let key of Object.keys(log4js.LevelType)) {
+    for (let key of ['info', 'warn', 'error', 'debug']) {
       if (key in console && key in logger) {
         (console as any)[key] = (logger as any)[key].bind(logger);
 
