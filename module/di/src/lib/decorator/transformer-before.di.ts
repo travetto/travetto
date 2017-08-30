6278c64b4ba6e7f70c10fac99424af8d03410e99
Timothy Soehnlin <tim@eaiti.com>
2017-08-30 09:15:55 -0400

    Working on annotation registration

--- module/di/src/lib/decorator/transformer-before.di.ts
+++ module/di/src/lib/decorator/transformer-before.di.ts
@@ -18,6 +18,30 @@ export const Transformer =
       return ret;
     };
 
+function processParam(state: State, param: ts.ParameterDeclaration) {
+  let name = TransformUtils.getDecorator(param, require.resolve('./injectable'), 'Inject');
+  let type = TransformUtils.getTypeChecker().getTypeAtLocation(param);
+  let decl = type!.symbol!.valueDeclaration!;
+  let path = (decl as any).parent.fileName;
+  let ident = ts.createIdentifier(`${(decl as any).name.text}`);
+  let importName = ts.createUniqueName(`import_${(decl as any).name.text}`);
+
+  if (require.resolve(path) !== state.path) {
+    state.imports.push({
+      ident: importName,
+      path
+    });
+
+    return TransformUtils.fromLiteral({
+      class: ts.createPropertyAccess(importName, ident),
+      name: name ? (name.expression as ts.CallExpression).arguments[0] : undefined
+    });
+  } else {
+    return TransformUtils.fromLiteral({
+      class: (decl as any).name
+    });
+  }
+}
 
 function visitNode<T extends ts.Node>(context: ts.TransformationContext, node: T, state: State): T {
   if (ts.isClassDeclaration(node)) {
@@ -35,49 +59,16 @@ function visitNode<T extends ts.Node>(context: ts.TransformationContext, node: T
     if (cons) {
       let dec = foundDec!;
       let expr = (dec.expression as ts.CallExpression).arguments[0] as ts.ObjectLiteralExpression;
-      if (!expr) {
-        expr = ts.createObjectLiteral([]);
-      }
-      let props = [
-        ...expr.properties,
-        ts.createPropertyAssignment(
-          ts.createLiteral('dependencies'),
-          ts.createArrayLiteral(
-            (cons.parameters! || []).map(x => {
-              let name = TransformUtils.getDecorator(x, require.resolve('./injectable'), 'Inject');
-              let type = TransformUtils.getTypeChecker().getTypeAtLocation(x);
-              let decl = type!.symbol!.valueDeclaration!;
-              let path = (decl as any).parent.fileName;
-              let ident = ts.createIdentifier(`${(decl as any).name.text}`);
-              let importName = ts.createUniqueName(`import_${(decl as any).name.text}`);
-
-              if (require.resolve(path) !== state.path) {
-                state.imports.push({
-                  ident: importName,
-                  path
-                });
-
-                let obj = ts.createObjectLiteral([
-                  ts.createPropertyAssignment('class', ts.createPropertyAccess(importName, ident)),
-                  ts.createPropertyAssignment('name', name ? (name.expression as ts.CallExpression).arguments[0] : ts.createIdentifier('undefined'))
-                ]);
-                return obj;
-              } else {
-                let obj = ts.createObjectLiteral([
-                  ts.createPropertyAssignment('class', (decl as any).name),
-                ]);
-                return obj;
-              }
-            })
-          )
-        )
-      ];
+      let conf = TransformUtils.extendObjectLiteral({
+        annotations: (node.decorators! || []).map(x => TransformUtils.getDecoratorIdent(x)),
+        dependencies: (cons.parameters! || []).map(x => processParam(state, x))
+      });
 
       dec = ts.updateDecorator(dec, ts.updateCall(
         (dec.expression as ts.CallExpression),
         (dec.expression as ts.CallExpression).expression,
         undefined,
-        [ts.updateObjectLiteral(expr!, props)]
+        [conf]
       ))
 
       ret = ts.updateClassDeclaration(ret,
