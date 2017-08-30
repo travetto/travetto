f4d245ed8093dec9ec404eab0dd5d7e67f0e491e
Timothy Soehnlin <tim@eaiti.com>
2017-08-30 07:58:55 -0400

    Working on di

--- module/di/src/lib/service/registry.ts
+++ module/di/src/lib/service/registry.ts
@@ -7,20 +7,24 @@ export interface Dependency {
   type: Class<any>
 }
 
+export interface BaseInjectable {
+  postConstruct?: () => any
+}
+
 export class Registry {
   static providers = new Map<Class<any>, Map<string, Class<any>>>();
   static classesByAnnotation = new Map<Function, Set<Class<any>>>();
   static instances = new Map<Class<any>, Map<string, any>>();
   static dependencies = new Map<Class<any>, Array<Dependency>>();
 
-  private static registerFullInstance<T>(cls: Class<T>, instance: T, name: string = DEFAULT_INSTANCE) {
+  private static registerFullInstance<T extends BaseInjectable>(cls: Class<T>, instance: T, name: string = DEFAULT_INSTANCE) {
     if (!this.instances.has(cls)) {
       this.instances.set(cls, new Map());
     }
     this.instances.get(cls)!.set(name, instance);
   }
 
-  static registerProvider<T>(cls: Class<T>, target: Class<T> = cls, name: string = DEFAULT_INSTANCE, dependencies: Dependency[] = []) {
+  static registerProvider<T extends BaseInjectable>(cls: Class<T>, target: Class<T> = cls, name: string = DEFAULT_INSTANCE, dependencies: Dependency[] = []) {
     if (!this.providers.has(target)) {
       this.providers.set(target, new Map());
     }
@@ -33,14 +37,17 @@ export class Registry {
     this.dependencies.set(cls, dependencies.slice(0));
   }
 
-  static construct<T>(cls: Class<T>, name: string = DEFAULT_INSTANCE): T {
+  static async construct<T extends BaseInjectable>(cls: Class<T>, name: string = DEFAULT_INSTANCE): Promise<T> {
     let deps = (this.dependencies.get(cls)! || [])
       .map(x => this.getInstance(x.type, x.name));
-    let inst = new cls(...deps);
+    let inst = new cls(...(await Promise.all(deps)));
+    if (inst.postConstruct) {
+      await inst.postConstruct();
+    }
     return inst;
   }
 
-  static getInstance<T>(cls: Class<T>, name: string = DEFAULT_INSTANCE): T {
+  static getInstance<T extends BaseInjectable>(cls: Class<T>, name: string = DEFAULT_INSTANCE): Promise<T> {
     if (!this.instances.has(cls)) {
       this.instances.set(cls, new Map());
     }
