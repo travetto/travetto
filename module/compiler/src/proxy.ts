da742584b11f737111338c0d58ac2283127238bd
Timothy Soehnlin <tim@eaiti.com>
2017-08-31 09:26:33 -0400

    Finalizing loading process

--- module/compiler/src/proxy.ts
+++ module/compiler/src/proxy.ts
@@ -1,6 +1,34 @@
 export class RetargettingHandler<T extends any> implements ProxyHandler<T> {
   constructor(public target: T) { }
 
+
+  isExtensible?(target: T): boolean {
+    return Object.isFrozen(this.target);
+  }
+
+  getOwnPropertyDescriptor(target: T, property: PropertyKey) {
+    if (property === '__esModule') {
+      return undefined;
+    } else {
+      return Object.getOwnPropertyDescriptor(this.target, property);
+    }
+  }
+
+  preventExtensions?(target: T): boolean {
+    return !!Object.preventExtensions(this.target);
+  }
+
+  apply?(target: T, thisArg: any, argArray?: any): any {
+    return this.target.apply(thisArg, argArray);
+  }
+  construct?(target: T, argArray: any, newTarget?: any) {
+    return new this.target(...argArray);
+  }
+
+  setPrototypeOf?(target: T, v: any): boolean {
+    return Object.setPrototypeOf(this.target, v);
+  }
+
   getPrototypeOf(target: T): object | null {
     return Object.getPrototypeOf(this.target);
   }
@@ -17,12 +45,11 @@ export class RetargettingHandler<T extends any> implements ProxyHandler<T> {
     return this.target[prop] = value;
   }
 
-  enumerate?(target: T): PropertyKey[] {
-    return Object.keys(this.target);
-  }
-
   ownKeys?(target: T): PropertyKey[] {
-    return Object.getOwnPropertyNames(this.target);
+    let keys = ([] as PropertyKey[])
+      .concat(Object.getOwnPropertyNames(this.target))
+      .concat(Object.getOwnPropertySymbols(this.target));
+    return keys;
   }
 
   deleteProperty(target: T, p: PropertyKey) {
