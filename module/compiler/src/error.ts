87ad1dfe3254e54c2b668bcc32a7f270117c3e8f
Timothy Soehnlin <tim@eaiti.com>
2017-08-31 16:12:40 -0400

    Upping error handling

--- module/compiler/src/error.ts
+++ module/compiler/src/error.ts
@@ -1,9 +1,9 @@
 // Pulled from http://stackoverflow.com/questions/31089801/extending-error-in-javascript-with-es6-syntax#answer-32749533
-export class BaseError<T = any> extends Error {
-  constructor(message: string, public payload?: T) {
-    super(message);
-    this.name = this.constructor.name;
-    this.message = message;
+export class BaseError<T = any> {
+  name: string;
+  stack: any;
+
+  constructor(public message: string, public payload?: T) {
     if (typeof Error.captureStackTrace === 'function') {
       Error.captureStackTrace(this, this.constructor);
     } else {
@@ -11,3 +11,5 @@ export class BaseError<T = any> extends Error {
     }
   }
 }
+
+Object.setPrototypeOf(BaseError.prototype, Error.prototype);
