372a34285ce1dbdcc17e05a9b828e515b266ac74
Timothy Soehnlin <tim@eaiti.com>
2017-08-31 07:23:18 -0400

    Moving folders

--- module/base/bootstrap.js
+++ module/base/bootstrap.js
@@ -11,4 +11,4 @@ require.extensions['.ts'] = function load(m, tsf) {
   return m._compile(ts.transpile(fs.readFileSync(tsf, 'utf-8')), tsf.replace(/\.ts$/, '.js'));
 };
 
-require('./src/lib/compiler.ts').Compiler.init(process.cwd());
+require('./src/compiler.ts').Compiler.init(process.cwd());
