109118e1c2f1e461c4c8d98d538237d96168bfe6
Timothy Soehnlin <tim@eaiti.com>
2017-08-31 14:35:44 -0400

    Fixing types again

--- module/base/src/transform-util.ts
+++ module/base/src/transform-util.ts
@@ -117,7 +117,7 @@ export class TransformUtil {
   }
 
   static getTypeInfoForNode(node: ts.Node) {
-    let type = TransformUtil.getTypeChecker().getTypeAtLocation(node);
+    let type = this.getTypeChecker().getTypeAtLocation(node);
     let decl = type!.symbol!.valueDeclaration!;
     let path = (decl as any).parent.fileName;
     let ident = (decl as any).name;
