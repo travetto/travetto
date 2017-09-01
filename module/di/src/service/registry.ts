317feceebc636ae8a0a86cb8f6e9cf618b22c025
Timothy Soehnlin <tim@eaiti.com>
2017-09-01 01:33:20 -0400

    Fixing globs

--- module/di/src/service/registry.ts
+++ module/di/src/service/registry.ts
@@ -173,7 +173,7 @@ export class Registry {
   static async initialize() {
     if (!this._waitingForInit) {
       this._waitingForInit = true;
-      let globs = (process.env.SCAN_GLOBS || 'node_modules/@encore/src/**/*.ts src/**/*.ts').split(/\s+/);
+      let globs = (process.env.SCAN_GLOBS || 'node_modules/@encore/*/src/**/*.ts src/**/*.ts').split(/\s+/);
       for (let glob of globs) {
         bulkRequire(glob, undefined, p => p.indexOf('/ext/') < 0);
       }
