015fc91508c5badedccb37e58c3c0a22c8abfbf1
Timothy Soehnlin <tim@eaiti.com>
2017-09-01 11:26:30 -0400

    Finalizing changes

--- module/schema/src/service/validator.ts
+++ module/schema/src/service/validator.ts
@@ -11,16 +11,16 @@ export class SchemaValidator {
   static schemas: Map<Class, Map<string, mg.Schema>> = new Map();
 
   static getSchema<T>(cls: Class<T>, view: string = SchemaRegistry.DEFAULT_VIEW) {
-    if (!SchemaValidator.schemas.has(cls)) {
-      SchemaValidator.schemas.set(cls, new Map());
+    if (!this.schemas.has(cls)) {
+      this.schemas.set(cls, new Map());
     }
-    let viewMap: Map<string, mg.Schema> = SchemaValidator.schemas.get(cls) as Map<string, mg.Schema>;
+    let viewMap: Map<string, mg.Schema> = this.schemas.get(cls) as Map<string, mg.Schema>;
     if (!viewMap.has(view)) {
       let config = SchemaRegistry.schemas.get(cls);
       if (!config || !config.views[view]) {
         throw new Error(`Unknown view found: ${view}`);
       }
-      viewMap.set(view, SchemaValidator.getSchemaRaw(config.views[view].schema));
+      viewMap.set(view, this.getSchemaRaw(config.views[view].schema));
     }
     return viewMap.get(view) as mg.Schema;
   }
@@ -30,7 +30,7 @@ export class SchemaValidator {
       let isArray = Array.isArray(schema[key].type);
       let type = isArray ? schema[key].type[0] : schema[key].type;
       if (ObjectUtil.isPlainObject(type)) {
-        let sub = SchemaValidator.getSchemaRaw(type);
+        let sub = this.getSchemaRaw(type);
         schema[key].type = isArray ? [sub] : sub;
       }
     }
@@ -45,16 +45,16 @@ export class SchemaValidator {
 
   static async validateAllRaw<T>(obj: T[], schema: mg.Schema): Promise<T[]> {
     return await Promise.all<T>((obj || [])
-      .map((o, i) => SchemaValidator.validateRaw(o, schema)));
+      .map((o, i) => this.validateRaw(o, schema)));
   }
 
   static async validate<T>(o: T, view?: string): Promise<T> {
-    return await SchemaValidator.validateRaw(o, SchemaValidator.getSchema(SchemaRegistry.getClass(o), view));
+    return await this.validateRaw(o, this.getSchema(SchemaRegistry.getClass(o), view));
   }
 
   static async validateAll<T>(obj: T[], view: string = SchemaRegistry.DEFAULT_VIEW): Promise<T[]> {
     return await Promise.all<T>((obj || [])
-      .map((o, i) => SchemaValidator.validate(o, view)));
+      .map((o, i) => this.validate(o, view)));
   }
 }
 
