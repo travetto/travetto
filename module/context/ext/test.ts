8d4c45bb6e9fa5e76e5ee233e29bdd4563e0e2a4
Timothy Soehnlin <tim@eaiti.com>
2017-09-02 15:01:10 -0400

    Fixing deps

--- module/context/ext/test.ts
+++ module/context/ext/test.ts
@@ -1,1 +1,26 @@
-export * from '../src/ext/test';
+import { Context } from '../service';
+import { beforeTest, afterTest } from '@encore/test';
+
+let store: { [key: string]: any } = {};
+
+let storage = {
+  run: () => { },
+  bindEmitter(e: any) { },
+  get: (key: string) => store[key],
+  set: (key: string, val: any) => store[key] = val
+};
+
+let ctx = {};
+
+beforeTest(() => {
+  //  Context.storage = storage;
+  //  Context.set(Object.assign({}, ctx))
+});
+
+afterTest(() => {
+  // Context.clear()
+});
+
+export function setDefault(obj: any) {
+  Object.assign(ctx, obj);
+}
