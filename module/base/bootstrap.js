39662c26755a15cfa6d333caee4f2e6df47e0cd2
Timothy Soehnlin <tim@eaiti.com>
2017-09-02 10:21:32 -0400

    Simplifying base

--- module/base/bootstrap.js
+++ module/base/bootstrap.js
@@ -1,2 +1,7 @@
-require('./bootstrap-shim');
-require('./src/compiler.ts').Compiler.init(process.cwd());
+const fs = require('fs');
+const ts = require('typescript');
+
+//Simple bootstrap to load compiler
+require.extensions['.ts'] = function load(m, tsf) {
+  return m._compile(ts.transpile(fs.readFileSync(tsf, 'utf-8')), tsf.replace(/\.ts$/, '.js'));
+};
--- module/base/src/index.ts
+++ module/base/src/index.ts
@@ -1,5 +1,2 @@
-export * from './compiler';
-export * from './transform-util';
 export * from './app-info';
-export * from './proxy';
 export * from './bulk-find';
