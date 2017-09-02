2ccf6ffd6c7ab8aa3a3f72d9f09d76cb1933b813
Timothy Soehnlin <tim@eaiti.com>
2017-09-02 01:34:03 -0400

    Setting up shim

--- module/base/bootstrap.js
+++ module/base/bootstrap.js
@@ -1,14 +1,2 @@
-const fs = require('fs');
-const ts = require('typescript');
-
-// Expose
-if (!console.debug) {
-  console.debug = console.log;
-}
-
-//Simple bootstrap to load compiler
-require.extensions['.ts'] = function load(m, tsf) {
-  return m._compile(ts.transpile(fs.readFileSync(tsf, 'utf-8')), tsf.replace(/\.ts$/, '.js'));
-};
-
+require('./bootstrap-shim');
 require('./src/compiler.ts').Compiler.init(process.cwd());
