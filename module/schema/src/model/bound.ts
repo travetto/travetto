a7fe39b7669ac12c83bef7e832cb660d83cd8474
Timothy Soehnlin <tim@eaiti.com>
2017-09-02 15:01:24 -0400

    Fixing deps

--- module/schema/src/model/bound.ts
+++ module/schema/src/model/bound.ts
@@ -3,6 +3,6 @@ import { SchemaRegistry } from '../service';
 
 export class SchemaBound {
   static from<T>(this: new () => T, data: any, view: string = SchemaRegistry.DEFAULT_VIEW): T {
-    return BindUtil.bindSchema(this, new this(), data, view);
+    return BindUtil.bindSchema(this as any, new this(), data, view);
   }
 }
