a4636d8588ea66783341ace7c56eec89a118cf3f
Timothy Soehnlin <tim@eaiti.com>
2017-09-01 23:09:23 -0400

    Removing dependency

--- module/config/src/decorator/config.ts
+++ module/config/src/decorator/config.ts
@@ -1,15 +1,9 @@
 import { ConfigLoader } from '../service/config-loader';
-import { DependencyRegistry, Class, DEFAULT_INSTANCE } from '@encore/di';
+import { Class, DEFAULT_INSTANCE } from '@encore/di';
 
 export function Config(ns: string, depTarget?: Class<any>, name: string = DEFAULT_INSTANCE) {
   return (target: Class<any & { postConstruct?: () => any }>) => {
     let og = target.prototype.postConstruct;
-
-    DependencyRegistry.finalizeClass({
-      name,
-      class: target,
-      target: depTarget || target
-    });
 
     target.prototype.postConstruct = function () {
       // Apply config
