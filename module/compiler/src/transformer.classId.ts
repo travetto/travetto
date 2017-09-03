fbfffb366277a4ace847b2e025dbc80670f01837
Timothy Soehnlin <tim@eaiti.com>
2017-09-03 11:30:19 -0400

    Precomputing classId transformer

--- module/compiler/src/transformer.classId.ts
+++ module/compiler/src/transformer.classId.ts
@@ -1,5 +1,10 @@
 import * as ts from 'typescript';
-import { TransformUtil } from './transform-util';
+import * as path from 'path';
+
+const SEP = path.sep;
+const RE_SEP = SEP === '/' ? '\\/' : SEP;
+const SRC_RE = new RegExp(`${RE_SEP}src${RE_SEP}`, 'g');
+const PATH_RE = new RegExp(RE_SEP, 'g');
 
 function createStaticField(name: string, val: ts.Expression) {
   return ts.createProperty(
@@ -9,7 +14,7 @@ function createStaticField(name: string, val: ts.Expression) {
   );
 }
 
-function visitNode<T extends ts.Node>(context: ts.TransformationContext, node: T): T {
+function visitNode<T extends ts.Node>(context: ts.TransformationContext, node: T, state: { file: string }): T {
   if (ts.isClassDeclaration(node)) {
     return ts.updateClassDeclaration(node,
       node.decorators,
@@ -19,26 +24,24 @@ function visitNode<T extends ts.Node>(context: ts.TransformationContext, node: T
       ts.createNodeArray(node.heritageClauses),
       ts.createNodeArray([
         createStaticField('__filename', ts.createIdentifier('__filename')),
-        createStaticField('__id', ts.createBinary(
-          ts.createIdentifier('__filename'),
-          ts.SyntaxKind.PlusToken,
-          ts.createBinary(
-            ts.createLiteral('#'),
-            ts.SyntaxKind.PlusToken,
-            ts.createIdentifier(node.name!.getText())
-          )
-        )),
+        createStaticField('__id', ts.createLiteral(state.file + '#' + node.name!.getText())),
 
         ...node.members
       ])
     ) as any;
   }
-  return ts.visitEachChild(node, c => visitNode(context, c), context);
+  return ts.visitEachChild(node, c => visitNode(context, c, state), context);
 }
-
 
 export const ClassIdTransformer = {
   transformer: (context: ts.TransformationContext) =>
-    (file: ts.SourceFile) => visitNode(context, file),
+    (file: ts.SourceFile) => {
+      let fileRoot = file.fileName.split(process.cwd())[1]
+        .replace(SRC_RE, SEP)
+        .replace(PATH_RE, '.')
+        .replace(/^\./, '')
+        .replace(/\.(t|j)s$/, '');
+      return visitNode(context, file, { file: fileRoot })
+    },
   phase: 'before'
 }
