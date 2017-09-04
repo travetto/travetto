5e1b97d4502547fed94cdbc3f21b7816a164a52d
Timothy Soehnlin <tim@eaiti.com>
2017-09-04 09:05:29 -0400

    Supporting all envs

--- module/base/src/env.ts
+++ module/base/src/env.ts
@@ -1,17 +1,19 @@
 let e = process.env;
 
-const envs = new Set([
+const envs = [
   'application', ...(e.ENV || e.env || e.NODE_ENV || 'dev')
     .toLowerCase().split(/[, ]+/)
-]);
+];
 
-const is = envs.has.bind(envs);
+const envSet = new Set(envs);
+
+const is = envSet.has.bind(envs);
 
 const prod = is('prod') || is('production');
 const test = is('test') || is('testing');
 const dev = !prod && !test;
 const watch = dev && !('NO_WATCH' in e);
 
-export const AppEnv = { prod, dev, test, is, watch };
+export const AppEnv = { prod, dev, test, is, watch, all: envs };
 
 process.env.NODE_ENV = prod ? 'production' : 'development';
