cd1c682ba2eebcc5cf15ae30a7e78b2c024ef057
Timothy Soehnlin <tim@eaiti.com>
2017-09-03 22:59:26 -0400

    Testing

---
+++ module/compiler/test/stack.ts
@@ -0,0 +1,17 @@
+import '@encore/base/src/stacktrace';
+
+function test() {
+  setTimeout(function inner1() {
+    setTimeout(function inner2() {
+      setTimeout(function inner3() {
+        throw new Error('Uh oh');
+      }, 1);
+    }, 1);
+  }, 1);
+}
+
+try {
+  test();
+} catch (e) {
+  console.log(e);
+}
