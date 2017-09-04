3389be3e9bb1524c39a13e74ffffba332569a440
Timothy Soehnlin <tim@eaiti.com>
2017-09-04 09:56:08 -0400

    Working on schema

--- module/schema/src/model/bound.ts
+++ module/schema/src/model/bound.ts
@@ -2,7 +2,7 @@ import { BindUtil } from '../util';
 import { SchemaRegistry } from '../service';
 
 export class SchemaBound {
-  static from<T>(this: new () => T, data: any, view: string = SchemaRegistry.DEFAULT_VIEW): T {
+  static from<T>(this: new () => T, data: Partial<T>, view: string = SchemaRegistry.DEFAULT_VIEW): T {
     return BindUtil.bindSchema(this as any, new this(), data, view);
   }
 }
