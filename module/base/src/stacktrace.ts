4c048aaa31c67c611e8ef3c0a7239ba8e8158b68
Timothy Soehnlin <tim@eaiti.com>
2017-09-03 22:59:11 -0400

    Stack trace management

--- module/base/src/stacktrace.ts
+++ module/base/src/stacktrace.ts
@@ -2,13 +2,19 @@ import * as async_hooks from 'async_hooks';
 import * as fs from 'fs';
 import { AppEnv } from './env';
 
+let installed = false;
+let ogPrep: any;
+
+let ogName = __filename.replace(/\.js$/, '');
+
 class StackTraceListener {
   currentId: number = -1;
   stackSeparator = 'Continued from';
   stackMap = new Map<number, string>();
+  ogPrep: any;
+
 
   constructor() {
-    (Error as any).prepareStackTrace = (err: any, stack: any) => this.prepareStackTrace(err, stack);
     this.init = this.init.bind(this);
     this.destroy = this.destroy.bind(this);
   }
@@ -17,42 +23,56 @@ class StackTraceListener {
     fs.writeSync(1, msg + '\n');
   }
 
-  filterErrorStack(err: string) {
+  filterErrorStack(err: string, removeHead: boolean) {
     const [head, ...rest] = (err || '').split('\n');
-    return rest
+    return (removeHead ? '' : `${head}\n`) + (rest
       .filter(l =>
-        l.indexOf(__filename) < 0 &&
-        l.indexOf('(timers.js') < 0 &&
+        l.indexOf(ogName) < 0 &&
+        l.indexOf('timers.js') < 0 &&
         l.indexOf('async_hooks') < 0 &&
         l.indexOf('module.js') < 0 &&
         l.indexOf('(native)') < 0 &&
         l.indexOf('source-map-support.js')
-      ).join('\n');
+      ).join('\n'));
   }
 
   prepareStackTrace(err: any, stack: any) {
     let parentId = async_hooks.executionAsyncId() || this.currentId;
+    let errMsg = (typeof err === 'string' ? err : err.stack) || '';
+    errMsg = this.filterErrorStack(errMsg, false);
 
     if (this.stackMap.has(parentId)) {
-      err = (err || '') + '\n' + this.stackMap.get(parentId)!;
+      errMsg += '\n' + this.stackMap.get(parentId)!;
+    }
+
+    if (typeof err === 'string') {
+      err = errMsg;
+    } else {
+      err.stack = errMsg;
     }
 
     return err;
   }
 
   init(id: number, type: string, triggerAsyncId: number, resource: any) {
+    if (!installed) {
+      installed = true;
+      ogPrep = (Error as any).prepareStackTrace;
+      console.log('ogprep', ogPrep);
+      (Error as any).prepareStackTrace = (err: any, stck: any) => this.prepareStackTrace(ogPrep ? ogPrep(err, stck) : err, stck);
+
+    }
     let stack: any = {};
-    let prep = (Error as any).prepareStackTrace;
-    (Error as any).prepareStackTrace = null;
+    let prep: any = (Error as any).prepareStackTrace;
+    (Error as any).prepareStackTrace = ogPrep;
     Error.captureStackTrace(stack);
     (Error as any).prepareStackTrace = prep;
 
     let parentId = triggerAsyncId || this.currentId;
-
     let parent = this.stackMap.get(parentId)!;
 
     this.stackMap.set(id,
-      this.filterErrorStack(stack.stack) +
+      this.filterErrorStack(stack.stack, true) +
       (parent ? `\n${this.stackSeparator}\n${parent}` : '')
     );
   }
