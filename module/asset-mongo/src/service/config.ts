ae76dc1e418490b97df84cc5d57305e125acf85b
Timothy Soehnlin <tim@eaiti.com>
2017-09-05 09:08:23 -0400

    Fixing config

--- module/asset-mongo/src/service/config.ts
+++ module/asset-mongo/src/service/config.ts
@@ -2,10 +2,10 @@ import { Config } from '@encore/config';
 
 @Config('asset.mongo')
 export class MongoAssetConfig {
-  hosts: 'localhost';
-  schema: 'app';
-  port: 27017;
-  options: {};
+  hosts = 'localhost';
+  schema = 'app';
+  port = 27017;
+  options = {};
 
   get url() {
     let hosts = this.hosts.split(',').map(h => `${h}:${this.port}`).join(',');
