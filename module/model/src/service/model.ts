2c430ef53e6127110ae68ef9ed4c5309647dd2d0
Timothy Soehnlin <tim@eaiti.com>
2017-09-06 06:39:11 -0400

    Working on model sourcery

--- module/model/src/service/model.ts
+++ module/model/src/service/model.ts
@@ -1,8 +1,10 @@
 import { BindUtil, Class, SchemaRegistry, SchemaValidator } from '@encore/schema';
 import { ModelOptions } from './types';
-import { ModelCore, Query, QueryOptions } from '../model';
+import { ModelCore, Query, QueryOptions, BulkState } from '../model';
 import { ModelSource } from './source';
 import { ModelRegistry } from './registry';
+
+type Id = number | string;
 
 export class ModelService {
 
@@ -17,51 +19,47 @@ export class ModelService {
 
     let cons = cls;
 
-    if (config && config.subtypes && !!(o as any)['_type']) {
-      cons = config.subtypes[(o as any)['_type']];
+    if (config && config.subtypes && !!(o as any)[this.source.getTypeField()]) {
+      cons = config.subtypes[(o as any)[this.source.getTypeField()]];
     }
 
     return BindUtil.bindSchema(cons, new cons(), o);
   }
 
   async prePersist<T extends ModelCore>(o: T, view: string = SchemaRegistry.DEFAULT_VIEW) {
-    return await SchemaValidator.validate(o.preSave ? o.preSave() : o, view);
+    let res = await SchemaValidator.validate(o.preSave ? o.preSave() : o, view);
+    res = await this.source.prePersist(res);
+    return res;
   }
 
-  onRetrieve<T extends ModelCore>(cls: Class<T>, o: T): T {
+  postLoad<T extends ModelCore>(cls: Class<T>, o: T): T {
+    o = this.source.postLoad(o);
     o = this.convert(cls, o);
-    return o.postLoad ? o.postLoad() : o;
+    o = o.postLoad ? o.postLoad() : o;
+    return o;
   }
 
-  rewriteError<T extends ModelCore>(cls: Class<T>, e: any) {
-    if (e.code === 11000) { // Handle duplicate errors
-      e = new Error('Duplicate entry already exists');
-    }
-
-    return e;
-  }
-
-  async getByQuery<T extends ModelCore>(cls: Class<T>, query: Query<T> = {}, options: QueryOptions = {}): Promise<T[]> {
+  async getAllByQuery<T extends ModelCore>(cls: Class<T>, query: Query<T> = {}, options: QueryOptions = {}): Promise<T[]> {
     const config = this.getConfig(cls);
     if (!options.sort && config.defaultSort) {
       options.sort = config.defaultSort;
     }
-    let res = await this.source.getByQuery<T>(cls, query, options);
-    return res.map(o => onRetrieve(cls, o));
+    let res = await this.source.getAllByQuery(cls, query, options);
+    return res.map(o => this.postLoad(cls, o));
   }
 
-  async getCountByQuery<T extends ModelCore>(cls: Class<T>, query: Object = {}, options: QueryOptions = {}): Promise<{ count: number }> {
-    let res = await this.source.getCountByQuery(cls, query, options);
+  async getCountByQuery<T extends ModelCore>(cls: Class<T>, query: Query<T> = {}): Promise<number> {
+    let res = await this.source.getCountByQuery(cls, query);
     return res;
   }
 
-  async findOne<T extends ModelCore>(cls: Class<T>, query: Object, options: QueryOptions = {}, failOnMany: boolean = true): Promise<T> {
-    let res = await this.source.findOne<T>(cls, query, options, failOnMany);
-    return this.onRetrieve(cls, res);
+  async getByQuery<T extends ModelCore>(cls: Class<T>, query: Query<T>, options: QueryOptions = {}, failOnMany: boolean = true): Promise<T> {
+    let res = await this.source.getByQuery<T>(cls, query, options, failOnMany);
+    return this.postLoad(cls, res);
   }
 
-  async createOrUpdate<T extends ModelCore>(o: T, query: Object): Promise<T> {
-    let res = await this.getByQuery(SchemaRegistry.getClass(o), query);
+  async saveOrUpdate<T extends ModelCore>(o: T, query: Query<T>): Promise<T> {
+    let res = await this.getAllByQuery(SchemaRegistry.getClass(o), query, { limit: 2 });
     if (res.length === 1) {
       o = ObjectUtil.merge(res[0], o);
       return await this.update(o);
@@ -71,83 +69,56 @@ export class ModelService {
     throw new Error(`Too many already exist: ${res.length}`);
   }
 
-  async getById<T extends ModelCore>(cls: Class<T>, id: string): Promise<T> {
+  async getById<T extends ModelCore>(cls: Class<T>, id: Id): Promise<T> {
     let res = await this.source.getById<T>(cls, id);
-    return this.onRetrieve(cls, res);
+    return this.postLoad(cls, res);
   }
 
-  async deleteById<T extends ModelCore>(cls: Class<T>, id: string): Promise<number> {
-    try {
-      return await this.source.deleteById(cls, id);
-    } catch (e) {
-      throw this.rewriteError(cls, e);
-    }
+  async deleteById<T extends ModelCore>(cls: Class<T>, id: Id): Promise<void> {
+    return await this.source.deleteById(cls, id);
   }
 
-  async delete<T extends ModelCore>(o: T): Promise<number> {
-    return await this.deleteById(SchemaRegistry.getClass(o), o._id);
-  }
-
-  async deleteByQuery<T extends ModelCore>(cls: Class<T>, query: Object & { _id?: any } = {}): Promise<number> {
+  async deleteByQuery<T extends ModelCore>(cls: Class<T>, query: Query<T> = {}): Promise<number> {
     return await this.source.deleteByQuery(cls, query);
   }
 
   async save<T extends ModelCore>(o: T): Promise<T> {
     let cls = SchemaRegistry.getClass(o);
-    try {
-      o = await this.prePersist(o);
-      let res = await this.source.save<T>(cls, o);
-      return this.onRetrieve(cls, res);
-    } catch (e) {
-      throw this.rewriteError(cls, e);
-    }
+    o = await this.prePersist(o);
+    let res = await this.source.save<T>(o);
+    return this.postLoad(cls, res);
   }
 
   async saveAll<T extends ModelCore>(objs: T[]): Promise<T[]> {
     let cls = SchemaRegistry.getClass(objs[0]);
-    try {
-      objs = await SchemaValidator.validateAll(objs.map(o => o.preSave ? o.preSave() : o));
-      let res = await this.source.saveAll<T>(cls, objs);
-      return res.map(x => onRetrieve(cls, x));
-    } catch (e) {
-      throw this.rewriteError(cls, e);
-    }
+    objs = await Promise.all(objs.map(o => this.prePersist(o)));
+    let res = await this.source.saveAll<T>(objs);
+    return res.map(x => this.postLoad(cls, x));
   }
 
   async update<T extends ModelCore>(o: T): Promise<T> {
     let cls = SchemaRegistry.getClass(o);
-    try {
-      o = await this.prePersist(o);
-      let res = await this.source.update(cls, o);
-      return this.onRetrieve(cls, res);
-    } catch (e) {
-      throw this.rewriteError(cls, e);
-    }
+    o = await this.prePersist(o);
+    let res = await this.source.update(o);
+    return this.postLoad(cls, res);
   }
 
-  async updateView<T extends ModelCore>(o: Partial<T>, view: string): Promise<T> {
-    let cls = SchemaRegistry.getClass(o);
-    try {
-      o = await this.prePersist(o, view);
-      let partial = BindUtil.bindSchema(cls, {}, o, view);
-      let res = await this.source.partialUpdate<T>(partial);
-      return this.onRetrieve(cls, res);
-    } catch (e) {
-      throw this.rewriteError(cls, e);
-    }
+  async updateAll<T extends ModelCore>(objs: T[]): Promise<T[]> {
+    let cls = SchemaRegistry.getClass(objs[0]);
+    objs = await Promise.all(objs.map(o => this.prePersist(o)));
+    let res = await this.source.updateAll<T>(objs);
+    return res.map(x => this.postLoad(cls, x));
   }
 
-  async bulkProcess<T extends ModelCore>(named: Class<T>, state: { upsert?: T[], delete?: T[] }) {
-    let keys = this.getConfig(named).primaryUnique || ['_id'];
+  async updatePartial<T extends ModelCore>(o: Partial<T>, view: string): Promise<T> {
+    let cls = SchemaRegistry.getClass(o) as Class<T>;
+    o = await this.prePersist(o, view);
+    let partial = BindUtil.bindSchema(cls, {}, o, view);
+    let res = await this.source.updatePartial<T>(partial);
+    return this.postLoad(cls, res);
+  }
 
-    try {
-      return await this.source.bulkProcess(named, {
-        upsert: state.upsert,
-        delete: state.delete,
-        getId: (p: any) => new Map(keys.map(x => [x, x === '_id' ? new mongo.ObjectID(p[x]) : p[x]] as [string, any]))
-      });
-    } catch (e) {
-      throw this.rewriteError(named, e);
-    }
+  async bulkProcess<T extends ModelCore>(named: Class<T>, state: BulkState<T>) {
+    return this.source.bulkProcess(named, state);
   }
 }
--- module/model/src/service/source.ts
+++ module/model/src/service/source.ts
@@ -1,4 +1,4 @@
-import { ModelCore } from '../model';
+import { ModelCore, QueryOptions } from '../model';
 import { Class } from '@encore/schema';
 import { BulkState, BulkResponse } from '../model/bulk';
 import { Query } from '../model/query';
@@ -6,22 +6,25 @@ import { Query } from '../model/query';
 type Id = string | number;
 
 export abstract class ModelSource {
-  abstract prePersist<T>(model: T): T;
+  abstract getIdField(): string;
+  abstract getTypeField(): string;
+
+  abstract prePersist<T>(model: T): Promise<T>;
   abstract postLoad<T>(model: T): T;
 
-  abstract getById<T>(cls: Class<T>, id: Id): Promise<T>;
-  abstract deleteById<T>(cls: Class<T>, id: Id): Promise<void>;
   abstract save<T>(model: T): Promise<T>;
   abstract saveAll<T>(models: T[]): Promise<T[]>;
   abstract update<T>(model: T): Promise<T>;
   abstract updateAll<T>(model: T[]): Promise<T[]>;
-  abstract partialUpdate<T>(model: Partial<T>): Promise<T>;
-  abstract bulkProcess<T>(cls: Class<T>, state: BulkState<T>): Promise<BulkResponse>;
+  abstract updatePartial<T>(model: Partial<T>): Promise<T>;
 
+  abstract bulkProcess<T>(cls: Class<T>, state: BulkState<T>): Promise<BulkResponse>;
+  abstract getById<T>(cls: Class<T>, id: Id): Promise<T>;
+  abstract deleteById<T>(cls: Class<T>, id: Id): Promise<void>;
   abstract getIdsByQuery<T>(cls: Class<T>, query: Query<T>): Promise<Id>;
-  abstract getAllByQuery<T>(cls: Class<T>, query: Query<T>): Promise<T[]>;
+  abstract getAllByQuery<T>(cls: Class<T>, query: Query<T>, options?: QueryOptions): Promise<T[]>;
   abstract getCountByQuery<T>(cls: Class<T>, query: Query<T>): Promise<number>;
-  abstract getByQuery<T>(cls: Class<T>, query: Query<T>): Promise<T>;
+  abstract getByQuery<T>(cls: Class<T>, query: Query<T>, options?: QueryOptions, failOnMany?: boolean): Promise<T>;
   abstract deleteByQuery<T>(cls: Class<T>, query: Query<T>): Promise<number>;
   abstract partialUpdateByQuery<T>(cls: Class<T>, body: Partial<T>, query: Query<T>): Promise<number>;
 
