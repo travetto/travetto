7b4934940cd913f6bc5fd934066316222834696a
Timothy Soehnlin <tim@eaiti.com>
2017-09-06 08:49:42 -0400

    Reworking and cleaning

--- module/model-mongo/src/config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from '@encore/config';
-
-export default Configure.registerNamespace('mongo', {
-  hosts: 'localhost',
-  schema: 'app',
-  port: 27017,
-  options: {}
-});
--- module/model-mongo/src/model/index.ts
+++ module/model-mongo/src/model/index.ts
@@ -1,3 +1,1 @@
 export * from './base';
-export * from './bulkState';
-export * from './query';
