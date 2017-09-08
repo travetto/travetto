63cab9fb3fecf12b6c529ca2896bb15401b7121c
Timothy Soehnlin <tim@eaiti.com>
2017-09-08 08:35:56 -0400

    Fixing root

--- module/registry/src/service/root.ts
+++ module/registry/src/service/root.ts
@@ -1,9 +1,16 @@
 import { Registry } from './registry';
 import { CompilerClassSource } from './compiler-source';
+import { ChangeEvent } from './class-source';
 
 class $RootRegistry extends Registry {
   constructor() {
     super(new CompilerClassSource());
+  }
+
+  // Auto propagate
+  async onEvent(e: ChangeEvent) {
+    await super.onEvent(e);
+    this.emit(e);
   }
 }
 
