02b9092d6787602c0ffa5fd53e7fab99e1dd36ce
Timothy Soehnlin <tim@eaiti.com>
2017-09-10 15:01:34 -0400

    Fixing query model

--- module/model/src/model/query.ts
+++ module/model/src/model/query.ts
@@ -4,22 +4,28 @@ export type FieldComparableType = Date | number;
 export type FieldType = string | number | boolean | Date | object;
 export type FieldArrayType = FieldType[];
 
+export function isSubQuery(o: any): o is SubQuery {
+  return o.$lt || o.$lte || o.$gt || o.$gte || o.$eq || o.$ne || o.$in || o.$nin || o.$exists;
+}
+
+export type SubQuery = {
+  $lt: FieldComparableType;
+  $lte?: FieldComparableType;
+  $gt?: FieldComparableType;
+  $gte?: FieldComparableType;
+  $eq?: FieldType;
+  $ne?: FieldType;
+  $in?: FieldArrayType;
+  $nin?: FieldArrayType;
+  $exists?: boolean;
+}
+
 export type Query = {
-  [key: string]: (
-    {
-      $lt: FieldComparableType;
-      $lte?: FieldComparableType;
-      $gt?: FieldComparableType;
-      $gte?: FieldComparableType;
-      $eq?: FieldType;
-      $ne?: FieldType;
-      $in?: FieldArrayType;
-      $nin?: FieldArrayType;
-      $exists?: boolean;
-    } | RegExp | FieldType
-  );
-} | { $and: Query[] }
-  | { $or: Query[] };
+  [key: string]: FieldType | RegExp | SubQuery;
+} & {
+    $and?: Query[],
+    $or?: Query[]
+  };
 
 
 export interface QueryOptions {
