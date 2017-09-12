bbc6fdfb477c1046c01d48d42ec36496ee4368b8
Timothy Soehnlin <tim@eaiti.com>
2017-09-12 18:04:42 -0400

    Listen if main entry point

--- module/registry/bootstrap.js
+++ module/registry/bootstrap.js
@@ -2,3 +2,7 @@ require('@encore2/config/bootstrap');
 require('@encore2/compiler/bootstrap');
 
 module.exports = require('./src/service/root').RootRegistry;
+
+if (require.resolve(process.argv[1]) === __filename) {
+  module.exports.init();
+}
