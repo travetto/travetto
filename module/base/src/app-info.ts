ee736d38039b67c56d6461011176785195f0fe60
Timothy Soehnlin <tim@eaiti.com>
2017-09-13 09:06:04 -0400

    Providing context for app

--- module/base/src/app-info.ts
+++ module/base/src/app-info.ts
@@ -6,6 +6,7 @@ export const AppInfo = {
   VERSION: pkg.version,
   NAME: pkg.name,
   SIMPLE_NAME: pkg.name.replace(/[@]/g, '').replace(/[\/]/g, '_'),
+  PACKAGE: pkg.name.split('/')[0],
   LICENSE: pkg.license,
   AUTHOR: pkg.author,
   DESCRIPTION: pkg.description
