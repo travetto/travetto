cf9199e8af8cde6d0f757cf733cfe8b732748db9
Timothy Soehnlin <tim@eaiti.com>
2017-09-15 22:53:08 -0400

    Prepping bootstrap

--- module/base/bootstrap.js
+++ module/base/bootstrap.js
@@ -3,7 +3,8 @@ const ts = require('typescript');
 
 //Simple bootstrap to load compiler
 require.extensions['.ts'] = function load(m, tsf) {
-  return m._compile(ts.transpile(fs.readFileSync(tsf, 'utf-8')), tsf.replace(/\.ts$/, '.js'));
+  const content = ts.transpile(fs.readFileSync(tsf, 'utf-8'));
+  return m._compile(content, tsf.replace(/\.ts$/, '.js'));
 };
 
 require('./src/stacktrace'); //Prep stack traces
