30b3e926f51bb2437eb00a1994364449ecb3aad6
Timothy Soehnlin <tim@eaiti.com>
2017-09-17 19:49:22 -0400

    Supporting debug

--- module/base/src/env.ts
+++ module/base/src/env.ts
@@ -13,7 +13,12 @@ const prod = is('prod') || is('production');
 const test = is('test') || is('testing');
 const dev = !prod && !test;
 const watch = (dev && !('NO_WATCH' in e)) || 'WATCH' in e;
+const debug = 'DEBUG' in e && e.DEBUG;
 
-export const AppEnv = { prod, dev, test, is, watch, all: envs };
+export const AppEnv = { prod, dev, test, is, watch, all: envs, debug };
 
 process.env.NODE_ENV = prod ? 'production' : 'development';
+
+if (AppEnv.debug) {
+  console.debug = console.log;
+}
