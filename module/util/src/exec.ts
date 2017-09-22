d9a792dcb8c85a6a7c5e919b095fabf40d3e2385
Timothy Soehnlin <tim@eaiti.com>
2017-09-22 06:58:45 -0400

    Being able to suppress stdout collection for long running pieces

--- module/util/src/exec.ts
+++ module/util/src/exec.ts
@@ -3,6 +3,7 @@ import * as child_process from 'child_process';
 export type ExecOptions = child_process.SpawnOptions & {
   timeout?: number;
   exposeProcess?: boolean;
+  quiet?: boolean;
   kill?: (proc: child_process.ChildProcess) => Promise<void>;
 };
 export interface ExecResult {
@@ -49,8 +50,10 @@ export function exec(cmd: string, options: ExecOptions = {}): Promise<ExecResult
       }
     };
 
-    p.stdout.on('data', (d: string) => stdout += d + '\n');
-    p.stderr.on('data', (d: string) => stderr += d + '\n');
+    if (!options.quiet) {
+      p.stdout.on('data', (d: string) => stdout += d + '\n');
+      p.stderr.on('data', (d: string) => stderr += d + '\n');
+    }
     p.on('error', (err: Error) =>
       finish({ code: 1, stdout, stderr, message: err.message, valid: false })
     );
@@ -65,8 +68,6 @@ export function exec(cmd: string, options: ExecOptions = {}): Promise<ExecResult
       }
       finish({ code: 1, stderr, stdout, message: `Execution timed out after: ${timeout} ms`, valid: false });
     }, timeout);
-
-    timer.unref();
   });
   if (options.exposeProcess) {
     return [prom, p];
