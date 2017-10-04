91458c45d70062ae7422bc1d4e4ca6c9d8430268
Timothy Soehnlin <tim@eaiti.com>
2017-10-03 20:13:11 -0400

    Cloning data to prevent reusage issues

--- module/context/src/decorator/context.ts
+++ module/context/src/decorator/context.ts
@@ -1,12 +1,15 @@
 import { Context } from '../service';
 
-export function WithContext<T extends { context: Context }>(data: any) {
+export function WithContext<T extends { context: Context }>(data?: any) {
   return function (target: T, prop: string, descriptor: TypedPropertyDescriptor<(...args: any[]) => Promise<any>>) {
     let og = descriptor.value;
     descriptor.value = function (...args: any[]) {
       return new Promise((resolve, reject) => {
         target.context.namespace.run(() => {
           try {
+            if (data) {
+              target.context.set(JSON.parse(JSON.stringify(data))); // Clone data
+            }
             resolve(og.apply(this, args));
           } catch (e) {
             reject(e);
