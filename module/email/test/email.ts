99a09546c72fa93fe0c31e102fcbc903a5764be5
Timothy Soehnlin <tim@eaiti.com>
2017-10-04 00:17:31 -0400

    Fixing tests

--- module/email/test/email.ts
+++ module/email/test/email.ts
@@ -1,32 +1,42 @@
-import 'mocha';
-
+import { Test, Suite, BeforeAll } from '@travetto/test';
 import { EmailService } from '../src';
-import { expect } from 'chai';
-import { Registry } from '@travetto/di';
+import { DependencyRegistry } from '@travetto/di';
+import { RootRegistry } from '@travetto/registry';
 
-describe('Emails', () => {
-  it('Should template properly', async () => {
-    let instance = await Registry.getInstance(EmailService);
+import * as assert from 'assert';
+
+@Suite('Emails')
+class EmailSuite {
+
+  @BeforeAll()
+  async init() {
+    await RootRegistry.init();
+  }
+
+  @Test('Should template properly')
+  async templating() {
+    let instance = await DependencyRegistry.getInstance(EmailService);
 
     let out = await instance.template(`<row>
-      <columns large="{{left}}">Bob</columns>
-      <columns large="{{right}}"></columns>
-    </row>`, { left: 6, right: 6 });
+          <columns large="{{left}}">Bob</columns>
+          <columns large="{{right}}"></columns>
+        </row>`, { left: 6, right: 6 });
+    assert(out.includes('>Bob</th>'))
+  }
 
-    expect(out).to.include('>Bob</th>');
-  });
-
-  it('Send email', async () => {
-    let instance = await Registry.getInstance(EmailService);
+  @Test('Send email')
+  async sendEmail() {
+    let instance = await DependencyRegistry.getInstance(EmailService);
 
     await instance.sendEmail({
       to: 'tim@eaiti.com',
       subject: 'Simple Test',
       body: `<row>
-        <columns large="6">{{name}}</columns>
-        <columns large="6">{{price}}</columns>
-      </row>`,
+            <columns large="6">{{name}}</columns>
+            <columns large="6">{{price}}</columns>
+          </row>`,
       context: { name: 'Tim', price: '100' }
     });
-  });
-});
+
+  }
+}
