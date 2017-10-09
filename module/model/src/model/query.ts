d98450eaa795e44e1737dfd651678c73f2f1b784
Timothy Soehnlin <tim@eaiti.com>
2017-10-09 00:17:39 -0400

    Working on query language

--- module/model/src/model/query.ts
+++ module/model/src/model/query.ts
@@ -6,34 +6,44 @@ export type FieldArrayType = FieldType[];
 
 export type Point = [number, number];
 
-export type FieldQuery =
-  { lt: FieldComparableType; } |
-  { lte: FieldComparableType; } |
-  { gt: FieldComparableType; } |
-  { gte: FieldComparableType; } |
-  { eq: FieldType; } |
-  { ne: FieldType; } |
-  { in: FieldArrayType; } |
-  { nin: FieldArrayType; } |
-  { all: FieldArrayType; } |
-  { exists: boolean; } |
-  { regex: RegExp; } |
-  { geoWithin: Point[] } |
-  { geoIntersects: Point[] } |
-  FieldType;
+export type FieldQuery = {
+  lt?: FieldComparableType;
+  lte?: FieldComparableType;
+  gt?: FieldComparableType;
+  gte?: FieldComparableType;
+  eq?: FieldType;
+  ne?: FieldType;
+  in?: FieldArrayType;
+  nin?: FieldArrayType;
+  all?: FieldArrayType;
+  exists?: boolean;
+  regex?: RegExp;
+  geoWithin?: Point[];
+  geoIntersects?: Point[];
+}
 
-export type Query =
-  { and: Query[]; } |
-  { or: Query[]; } |
-  { not: Query; } |
-  { [key: string]: FieldQuery; };
+export type MatchQuery = {
+  [key: string]: FieldQuery | FieldType;
+};
+
+export type GroupingQuery =
+  { and: (GroupingQuery | MatchQuery)[]; } |
+  { or: (GroupingQuery | MatchQuery)[]; } |
+  { not: (GroupingQuery | MatchQuery); };
+
+export type Query = GroupingQuery | MatchQuery;
+
+export function isQuery(o: Query): o is GroupingQuery {
+  return 'and' in o || 'or' in o || 'not' in o;
+}
+
+export function isFieldType(o: any): o is FieldType {
+  let type = typeof o;
+  return o === 'number' || o === 'string' || o === 'boolean' || o instanceof Date;
+}
 
 export interface QueryOptions {
   sort?: SortOptions;
   limit?: number;
   offset?: number;
-}
-
-let q: Query = {
-  'name.first': 'orange'
 }
