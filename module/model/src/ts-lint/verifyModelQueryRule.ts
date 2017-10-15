5bf6a19599c14c27d666cc46e575585a3f948ce2
Timothy Soehnlin <tim@eaiti.com>
2017-10-15 16:51:11 -0400

    Type checking query structure

--- module/model/src/ts-lint/verifyModelQueryRule.ts
+++ module/model/src/ts-lint/verifyModelQueryRule.ts
@@ -1,10 +1,6 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
 
-const QueryTypes = [
-  'Query', 'ModelQuery', 'PageableModelQuery'
-].reduce((acc, v) => { acc[v] = true; return acc; }, {} as { [key: string]: boolean });
-
 export class Rule extends Lint.Rules.TypedRule {
 
   public static metadata: Lint.IRuleMetadata = {
@@ -19,66 +15,105 @@ export class Rule extends Lint.Rules.TypedRule {
   };
 
   public applyWithProgram(sourceFile: ts.SourceFile, program: ts.Program): Lint.RuleFailure[] {
-    return this.applyWithFunction(sourceFile, walk, undefined, program.getTypeChecker());
+    return this.applyWithFunction(sourceFile, (ctx: Lint.WalkContext<void>, tc: ts.TypeChecker) => {
+      return new QueryHandler(ctx, tc).walk();
+    }, undefined, program.getTypeChecker());
   }
 }
 
-function walk(ctx: Lint.WalkContext<void>, tc: ts.TypeChecker) {
-  return ts.forEachChild(ctx.sourceFile, function cb(node: ts.Node): void {
+class QueryHandler {
+
+  static QUERY_TYPES = [
+    'Query', 'ModelQuery', 'PageableModelQuery'
+  ].reduce((acc, v) => { acc[v] = true; return acc; }, {} as { [key: string]: boolean });
+
+  cache: { [key: string]: { [key: string]: ts.Symbol } } = {};
+
+  constructor(private ctx: Lint.WalkContext<void>, private tc: ts.TypeChecker) {
+    this.visitNode = this.visitNode.bind(this);
+  }
+
+
+  getMembersByType(type: ts.Type) {
+    let key = type.aliasSymbol!.escapedName.toString();
+    if (!(key in this.cache)) {
+      let members = this.tc.getPropertiesOfType(type).reduce((acc, v) => {
+        acc[v.escapedName.toString()] = v; return acc;
+      }, {} as { [key: string]: ts.Symbol });
+      this.cache[key] = members;
+    }
+    return this.cache[key];
+  }
+
+  walk() {
+    this.visitNode(this.ctx.sourceFile);
+  }
+
+  visitNode(node: ts.Node): void {
     // sHandle direct invocation
-    if (ts.isPropertyDeclaration(node)) {
-      processQuery(ctx, tc, tc.getTypeAtLocation(node));
+    if (ts.isPropertyDeclaration(node) && node.initializer) {
+      this.processQuery(this.tc.getTypeAtLocation(node), node.initializer);
     } else if (ts.isCallExpression(node)) {
-      let sig = tc.getResolvedSignature(node);
+      let sig = this.tc.getResolvedSignature(node);
       if (sig) {
+        let i = 0;
         for (let n of sig.parameters) {
-          processQuery(ctx, tc, (n as any).type);
+          this.processQuery((n as any).type, node.arguments[i]);
+          i++;
         }
       }
-    } else if (ts.isVariableDeclaration(node)) {
-      processQuery(ctx, tc, tc.getTypeAtLocation(node));
+    } else if (ts.isVariableDeclaration(node) && node.initializer) {
+      this.processQuery(this.tc.getTypeAtLocation(node), node.initializer);
     }
-    return ts.forEachChild(node, cb);
-  });
-}
-
-let cache: { [key: string]: { [key: string]: ts.Symbol } } = {};
-
-function getMembersByType(tc: ts.TypeChecker, type: ts.Type) {
-  let key = type.aliasSymbol!.escapedName.toString();
-  if (!(key in cache)) {
-    let members = tc.getPropertiesOfType(type).reduce((acc, v) => {
-      acc[v.escapedName.toString()] = v; return acc;
-    }, {} as { [key: string]: ts.Symbol });
-    cache[key] = members;
+    return ts.forEachChild(node, this.visitNode);
   }
-  return cache[key];
-}
 
-function processQuery(ctx: Lint.WalkContext<void>, tc: ts.TypeChecker, queryType: ts.Type) {
-  if (queryType && queryType.aliasSymbol) {
-    let queryName = `${queryType.aliasSymbol.escapedName}`;
 
-    if (QueryTypes[queryName] && queryType.aliasTypeArguments && queryType.aliasTypeArguments.length) {
-      let modelType = queryType.aliasTypeArguments[0];
+  processSelectClause(model: ts.Type, member: ts.Symbol) {
 
-      console.log(queryName, tc.typeToString(modelType));
-      let members = getMembersByType(tc, queryType)
+  }
 
-      if (members) {
-        if ('selct' in members) {
-          console.log('Has Select');
-        }
-        if ('where' in members) {
-          console.log('Has Where');
-        }
-        if ('groupBy' in members) {
-          console.log('Has GroupBy');
-        }
-        if ('sort' in members) {
-          console.log('Has SortBy');
+  processWhereClause(model: ts.Type, member: ts.Symbol) {
+
+  }
+
+  processGroupByClause(model: ts.Type, member: ts.Symbol) {
+
+  }
+
+  processSortClause(model: ts.Type, member: ts.Symbol) {
+
+  }
+
+  processQuery(queryType: ts.Type, passedNode: ts.Node) {
+    if (queryType && queryType.aliasSymbol) {
+      let queryName = `${queryType.aliasSymbol.escapedName}`;
+
+      if (QueryHandler.QUERY_TYPES[queryName] && queryType.aliasTypeArguments && queryType.aliasTypeArguments.length) {
+        let modelType = queryType.aliasTypeArguments[0];
+
+        console.log(queryName, this.tc.typeToString(modelType));
+        let members = this.getMembersByType(queryType)
+        if (members) {
+          let passedType = this.tc.getTypeAtLocation(passedNode);
+
+          if ('select' in members) {
+            this.processSelectClause(modelType, (passedType as any).members.get('select'))
+          }
+          if ('where' in members) {
+            this.processWhereClause(modelType, (passedType as any).members.get('where'))
+          }
+          if ('groupBy' in members) {
+            this.processGroupByClause(modelType, (passedType as any).members.get('groupBy'))
+          }
+          if ('sort' in members) {
+            this.processSortClause(modelType, (passedType as any).members.get('sort'))
+          }
         }
       }
     }
   }
+
 }
+
+
