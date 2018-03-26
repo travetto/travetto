cc3b34b0466d08ac03d2da704107b126bbf99c82
Timothy Soehnlin <tim@eaiti.com>
2018-03-25 22:47:23 -0400

    Working on tslint resolution

--- module/context/src/decorator/context.ts
+++ module/context/src/decorator/context.ts
@@ -4,7 +4,7 @@ export function WithContext<T extends { context: Context }>(data?: any) {
   return function (target: T, prop: string, descriptor: TypedPropertyDescriptor<(...args: any[]) => Promise<any>>) {
     const og = descriptor.value!;
     descriptor.value = function (...args: any[]) {
-      const self = this as T;
+      const self = this as T; // tslint:disable-line no-invalid-this
       return new Promise((resolve, reject) => {
         self.context.namespace.run(() => {
           try {
