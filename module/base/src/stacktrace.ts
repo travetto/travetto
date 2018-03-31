4f9127592423fbe4f66f9a3a69768d20927360c9
Timothy Soehnlin <tim@eaiti.com>
2018-03-31 14:20:01 -0400

    Working on stack traces

--- module/base/src/stacktrace.ts
+++ module/base/src/stacktrace.ts
@@ -1,14 +1,8 @@
 import { AppEnv } from './env';
 
-const FILTERS = [
-  __filename.replace(/\.js$/, ''),
-  'timers.js',
-  'async_hooks',
-  'module.js',
-  '(native)',
-  '<anonymous>',
-  'source-map-support.js'
-];
+const FILTERS: string[] = [];
+
+let filterRegex: RegExp = /./g;
 
 let registered = false;
 
@@ -20,19 +14,26 @@ export function initStackHandler() {
     registered = true;
   }
 
+  Error.stackTraceLimit = 500;
+
   require('trace');
   const chain = require('stack-chain');
+
+  addStackFilters(
+    __filename.replace(/\.js$/, ''),
+    'timers.js',
+    'async_hooks',
+    'module.js',
+    '(native)',
+    '<anonymous>',
+    'source-map-support.js'
+  );
 
   chain.filter.attach(function (error: Error, frames: any[]) {
     // Filter out traces related to this file
     const rewrite = frames.filter(function (callSite) {
       const name: string = callSite.getFileName() || '';
-      for (const f of FILTERS) {
-        if (name.includes(f)) {
-          return false;
-        }
-      }
-      return true;
+      return !filterRegex.test(name);
     });
 
     return rewrite;
@@ -42,5 +43,6 @@ export function initStackHandler() {
 export function addStackFilters(...names: string[]) {
   if (FILTERS) {
     FILTERS.push(...names);
+    filterRegex = new RegExp(`(${names.join('|')})`);
   }
 }
