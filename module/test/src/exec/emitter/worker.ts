00380c0b3daeac8199bcc9f8d65e70e352e4d24f
Timothy Soehnlin <tim@eaiti.com>
2018-04-03 00:58:05 -0400

    Using new assertion support

--- module/test/src/exec/emitter/worker.ts
+++ module/test/src/exec/emitter/worker.ts
@@ -7,13 +7,12 @@ export class WorkerEmitter implements TestEmitter {
   emit(event: ListenEvent) {
     if (event.phase === 'after') {
       if (event.type === 'test') {
-        for (const assrt of event.test.assertions) {
-          if (assrt.error) {
-            assrt.error = serialize(assrt.error);
-          }
-        }
         if (event.test.error) {
           event.test.error = serialize(event.test.error);
+        }
+      } else if (event.type === 'assertion') {
+        if (event.assertion.error) {
+          event.assertion.error = serialize(event.assertion.error);
         }
       }
     }
