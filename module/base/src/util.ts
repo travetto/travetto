f184b01def2a4fb44e7c2cd5ec61cc76c66f5ec4
Timothy Soehnlin <tim@eaiti.com>
2018-04-20 11:39:57 -0400

    Supporting common utils

---
+++ module/base/src/util.ts
@@ -0,0 +1,50 @@
+export function isPrimitive(el: any) {
+  const type = typeof el;
+  return (type === 'string' || type === 'boolean' || type === 'number' || el instanceof RegExp);
+}
+
+export function isPlainObject(obj: any) {
+  return typeof obj === 'object' // separate from primitives
+    && obj !== null         // is obvious
+    && obj.constructor === Object // separate instances (Array, DOM, ...)
+    && Object.prototype.toString.call(obj) === '[object Object]'; // separate build-in like Math
+}
+
+export function deepMerge<T extends any, U extends any>(a: T, b: U): T & U {
+  const isEmptyA = a === undefined || a === null;
+  const isEmptyB = b === undefined || b === null;
+  const isArrA = Array.isArray(a);
+  const isArrB = Array.isArray(b);
+
+  if (!isEmptyB) {
+    if (isPrimitive(b)) {
+      if (isEmptyA || isPrimitive(a)) {
+        return b as (T & U);
+      } else {
+        throw new Error(`Cannot merge primitive ${b} with ${a}`);
+      }
+    } else if (isArrB) {
+      const bArr = b as any as any[];
+      if (a === undefined) {
+        return bArr.slice(0) as any as T & U;
+      } else if (isArrA) {
+        const aArr = (a as any as any[]).slice(0);
+        for (let i = 0; i < bArr.length; i++) {
+          aArr[i] = deepMerge(aArr[i], bArr[i]);
+        }
+        return aArr as any as T & U;
+      } else {
+        throw new Error(`Cannot merge ${a} with ${b}`);
+      }
+    } else {
+      if (isEmptyA || isArrA || isPrimitive(a)) {
+        throw new Error(`Cannot merge ${b} onto ${a}`);
+      }
+      for (const key of Object.keys(b)) {
+        a[key] = deepMerge(a[key], b[key]);
+      }
+      return a as (T & U);
+    }
+  }
+  return a as (T & U);
+}
