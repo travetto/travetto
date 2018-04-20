472e646f4dfd28f5d364d1d746860e66fde7108a
Timothy Soehnlin <tim@eaiti.com>
2018-04-20 11:41:59 -0400

    Moving util to baes

--- module/registry/test/index.js
+++ module/registry/test/index.js
@@ -1,6 +1,5 @@
 require('@travetto/base/bootstrap').run()
   .then(x => {
-    //require('./simpleReg');
-    //require('./simple');
-    require('./merge');
+    require('./simpleReg');
+    require('./simple');
   });
