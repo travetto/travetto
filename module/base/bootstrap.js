30d5aba8cdc8d6d82c122e639096797d9c577217
Timothy Soehnlin <tim@eaiti.com>
2018-04-22 23:10:32 -0400

    Better default handling of bootstrap, allow for override as needed

--- module/base/bootstrap.js
+++ module/base/bootstrap.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const ts = require('typescript');
 
 //Simple bootstrap to load compiler
+const json = ts.readJsonConfigFile(`./tsconfig.json`, ts.sys.readFile);
+const opts = ts.parseJsonSourceFileConfigFileContent(json, ts.sys, process.cwd()).options;
+
 require.extensions['.ts'] = function load(m, tsf) {
-  const content = ts.transpile(fs.readFileSync(tsf, 'utf-8'));
+  const content = ts.transpile(fs.readFileSync(tsf, 'utf-8'), opts);
   return m._compile(content, tsf.replace(/\.ts$/, '.js'));
 };
 
