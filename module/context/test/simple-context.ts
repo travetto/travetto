8091a2601823ed6bfe5376391affcf02db00f734
Timothy Soehnlin <tim@eaiti.com>
2018-04-23 21:48:22 -0400

    Cleaning up packages/tests

--- module/context/test/simple-context.ts
+++ module/context/test/simple-context.ts
@@ -24,5 +24,13 @@ class VerifyContext {
   async loadContext() {
     const svc = await DependencyRegistry.getInstance(TestService)
     assert(svc.context !== null);
+    await new Promise(resolve => {
+      svc.context.namespace.run(async () => {
+        svc.context.set({ user: 'bob' });
+        await new Promise(resolve2 => setTimeout(resolve2, 1));
+        assert(svc.context.get().user === 'bob');
+        resolve();
+      })
+    })
   }
 }
