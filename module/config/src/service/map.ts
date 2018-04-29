cc29cd5698bc180398ce157645165c10fd4c521b
Timothy Soehnlin <tim@eaiti.com>
2018-04-29 18:55:58 -0400

    Updating config handling

--- module/config/src/service/map.ts
+++ module/config/src/service/map.ts
@@ -1,56 +1,76 @@
-import { deepMerge, isPlainObject } from '@travetto/base';
+import { deepMerge, isPlainObject, isSimple } from '@travetto/base';
 
-type Nested = { [key: string]: string | number | boolean | null | Nested };
+type Prim = number | string | boolean | null;
 
-const NULL = Symbol('NULL');
+type Nested = { [key: string]: Prim | Nested | Nested[] };
+
+function coerce(a: any, val: any): any {
+  if (a === 'null' && typeof val !== 'string') {
+    return null;
+  }
+
+  if (val === null || val === undefined) {
+    return a;
+  }
+
+  if (isSimple(val)) {
+    switch (typeof val) {
+      case 'string': return `${a}`;
+      case 'number': return `${a}`.indexOf('.') >= 0 ? parseFloat(`${a}`) : parseInt(`${a}`, 10);
+      case 'boolean': return (typeof a === 'string' && a === 'true') || !!a;
+      default:
+        throw new Error(`Unknown type ${typeof val}`);
+    }
+  }
+
+  if (Array.isArray(val)) {
+    return `${a}`.split(',').map(x => x.trim()).map(x => coerce(x, val[0]));
+  }
+}
 
 export class ConfigMap {
 
   // Lowered, and flattened
   private storage: Nested = {};
 
-  // Will store output when done
-  private _finalized: Nested;
-
   putAll(data: Nested) {
-    delete this._finalized;
     deepMerge(this.storage, data, 'coerce');
   }
 
-  putAllFlattened(data: { [key: string]: number | string | boolean | null }) {
-    const topLevel = new RegExp(`^(${Object.keys(this.storage).join('|')})`, 'i');
-    const validPaths = Object.keys(data).filter(x => topLevel.test(x));
-
-    for (const path of validPaths) {
-      // Handle flattening
+  putAllFlattened(data: { [key: string]: Prim }) {
+    for (const path of Object.keys(data)) {
+      this.putFlattened(path, data[path]);
     }
   }
 
-  private static dropNulls(o: any) {
-    if (isPlainObject(o)) {
-      for (const k of Object.keys(o)) {
-        if (o[k] === NULL) {
-          delete o[k];
-        } else {
-          this.dropNulls(o[k]);
-        }
+  putFlattened(path: string, value: Prim) {
+    const parts = path.split('_');
+    const key = parts.pop()!;
+    let data = this.storage;
+
+    while (parts.length) {
+      const part = parts.shift()!;
+      const match = new RegExp(part, 'i');
+      const next = Object.keys(data).find(x => match.test(x));
+      if (!next) {
+        return false;
+      } else {
+        data = data[next] as Nested;
       }
-    } else if (Array.isArray(o)) {
-      o = o.map(this.dropNulls).filter((x: any) => x !== NULL);
     }
-    return o;
-  }
 
-  private get finalized() {
-    if (!this._finalized) {
-      this._finalized = ConfigMap.dropNulls(this.storage);
+    if (!data) {
+      return false;
     }
-    return this._finalized;
+
+    data[key] = coerce(value, data[key]);
+
+    return true;
   }
 
   bindTo(obj: any, key: string) {
     const keys = key.split('.');
-    let sub: any = this.finalized;
+    let sub: any = this.storage;
     while (keys.length && sub[keys[0]]) {
       sub = sub[keys.shift()!];
     }
@@ -63,6 +83,6 @@ export class ConfigMap {
   }
 
   toJSON() {
-    return JSON.stringify(this.finalized, null, 2);
+    return JSON.stringify(this.storage, null, 2);
   }
 }
