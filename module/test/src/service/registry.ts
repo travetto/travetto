773629da56dd3dbae98bddc517411abc25ec73d2
Timothy Soehnlin <tim@eaiti.com>
2018-05-07 19:04:28 -0400

    Allowing for inheritance of before/after/each/all

--- module/test/src/service/registry.ts
+++ module/test/src/service/registry.ts
@@ -32,6 +32,14 @@ class $TestRegistry extends MetadataRegistry<SuiteConfig, TestConfig> {
   onInstallFinalize<T>(cls: Class<T>): SuiteConfig {
     const config = this.getOrCreatePending(cls) as SuiteConfig;
     const tests = this.pendingMethods.get(cls.__id)!.values();
+    const parent = this.getParentClass(cls);
+    if (parent && this.has(parent)) {
+      const pconf = this.get(parent);
+      config.afterAll.push(...pconf.afterAll);
+      config.beforeAll.push(...pconf.beforeAll);
+      config.afterEach.push(...pconf.afterEach);
+      config.beforeEach.push(...pconf.beforeEach);
+    }
     config.instance = new config.class();
     config.tests = Array.from(tests) as TestConfig[];
     if (!config.description) {
