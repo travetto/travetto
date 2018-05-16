c4adf6ec64a3602d252f854da8d54852f8aca92f
Timothy Soehnlin <tim@eaiti.com>
2018-05-15 20:24:39 -0400

    Moving phase

--- module/cache/phase/phase.bootstrap.ts
+++ module/cache/phase/phase.bootstrap.ts
@@ -1,7 +1,7 @@
 export const init = {
   priority: 100,
   action: () => {
-    const { CacheManager } = require('./src/service/cache');
+    const { CacheManager } = require('../src/service/cache');
     const { Shutdown } = require('@travetto/base');
     Shutdown.onShutdown('Cache Manager', CacheManager.cleanup.bind(CacheManager));
   }
