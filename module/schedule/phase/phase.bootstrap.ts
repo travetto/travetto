b70c15d17639f36f106417e47161eabcdc28c514
Timothy Soehnlin <tim@eaiti.com>
2018-05-15 20:24:01 -0400

    Moving bootstrap

--- module/schedule/phase/phase.bootstrap.ts
+++ module/schedule/phase/phase.bootstrap.ts
@@ -2,7 +2,7 @@ export const init = {
   priority: 100,
   action: () => {
     const { Shutdown } = require('@travetto/base');
-    const { Scheduler } = require('./src/service/schedule');
+    const { Scheduler } = require('../src/service/schedule');
     Shutdown.onShutdown('scheule.kill', () => Scheduler.kill());
   }
 }
--- module/schedule/test/schedule.ts
+++ module/schedule/test/schedule.ts
@@ -20,5 +20,4 @@ class Scheduling {
 
     assert(val === 3);
   }
-
 }
