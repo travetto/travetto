be687356952ce407ce2a1fa364f07c6999e2cb65
Timothy Soehnlin <tim@eaiti.com>
2018-05-18 00:27:02 -0400

    Changing names

--- module/cache/phase/phase.bootstrap.ts
+++
@@ -1,8 +0,0 @@
-export const init = {
-  priority: 100,
-  action: () => {
-    const { CacheManager } = require('../src/service/cache');
-    const { Shutdown } = require('@travetto/base');
-    Shutdown.onShutdown('Cache Manager', CacheManager.cleanup.bind(CacheManager));
-  }
-}
