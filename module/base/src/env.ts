9864dd321b01791f180ad536197d6c7b9d6735be
Timothy Soehnlin <tim@eaiti.com>
2018-05-24 07:59:16 -0400

    Checking

--- module/base/src/env.ts
+++ module/base/src/env.ts
@@ -1,3 +1,5 @@
+import { execSync } from 'child_process';
+
 const e = process.env;
 
 const envs = [
@@ -14,5 +16,11 @@ const test = is('test') || is('testing');
 const dev = !prod && !test;
 const watch = (dev && !('NO_WATCH' in e)) || 'WATCH' in e;
 const debug = 'DEBUG' in e && !!e.DEBUG;
+let docker = !('NO_DOCKER' in e && !!e.NO_DOCKER);
+if (docker) { // Check for docker existance
+  try {
+    docker = execSync('docker ps') && true;
+  } catch { };
+}
 
-export const AppEnv = { prod, dev, test, is, watch, all: envs, debug };
+export const AppEnv = { prod, dev, test, is, watch, all: envs, debug, docker };
