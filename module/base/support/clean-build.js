4c287859f93457420484b53090a8eb371d3f3729
Timothy Soehnlin <tim@eaiti.com>
2018-05-24 09:53:07 -0400

    Supporting npm oddities

--- module/base/support/clean-build.js
+++ module/base/support/clean-build.js
@@ -4,7 +4,7 @@ require('../bin/travetto').run().then(async () => {
   const { rimraf } = require('../src/scan-fs');
 
   try {
-    await rimraf(`${process.cwd()}/build`)
+    await rimraf(`${process.env.INIT_CWD || process.cwd()}/build`)
     console.log('Deleted build/');
   } catch (e) {
     console.log('Failed in deleting');
