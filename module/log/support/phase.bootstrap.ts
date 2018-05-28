b45f6fb6572f5bf62e26823747c7adf601b2b87e
Timothy Soehnlin <tim@eaiti.com>
2018-05-28 12:40:43 -0400

    Allowing for global error handling

--- module/log/support/phase.bootstrap.ts
+++ module/log/support/phase.bootstrap.ts
@@ -1,4 +1,8 @@
 export const init = {
   priority: 0,
-  action: () => require('../src/service').Logger._init()
+  action: () => {
+    const Logger = require('../src/service').Logger;
+    Logger._init();
+    require('@travetto/base/src/env').AppEnv.error = (...args: any[]) => Logger.log('error', args);
+  }
 };
