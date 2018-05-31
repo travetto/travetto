a14c844ca41630aed1ef310200a2c573c0ca650d
Timothy Soehnlin <tim@eaiti.com>
2018-05-31 08:24:38 -0400

    Working into schema directly

--- module/schema/src/decorator/schema.ts
+++ module/schema/src/decorator/schema.ts
@@ -3,13 +3,13 @@ import { SchemaRegistry, ValidatorFn } from '../service';
 import { Class } from '@travetto/registry';
 import { BindUtil } from '../util';
 
-type DeepPartial<T> = {
+export type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P]
 };
 
 export interface ClassWithSchema<T> {
   new(...args: any[]): T;
-  from(data: DeepPartial<T & { [key: string]: any }>, view?: string): T;
+  from<U>(this: Class<U>, data: DeepPartial<U & { [key: string]: any }>, view?: string): U;
 }
 
 export function Schema(auto: boolean = true): ClassDecorator {
@@ -20,7 +20,7 @@ export function Schema(auto: boolean = true): ClassDecorator {
     if (!res.from) {
       res.from = function (data: any, view: any) {
         // tslint:disable-next-line:no-invalid-this
-        return BindUtil.bindSchema(this as Class<T>, new this(), data, view);
+        return BindUtil.bindSchema(this, new this(), data, view);
       };
     }
 
