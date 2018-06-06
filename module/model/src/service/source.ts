917aa49333d0935836d3957925628ef0eb7a2fb6
Timothy Soehnlin <tim@eaiti.com>
2018-06-05 23:47:48 -0400

    Working through schema

--- module/model/src/service/source.ts
+++ module/model/src/service/source.ts
@@ -1,9 +1,9 @@
 import { Class, ChangeEvent } from '@travetto/registry';
+import { SchemaChangeEvent } from '@travetto/schema';
 
 import { ModelCore, Query, PageableModelQuery } from '../model';
 import { BulkState, BulkResponse } from '../model/bulk';
 import { ModelQuery } from '../model/query';
-import { SchemaChangeEvent } from './registry';
 
 export abstract class ModelSource {
   onChange?<T extends ModelCore>(e: ChangeEvent<Class<T>>): void;
