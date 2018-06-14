cb331fcc64682ff9d7a5ef56beae0ae983d56a7c
Timothy Soehnlin <tim@eaiti.com>
2018-06-13 23:57:12 -0400

    Fixing waiting

--- module/model-elasticsearch/support/phase.test.ts
+++ module/model-elasticsearch/support/phase.test.ts
@@ -1,29 +1,37 @@
 export const init = {
   priority: 0,
   action: async () => {
-    const { request } = require('@travetto/util');
+    const { DockerContainer } = require('@travetto/exec/src/docker');
 
-    async function waitForUrl(url: string, timeout: number) {
-      const start = Date.now();
-      while ((Date.now() - start) < timeout) {
-        try {
-          await request({ url });
-          return;
-        } catch (e) {
-          await new Promise(res => setTimeout(res, 100));
+    const defPort = process.env.MODEL_ELASTICSEARCH_PORT || 9200;
+
+    try {
+      await DockerContainer.waitForPort(defPort, 10);
+      process.env.MODEL_MONGO_NAMESPACE = `test_${Math.trunc(Math.random() * 10000)}`; // Randomize schema
+    } catch (e) {
+      const { request } = require('@travetto/util');
+
+      async function waitForUrl(url: string, timeout: number) {
+        const start = Date.now();
+        while ((Date.now() - start) < timeout) {
+          try {
+            await request({ url });
+            return;
+          } catch (e) {
+            await new Promise(res => setTimeout(res, 100));
+          }
         }
       }
+
+      const port = 50000 + Math.trunc(Math.random() * 10000);
+      process.env.MODEL_ELASTICSEARCH_PORT = `${port}`;
+      const container = new DockerContainer('elasticsearch:latest')
+        .forceDestroyOnShutdown()
+        .exposePort(port, 9200)
+        .addEnvVar('discovery.type', 'single-node');
+
+      container.run();
+      await waitForUrl(`http://127.0.0.1:${port}`, 10000);
     }
-
-    const port = 50000 + Math.trunc(Math.random() * 10000);
-    process.env.MODEL_ELASTICSEARCH_PORT = `${port}`;
-    const { DockerContainer } = require('@travetto/exec/src/docker');
-    const container = new DockerContainer('elasticsearch:latest')
-      .forceDestroyOnShutdown()
-      .exposePort(port, 9200)
-      .addEnvVar('discovery.type', 'single-node');
-
-    container.run();
-    await waitForUrl(`http://127.0.0.1:${port}`, 10000);
   }
 };
