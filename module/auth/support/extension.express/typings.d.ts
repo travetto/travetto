e7a609c2b4855fde050fdc75a339f3fdaed4d770
Timothy Soehnlin <tim@eaiti.com>
2018-07-01 02:10:56 -0400

    Moving express out to it's own package

--- module/auth/support/extension.express/typings.d.ts
+++
@@ -1,9 +0,0 @@
-import { Class } from '@travetto/registry';
-import { AuthService } from '../../src';
-
-declare module "express" {
-	export interface Request {
-		auth: AuthService
-		doLogin(providers: symbol[]): Promise<any>;
-	}
-}
