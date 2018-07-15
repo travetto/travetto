57d8bc833256cf846b0bf8cecbe0a38353f62a20
Timothy Soehnlin <tim@eaiti.com>
2018-07-15 15:29:46 -0400

    Reworking tests to use framework

--- module/exec/test/docker.ts
+++ module/exec/test/docker.ts
@@ -1,23 +1,22 @@
+import * as assert from 'assert';
+import { Suite, Test } from '@travetto/test';
 import { DockerContainer } from '../src/docker';
 
-async function test() {
-  const port = 10000;
-  const container = new DockerContainer('mongo:latest')
-    .createTempVolume('/var/workspace')
-    .exposePort(port)
-    .setWorkingDir('/var/workspace')
-    .forceDestroyOnShutdown();
+@Suite()
+export class DockerTet {
 
-  container.run('--storageEngine', 'ephemeralForTest', '--port', port);
-  await DockerContainer.waitForPort(port);
+  @Test()
+  async test() {
+    const port = 10000;
+    const container = new DockerContainer('mongo:latest')
+      .createTempVolume('/var/workspace')
+      .exposePort(port)
+      .setWorkingDir('/var/workspace')
+      .forceDestroyOnShutdown();
 
-  return;
+    container.run('--storageEngine', 'ephemeralForTest', '--port', port);
+    await DockerContainer.waitForPort(port);
+
+    return;
+  }
 }
-
-test().then(() => {
-  console.log('Done');
-  process.exit(0);
-}, (e: any) => {
-  console.log('Err', e);
-  process.exit(1);
-});
--- module/exec/test/index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-require('@travetto/base/bin/travetto').run()
-  .then(x =>
-    require(process.env.SRC || './docker-stdin.ts')
-  );
