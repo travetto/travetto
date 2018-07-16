5c01f3910f7479e9318dc2d5afa33b54b93eaf42
Timothy Soehnlin <tim@eaiti.com>
2018-07-15 23:02:34 -0400

    Refining env interaction, as well as test cleanup for exec/registry/config.  Fixed bug in scan-app that mangled relative file paths.

--- module/base/src/env.js
+++ module/base/src/env.js
@@ -1,63 +1,117 @@
 const path = require('path');
-const { execSync } = require('child_process');
 
-const p_env = process.env;
+const PROD_KEY = 'prod';
+const TEST_KEY = 'test';
+const DEV_KEY = 'dev';
 
-const envs = (p_env.ENV || p_env.env || p_env.NODE_ENV || '').toLowerCase().split(/[, ]+/);
-const envSet = new Set(envs);
-const env = envSet.has.bind(envSet);
+const envVal = k => process.env[k] || process.env[k.toLowerCase()] || process.env[k.toUpperCase()];
+const envListVal = k => (envVal(k) || '').split(/[, ]+/g).filter(x => !!x);
+const isEnvTrue = k => {
+  const val = envVal(k);
+  return !!val && !/(0|false|off)/i.test(val);
+};
 
-const cwd = (p_env.INIT_CWD || process.cwd()).replace(/[\\]+/g, path.sep).replace(/[\/\\]+$/, '');
-const prod = env('prod') || env('production');
-const test = env('test') || env('testing');
-const dev = !prod && !test;
-const watch = (dev && !('NO_WATCH' in p_env)) || 'WATCH' in p_env;
-const debug = ('DEBUG' in p_env && !!p_env.DEBUG) || ('debug' in p_env && !!p_env.debug) || dev;
-const trace = ('TRACE' in p_env && !!p_env.TRACE) || ('trace' in p_env && !!p_env.trace);
+const cwd = (envVal('init_cwd') || process.cwd()).replace(/[\\]+/g, path.sep).replace(/[\/\\]+$/, '');
 
-let frameworkDev = false;
-try {
-  frameworkDev = require(`${cwd}/package.json`).name.startsWith('@travetto');
-} catch (e) {}
+function checkFrameworkDev() {
+  let frameworkDev = false;
 
-const profiles = [
-  'application',
-  ...(p_env.profile || p_env.PROFILE || '').toLowerCase().split(/[, ]+/),
-  prod ? 'prod' : '',
-  test ? 'test' : '',
-  dev ? 'dev' : '',
-  ...envs,
-  ...(process.argv.slice(2) || []) // Pull in args
-].filter(x => !!x);
-const profileSet = new Set(profiles);
-const profile = profileSet.has.bind(profileSet);
-
-let docker = !('NO_DOCKER' in p_env && !!p_env.NO_DOCKER);
-if (docker) { // Check for docker existance
   try {
-    execSync('docker ps', { stdio: [undefined, undefined, undefined] });
-  } catch (e) {
-    docker = false;
+    frameworkDev = require(`${cwd}/package.json`).name.startsWith('@travetto');
+  } catch (e) {}
+
+  return { frameworkDev };
+}
+
+function checkDocker() {
+  let docker = !isEnvTrue('NO_DOCKER');
+  if (docker) { // Check for docker existance
+    try {
+      const { execSync } = require('child_process');
+      execSync('docker ps', { stdio: [undefined, undefined, undefined] });
+    } catch (e) {
+      docker = false;
+    }
   }
+  return { docker };
 }
 
-console.warn = (...args) => console.log('WARN', ...args);
-console.info = (...args) => console.log('INFO', ...args);
-console.debug = (...args) => console.log('DEBUG', ...args);
-console.trace = (...args) => console.log('TRACE', ...args);
-
-if (!trace) {
-  console.trace = (...args) => {};
+function checkWatch(dev) {
+  const watch = (dev && !isEnvTrue('NO_WATCH')) || isEnvTrue('watch');
+  return { watch };
 }
 
-if (!debug) {
-  console.debug = () => {}; // Suppress debug statements
+function buildLogging(dev) {
+  const debug = isEnvTrue('debug') || dev;
+  const trace = isEnvTrue('trace');
+
+  console.warn = (...args) => console.log('WARN', ...args);
+  console.info = (...args) => console.log('INFO', ...args);
+  console.debug = (...args) => console.log('DEBUG', ...args);
+  console.trace = (...args) => console.log('TRACE', ...args);
+
+  if (!trace) {
+    console.trace = (...args) => {};
+  }
+
+  if (!debug) {
+    console.debug = () => {}; // Suppress debug statements
+  }
+
+  function error(...args) {
+    console.error(...args.map(x => x && x.stack ? x.stack : x));
+  }
+
+  return { debug, trace, error };
 }
 
-function error(...args) {
-  console.error(...args.map(x => x && x.stack ? x.stack : x));
+function buildProfile() {
+  const cli = (process.argv.slice(2) || [])
+    .filter(x => /^-P[A-Za-z0-9_\-]+/.test(x))
+    .map(x => x.replace(/^-P/g, ''));
+
+  const mapping = {
+    production: PROD_KEY,
+    testing: TEST_KEY,
+    development: DEV_KEY
+  };
+
+  const ext = [...envListVal('node_env'), ...envListVal('env'), ...envListVal('profile'), ...cli]
+    .map(x => mapping[x] || x);
+
+  const primary =
+    (ext.includes(PROD_KEY) && PROD_KEY) ||
+    (ext.includes(TEST_KEY) && TEST_KEY) || DEV_KEY;
+
+  const allSet = new Set();
+
+  // Shift to front
+  const all = ['application', primary, ...ext]
+    .filter(x => {
+      const isNew = !allSet.has(x);
+      allSet.add(x);
+      return isNew;
+    });
+
+  return {
+    profiles: all,
+    is: allSet.has.bind(allSet),
+    prod: primary === PROD_KEY,
+    test: primary === TEST_KEY,
+    dev: primary === DEV_KEY
+  };
 }
 
-const Env = { prod, dev, test, watch, debug, trace, docker, cwd, error, profiles, is: profile, frameworkDev };
+const profile = buildProfile();
+
+const Env = [
+  { cwd },
+  profile,
+  buildLogging(profile.dev),
+  checkWatch(profile.dev),
+  checkFrameworkDev(),
+  checkDocker()
+].reduce((acc, el) =>
+  Object.assign(acc, el));
 
 module.exports = { Env };
--- module/config/test/simple-config.ts
+++ module/config/test/simple-config.ts
@@ -1,5 +1,8 @@
-import { Config, ConfigLoader } from '../src';
 import * as assert from 'assert';
+
+import { Test, Suite, BeforeEach, AfterEach } from '@travetto/test';
+
+import { ConfigLoader } from '../src';
 
 class DbConfig {
   name: string;
@@ -8,20 +11,62 @@ class DbConfig {
 }
 
 class TestConfig extends DbConfig {
-
+  anonHosts = ['a', 'b'];
 }
 
-const conf = new TestConfig();
+@Suite()
+export class ConfigTest {
 
-ConfigLoader.bindTo(conf, 'db.mysql');
+  envCopy: any;
 
-assert(conf.name === 'Oscar');
+  private reinit() {
+    delete (ConfigLoader as any)['_initialized'];
+    ConfigLoader.initialize();
+  }
 
-process.env.DB_MYSQL_NAME = 'Roger';
+  @BeforeEach()
+  before() {
+    this.envCopy = { ...process.env };
+    this.reinit();
+  }
 
-delete (ConfigLoader as any)['_initialized'];
-ConfigLoader.initialize();
+  @AfterEach()
+  after() {
+    process.env = this.envCopy;
+  }
 
-ConfigLoader.bindTo(conf, 'db.mysql');
+  @Test()
+  async verifyBasic() {
+    const conf = new TestConfig();
+    ConfigLoader.bindTo(conf, 'db.mysql');
+    assert(conf.name === 'Oscar');
+  }
 
-assert(conf.name === 'Roger');
+  @Test()
+  async verifyEnv() {
+    process.env.DB_MYSQL_NAME = 'Roger';
+    this.reinit();
+
+    const conf = new TestConfig();
+    ConfigLoader.bindTo(conf, 'db.mysql');
+    assert(conf.name === 'Roger');
+  }
+
+  @Test()
+  async verifyNotdefined() {
+    const conf = new TestConfig();
+    ConfigLoader.bindTo(conf, 'model.mongo');
+
+    // Default value from
+    assert(conf.anonHosts === ['a', 'b']);
+
+    process.env.MODEL_MONGO_ANONHOSTS = 'a,b,c,d';
+    this.reinit();
+
+    const newConf = new TestConfig();
+    ConfigLoader.bindTo(newConf, 'model.mongo');
+
+    // Default value from
+    assert(newConf.anonHosts === ['a', 'b', 'c', 'd']);
+  }
+}
