84a056b77f8126448e4849ebebe46ad962db45ff
Timothy Soehnlin <tim@eaiti.com>
2018-08-26 11:50:21 -0400

    Exlcuding e2e from packages, resolving lerna issues, as well as resolving style tag issues

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 export NODE_PRESERVE_SYMLINKS=1
+cp lerna.json lerna.json.bak
+cat lerna.json.bak | sed -e 's|"module/\*"|"module/*", "sample/*"|' > lerna.json
 lerna clean --yes
 lerna bootstrap --hoist
+mv lerna.json.bak lerna.json
 rm -f package-lock.json
 ./lerna-supplement.js $@
