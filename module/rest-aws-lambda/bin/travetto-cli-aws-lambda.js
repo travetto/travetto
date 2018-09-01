3ffd8f9b7919d98a1b746ca4b8ccc21f289d2d56
Timothy Soehnlin <tim@eaiti.com>
2018-09-01 15:21:25 -0400

    Unifying fs activity

--- module/rest-aws-lambda/bin/travetto-cli-aws-lambda.js
+++ module/rest-aws-lambda/bin/travetto-cli-aws-lambda.js
@@ -1,4 +1,4 @@
-const util = require('@travetto/base/src/fs-util');
+const { FsUtil } = require('@travetto/base/src/fs-util');
 
 module.exports = function init(program, cwd, dependOn) {
   const path = require('path');
@@ -12,42 +12,42 @@ module.exports = function init(program, cwd, dependOn) {
     .action(async (cmd) => {
 
       if (!cmd.workspace) {
-        cmd.workspace = util.tempDir('lambda-');
+        cmd.workspace = FsUtil.tempDir('lambda-');
         console.log('Temp Workspace', cmd.workspace);
       }
 
       const DIST = path.resolve(cwd, cmd.workspace);
       const ZIP = path.resolve(cwd, cmd.output || 'lambda.zip');
 
-      util.remove(DIST);
-      util.remove(ZIP);
-      util.mkdir(DIST);
+      FsUtil.remove(DIST);
+      FsUtil.remove(ZIP);
+      FsUtil.mkdirp(DIST);
 
       exec(`cp -r * ${DIST}`, { cwd });
 
-      util.writeFile(`${DIST}/index.js`,
+      FsUtil.writeFile(`${DIST}/index.js`,
         'process.env.TS_CACHE_DIR = `${__dirname}/cache`;\n' +
-        util.readFile(`${__dirname}/lambda.js`));
+        FsUtil.readFile(`${__dirname}/lambda.js`));
 
       await dependOn('compile', ['-o', './cache', '-r', '/var/task'], DIST);
 
       // Removing baggage
-      util.remove(`${DIST}/node_modules/typescript`);
-      util.remove(`${DIST}/node_modules/@types`);
-      util.remove(`${DIST}/node_modules/bson/browser_build`);
-      util.remove(`${DIST}/.git`);
+      FsUtil.remove(`${DIST}/node_modules/typescript`);
+      FsUtil.remove(`${DIST}/node_modules/@types`);
+      FsUtil.remove(`${DIST}/node_modules/bson/browser_build`);
+      FsUtil.remove(`${DIST}/.git`);
 
-      util.remove(`${DIST}/node_modules/source-map-support/browser-source-map-support.js`);
-      util.remove(`${DIST}/package-lock.json`);
+      FsUtil.remove(`${DIST}/node_modules/source-map-support/browser-source-map-support.js`);
+      FsUtil.remove(`${DIST}/package-lock.json`);
 
       // Stub out ts
-      util.mkdir(`${DIST}/node_modules/typescript`);
-      util.writeFile(`${DIST}/node_modules/typescript/index.js`,
+      FsUtil.mkdirp(`${DIST}/node_modules/typescript`);
+      FsUtil.writeFile(`${DIST}/node_modules/typescript/index.js`,
         'module.exports = {};');
 
       for (const p of ['e2e', 'test', 'dist']) {
-        for (const f of util.find(DIST, x => x.endsWith(`/${p}`), true)) {
-          util.remove(f);
+        for (const f of FsUtil.find(DIST, x => x.endsWith(`/${p}`), true)) {
+          FsUtil.remove(f);
         }
       }
 
@@ -56,13 +56,13 @@ module.exports = function init(program, cwd, dependOn) {
           'apis/5.0.js', 'apis/5.1.js', 'apis/5.2.js',
           'apis/5.3.js', 'apis/5.4.js'
         ]) {
-        for (const f of util.find(`${DIST}/node_modules`, x => x.endsWith(p))) {
-          util.remove(f);
+        for (const f of FsUtil.find(`${DIST}/node_modules`, x => x.endsWith(p))) {
+          FsUtil.remove(f);
         }
       }
 
       try {
-        util.move(
+        FsUtil.move(
           `${DIST}/node_modules/lodash/lodash.min.js`,
           `${DIST}/node_modules/lodash/lodash.js`
         );
