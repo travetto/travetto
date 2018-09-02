48efa1a83b6c8aed2bbc237a09f19dfb6507bbe1
Timothy Soehnlin <tim@eaiti.com>
2018-09-01 22:05:06 -0400

    Handling local install of cli properly

--- module/cli/bin/travetto.js
+++ module/cli/bin/travetto.js
@@ -3,12 +3,16 @@
 const fs = require('fs');
 const rel = `${process.cwd()}/node_modules/.bin/travetto`;
 
-if (!fs.existsSync(rel)) {
+if (!fs.existsSync(rel) || __filename !== rel) {
   const Module = require('module');
   const og = Module._load;
   Module._load = function(req, parent) {
     if (req.startsWith('@travetto/cli')) {
-      req = `${__dirname}/../${req.split('@travetto/cli')[1]}`.replace(/[\\\/]+/g, '/');
+      if (rel === __filename) {
+        req = `${__dirname}/../${req.split('@travetto/cli')[1]}`.replace(/[\\\/]+/g, '/');
+      } else {
+        req = `${process.cwd()}/node_modules/${req}`;
+      }
     }
     return og.call(Module, req, parent);
   };
