d176692450986b0291b825b66c8bb3dae0d5e633
Timothy Soehnlin <tim@eaiti.com>
2018-09-02 10:36:41 -0400

    Fixing cli choice

--- module/cli/bin/travetto.js
+++ module/cli/bin/travetto.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const rel = `${process.cwd()}/node_modules/.bin/travetto`;
+const rel = `${process.cwd()}/node_modules/@travetto/cli/bin/travetto.js`;
 
-if (!fs.existsSync(rel) || __filename !== rel) {
+const hasLocal = fs.existsSync(rel);
+const isLocal = __filename === rel;
+
+if (!hasLocal || !isLocal) {
   const Module = require('module');
   const og = Module._load;
   Module._load = function(req, parent) {
     if (req.startsWith('@travetto/cli')) {
-      if (rel === __filename) {
+      if (!hasLocal) { // Map all @travetto/cli calls to root of global package
         req = `${__dirname}/../${req.split('@travetto/cli')[1]}`.replace(/[\\\/]+/g, '/');
-      } else {
+      } else { // Rewrite @travetto/cli to map to local folder, when calling globally
         req = `${process.cwd()}/node_modules/${req}`;
       }
     }
