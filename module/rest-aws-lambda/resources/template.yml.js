6b84db1194f2fca89d290a68bb8a6ef1b3f4a05d
Timothy Soehnlin <tim@eaiti.com>
2018-09-08 20:10:39 -0400

    Working on SAM template

---
+++ module/rest-aws-lambda/resources/template.yml.js
@@ -0,0 +1,31 @@
+const { AppInfo } = require('@travetto/base/src/app-info');
+
+exports.template = function template(controllers, lambdaDir) {
+  return `
+AWSTemplateFormatVersion: '2010-09-09'
+Transform: 'AWS::Serverless-2016-10-31'
+Description: >-
+  Travetto Application deployment for ${AppInfo.SIMPLE_NAME}
+Resources:
+${controllers.map(cont => `
+  ${cont.class.name}:
+    Type: 'AWS::Serverless::Function'
+    Properties:
+      Handler: index.handler
+      Runtime: nodejs8.10
+      CodeUri: ./lambda.zip
+      Description: >-
+        ${cont.description || ''}.
+      MemorySize: 256
+      Timeout: 60
+      Events:
+${cont.endpoints.map(ep => `
+        ${ep.handlerName}:
+          Type: 'Api'
+          Properties:
+            Path: ${(cont.basePath + ep.path).replace(/\/+/g, '/').replace(/:([A-Za-z0-9_]+)/g, (a, n) => `{${n}}`)}
+            Method: ${ep.method.toUpperCase()}
+`.replace(/^\n|\n$/g,'')).join('\n')}
+`.replace(/^\n|\n$/g,'')).join('\n')}
+`.trim();
+}
