0f2332a80d220d2f80c49fbe7ca7e191978925af
Timothy Soehnlin <tim@eaiti.com>
2018-09-27 15:56:35 -0400

    Allowing sigint to not be treated as an error for shutdown purposes

--- module/exec/src/util.ts
+++ module/exec/src/util.ts
@@ -51,7 +51,7 @@ export class ExecUtil {
         finish({ code: 1, stdout, stderr, message: err.message, valid: false }));
 
       p.on('close', (code: number) =>
-        finish({ code, stdout, stderr, valid: code === null || code === 0 }));
+        finish({ code, stdout, stderr, valid: code === null || code === 0 || code === 130 })); // Sigint
 
       if (timeout) {
         timer = setTimeout(async x => {
