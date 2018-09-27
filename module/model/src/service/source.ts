01203a72679679405000aa0aea378a56b2487341
Timothy Soehnlin <tim@eaiti.com>
2018-09-27 09:14:26 -0400

    Reworking modelservice to not be a modelsource itself.

--- module/model/src/service/source.ts
+++ module/model/src/service/source.ts
@@ -5,30 +5,33 @@ import { ModelQuery, Query, PageableModelQuery } from '../model/query';
 import { BulkResponse, BulkOp } from '../model/bulk';
 import { ModelCore } from '../model/core';
 
-export abstract class ModelSource {
+export interface ModelSource extends IModelSource { }
+export abstract class ModelSource { }
+
+export interface IModelSource {
   onChange?<T extends ModelCore>(e: ChangeEvent<Class<T>>): void;
   onSchemaChange?(e: SchemaChangeEvent): void;
 
-  abstract prePersist<T extends ModelCore>(cls: Class<T>, model: Partial<T>): Partial<T>;
-  abstract prePersist<T extends ModelCore>(cls: Class<T>, model: T): T;
+  prePersist<T extends ModelCore>(cls: Class<T>, model: Partial<T>): Partial<T>;
+  prePersist<T extends ModelCore>(cls: Class<T>, model: T): T;
 
-  abstract postLoad<T extends ModelCore>(cls: Class<T>, model: Partial<T>): Partial<T>;
-  abstract postLoad<T extends ModelCore>(cls: Class<T>, model: T): T;
+  postLoad<T extends ModelCore>(cls: Class<T>, model: Partial<T>): Partial<T>;
+  postLoad<T extends ModelCore>(cls: Class<T>, model: T): T;
 
-  abstract save<T extends ModelCore>(cls: Class<T>, model: T, keepId?: boolean): Promise<T>;
-  abstract saveAll<T extends ModelCore>(cls: Class<T>, models: T[], keepId?: boolean): Promise<T[]>;
-  abstract update<T extends ModelCore>(cls: Class<T>, model: T): Promise<T>;
-  abstract updateAllByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>, data: Partial<T>): Promise<number>;
-  abstract updatePartial<T extends ModelCore>(cls: Class<T>, model: Partial<T>): Promise<T>;
-  abstract updatePartialByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>, body: Partial<T>): Promise<T>;
+  save<T extends ModelCore>(cls: Class<T>, model: T, keepId?: boolean): Promise<T>;
+  saveAll<T extends ModelCore>(cls: Class<T>, models: T[], keepId?: boolean): Promise<T[]>;
+  update<T extends ModelCore>(cls: Class<T>, model: T): Promise<T>;
+  updateAllByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>, data: Partial<T>): Promise<number>;
+  updatePartial<T extends ModelCore>(cls: Class<T>, model: Partial<T>): Promise<T>;
+  updatePartialByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>, body: Partial<T>): Promise<T>;
 
-  abstract query<T extends ModelCore, U = T>(cls: Class<T>, builder: Query<T>): Promise<U[]>;
+  query<T extends ModelCore, U = T>(cls: Class<T>, builder: Query<T>): Promise<U[]>;
 
-  abstract bulkProcess<T extends ModelCore>(cls: Class<T>, operations: BulkOp<T>[]): Promise<BulkResponse>;
-  abstract getById<T extends ModelCore>(cls: Class<T>, id: string): Promise<T>;
-  abstract getByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>, failOnMany?: boolean): Promise<T>;
-  abstract getAllByQuery<T extends ModelCore>(cls: Class<T>, query: PageableModelQuery<T>): Promise<T[]>;
-  abstract getCountByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>): Promise<number>;
-  abstract deleteById<T extends ModelCore>(cls: Class<T>, id: string): Promise<number>;
-  abstract deleteByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>): Promise<number>;
+  bulkProcess<T extends ModelCore>(cls: Class<T>, operations: BulkOp<T>[]): Promise<BulkResponse>;
+  getById<T extends ModelCore>(cls: Class<T>, id: string): Promise<T>;
+  getByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>, failOnMany?: boolean): Promise<T>;
+  getAllByQuery<T extends ModelCore>(cls: Class<T>, query: PageableModelQuery<T>): Promise<T[]>;
+  getCountByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>): Promise<number>;
+  deleteById<T extends ModelCore>(cls: Class<T>, id: string): Promise<number>;
+  deleteByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>): Promise<number>;
 }
