507c52d27d6583263747a41875f850f972e40f56
Timothy Soehnlin <tim@eaiti.com>
2018-11-01 21:01:57 -0400

    Adding docs

--- module/model/extension/rest.ts
+++ module/model/extension/rest.ts
@@ -14,6 +14,7 @@ export function ModelController<T extends ModelCore>(path: string, cls: Class<T>
         target, async function getAll(this: Svc, req: Request) {
           return this.source.getAllByQuery(cls, JSON.parse(req.query.q || '{}'));
         }), {
+        description: `Get all ${cls.name}`,
         priority: 101, method: 'get', path: '/', headers: {
           Expires: '-1',
           'Cache-Control': 'max-age=0, no-cache'
@@ -26,6 +27,7 @@ export function ModelController<T extends ModelCore>(path: string, cls: Class<T>
         target, async function getById(this: Svc, req: Request) {
           return this.source.getById(cls, req.params.id);
         }), {
+        description: `Get ${cls.name} by id`,
         priority: 102, method: 'get', path: '/:id', headers: {
           Expires: '-1',
           'Cache-Control': 'max-age=0, no-cache'
@@ -38,19 +40,34 @@ export function ModelController<T extends ModelCore>(path: string, cls: Class<T>
       ControllerRegistry.getOrCreateEndpointConfig(
         target, async function update(this: Svc, req: Request) {
           return this.source.update(cls, await getSchemaBody(req, cls));
-        }), { priority: 103, method: 'put', path: '/', requestType: { type: cls, description: cls.name }, responseType: { description: cls.name, type: cls } });
+        }), {
+        description: `Update ${cls.name}`,
+        priority: 103, method: 'put', path: '/',
+        requestType: { type: cls, description: cls.name },
+        responseType: { description: cls.name, type: cls }
+      }
+    );
 
     Object.assign(
       ControllerRegistry.getOrCreateEndpointConfig(
         target, async function create(this: Svc, req: Request) {
           return this.source.save(cls, await getSchemaBody(req, cls));
-        }), { priority: 104, method: 'post', path: '/', requestType: { type: cls, description: cls.name }, responseType: { description: cls.name, type: cls } });
+        }), {
+        description: `Create ${cls.name}`,
+        priority: 104, method: 'post', path: '/',
+        requestType: { type: cls, description: cls.name },
+        responseType: { description: cls.name, type: cls }
+      }
+    );
 
     Object.assign(
       ControllerRegistry.getOrCreateEndpointConfig(
         target, async function remove(this: Svc, req: Request) {
           return this.source.deleteById(cls, req.params.id);
-        }), { priority: 105, method: 'delete', path: '/:id' });
+        }), {
+        description: `Delete ${cls.name} by id`,
+        priority: 105, method: 'delete', path: '/:id'
+      });
 
 
     ControllerRegistry.register(target, {
