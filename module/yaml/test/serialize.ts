50aa19bc502d1f5f3032edce60757888be7c3c09
Timothy Soehnlin <tim@eaiti.com>
2018-11-08 10:17:55 -0500

    Yaml fixes and model-es fixes

--- module/yaml/test/serialize.ts
+++ module/yaml/test/serialize.ts
@@ -22,7 +22,7 @@ export class SerializeTest {
         3,
         'lorem ipsum whadya think about them apples, huh? lorem ipsum whadya think about them apples, huh? huh?'
       ]
-    });
+    }, 2, 100);
     assert(out === `
 a: 20
 b: 30
