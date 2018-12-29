fa7ef9456483c4198e499cf35e62bd2480b97aec
Timothy Soehnlin <tim@eaiti.com>
2018-12-29 16:23:48 -0500

    Allowing for extraction of cached app list more easily as well as direct invocation of runner without cli installed

--- module/di/bin/travetto-cli-run.js
+++ module/di/bin/travetto-cli-run.js
@@ -1,22 +1,9 @@
 //@ts-check
-const fs = require('fs');
-const path = require('path');
-
-// @ts-ignore
-const { Util: { cwd, program, fork } } = require('@travetto/cli/src/util');
-
-const config = path.join(cwd, '.app-config.cache');
+const { getCachedAppList } = require('./travetto-find-apps');
 
 async function getAppList() {
   try {
-    //Read cache it
-    if (!fs.existsSync(config)) {
-      const text = await fork(`${__dirname}/travetto-find-apps.js`);
-      return JSON.parse(text); // Skipping remainder
-      // fs.writeFileSync(config, text); // Skip for now
-    }
-
-    return JSON.parse(fs.readFileSync(config).toString());
+    return await getCachedAppList();
   } catch (err) {
     console.error(err && err.stack ? err.stack : err);
     process.exit(1);
@@ -51,67 +38,70 @@ async function runApp(app) {
   }
 }
 
-module.exports = function () {
-  let listHelper;
-
-  program
-    .command('run [application]')
-    .on('--help', () => {
-      console.log('\n  Available Applications:');
-      if (listHelper) {
-        console.log();
-        console.log(listHelper());
-      } else {
-        console.log('\n  No applications defined, use @Application to registry entry points');
-      }
-      console.log();
-    })
-    .option('-e, --env [env]', 'Application environment (dev|test|e2e|prod)', /^(dev|test|e2e|prod)$/i, 'dev')
-    .option('-w, --watch', 'Run the application in watch mode')
-    .option('-p, --profile [profile]', 'Specify additional application profiles', (v, ls) => { ls.push(v); return ls; }, [])
-    .action(async (app, cmd) => {
-
-      cmd.watchSpecified = cmd.watch !== undefined;
-      cmd.envSpecified = !(cmd.env === undefined || (cmd.env === 'dev' && !process.argv.find(x => x === 'dev'))); // If env is default selected
-
-      cmd.profile = [
-        ...(cmd.profile || []),
-        ...(process.env.PROFILE || '').split(/,/g)
-      ]
-        .filter(x => !!x)
-        .map(x => x.trim());
-
-
-      process.env.ENV = cmd.env; //Preemptively set b/c env changes how we compile some things
-
-      const apps = await getAppList();
-      const selected = apps.find(x => x.name === app);
-
-      if (!selected) {
-        if (apps.length) {
-          listHelper = generateAppHelpList.bind(null, apps, cmd);
-        }
-        cmd.help();
-      } else {
-        cmd.env = cmd.env || process.env.ENV || process.env.env;
-
-        if (!cmd.envSpecified && selected.filename.includes('e2e')) {
-          cmd.env = 'e2e';
-        }
-        if (!cmd.watchSpecified) {
-          cmd.watch = selected.watchable && cmd.env !== 'prod';
-        }
-      }
-
-      process.env.ENV = cmd.env;
-      process.env.PROFILE = cmd.profile.join(',');
-      process.env.WATCH = `${cmd.watch}`;
-
-      runApp(app);
-    });
-};
-
 //@ts-ignore
 if (require.main === module) {
   runApp(process.argv.pop()); //If loaded directly as main entry, run
+} else {
+  module.exports = function () {
+    let listHelper;
+
+    // @ts-ignore
+    const { Util: { program } } = require('@travetto/cli/src/util');
+
+    program
+      .command('run [application]')
+      .on('--help', () => {
+        console.log('\n  Available Applications:');
+        if (listHelper) {
+          console.log();
+          console.log(listHelper());
+        } else {
+          console.log('\n  No applications defined, use @Application to registry entry points');
+        }
+        console.log();
+      })
+      .option('-e, --env [env]', 'Application environment (dev|test|e2e|prod)', /^(dev|test|e2e|prod)$/i, 'dev')
+      .option('-w, --watch', 'Run the application in watch mode')
+      .option('-p, --profile [profile]', 'Specify additional application profiles', (v, ls) => { ls.push(v); return ls; }, [])
+      .action(async (app, cmd) => {
+
+        cmd.watchSpecified = cmd.watch !== undefined;
+        cmd.envSpecified = !(cmd.env === undefined || (cmd.env === 'dev' && !process.argv.find(x => x === 'dev'))); // If env is default selected
+
+        cmd.profile = [
+          ...(cmd.profile || []),
+          ...(process.env.PROFILE || '').split(/,/g)
+        ]
+          .filter(x => !!x)
+          .map(x => x.trim());
+
+
+        process.env.ENV = cmd.env; //Preemptively set b/c env changes how we compile some things
+
+        const apps = await getAppList();
+        const selected = apps.find(x => x.name === app);
+
+        if (!selected) {
+          if (apps.length) {
+            listHelper = generateAppHelpList.bind(null, apps, cmd);
+          }
+          cmd.help();
+        } else {
+          cmd.env = cmd.env || process.env.ENV || process.env.env;
+
+          if (!cmd.envSpecified && selected.filename.includes('e2e')) {
+            cmd.env = 'e2e';
+          }
+          if (!cmd.watchSpecified) {
+            cmd.watch = selected.watchable && cmd.env !== 'prod';
+          }
+        }
+
+        process.env.ENV = cmd.env;
+        process.env.PROFILE = cmd.profile.join(',');
+        process.env.WATCH = `${cmd.watch}`;
+
+        runApp(app);
+      });
+  };
 }
