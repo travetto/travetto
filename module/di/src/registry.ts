353620337df557d13a68c00c0ad5352cb5ad6e41
Timothy Soehnlin <tim@eaiti.com>
2019-02-01 11:19:41 -0500

    Reworking config to pull profiles from resources

--- module/di/src/registry.ts
+++ module/di/src/registry.ts
@@ -176,7 +176,6 @@ export class $DependencyRegistry extends MetadataRegistry<InjectableConfig> {
     const managed = this.get(clz)!;
 
     // Only fetch constructor values
-    console.log('Fetch dependencies for', target.__id)
     const consValues = await this.fetchDependencies(managed, managed.dependencies.cons);
 
     // Create instance
