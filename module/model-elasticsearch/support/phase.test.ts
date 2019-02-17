80b91067e3511e145a02e7b1817360cc2ff75542
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2019-02-17 18:12:40 -0500

    Upgrading typescript to latest, and exposing boot cli command

--- module/model-elasticsearch/support/phase.test.ts
+++ module/model-elasticsearch/support/phase.test.ts
@@ -1,20 +1,31 @@
 import * as http from 'http';
 import * as https from 'https';
 
-function client(url: string) {
-  return (url.startsWith('https') ? https : http) as typeof http;
-}
-
 function check(url: string) {
   return new Promise((resolve) => {
     try {
-      const req = client(url).get(url, (msg: http.IncomingMessage) =>
-        msg.on('end', () => resolve((msg.statusCode || 200))));
+      const client = url.startsWith('https') ? https : http;
+      const req = client.get(url, (msg: http.IncomingMessage) =>
+        msg
+          .on('error', () => resolve(500))
+          .on('end', () => resolve((msg.statusCode || 200)))
+          .on('close', () => resolve((msg.statusCode || 200))));
       req.on('error', () => resolve(500));
     } catch (e) {
       resolve(400);
     }
   });
+}
+
+async function waitForUrl(url: string, timeout: number) {
+  const start = Date.now();
+  while ((Date.now() - start) < timeout) {
+    const status = await check(url);
+    if (status >= 200 && status <= 299) {
+      return; // We good
+    }
+    await new Promise(res => setTimeout(res, 100));
+  }
 }
 
 export const init = {
@@ -30,17 +41,6 @@ export const init = {
       process.env.MODEL_ELASTICSEARCH_NAMESPACE = `test_${Math.trunc(Math.random() * 10000)}`; // Randomize schema
     } catch (e) {
 
-      async function waitForUrl(url: string, timeout: number) {
-        const start = Date.now();
-        while ((Date.now() - start) < timeout) {
-          const status = await check(url);
-          if (status >= 200 && status <= 299) {
-            return; // We good
-          }
-          await new Promise(res => setTimeout(res, 100));
-        }
-      }
-
       const port = 50000 + Math.trunc(Math.random() * 10000);
       process.env.MODEL_ELASTICSEARCH_PORT = `${port}`;
       const container = new DockerContainer('elasticsearch:6.5.4')
@@ -49,6 +49,7 @@ export const init = {
         .addEnvVar('discovery.type', 'single-node');
 
       container.run();
+      await new Promise(r => setTimeout(r, 2000)); // Wait 2 seconds
       await waitForUrl(`http://127.0.0.1:${port}`, 10000);
     }
 
