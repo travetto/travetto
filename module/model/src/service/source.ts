6f020cb4f5db0931713d51afa10f1eb07e9de695
Timothy Soehnlin <tim@eaiti.com>
2019-02-19 15:59:35 -0500

    Missed typings

--- module/model/src/service/source.ts
+++ module/model/src/service/source.ts
@@ -28,7 +28,8 @@ export interface IModelSource {
   updateAllByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>, data: Partial<T>): Promise<number>;
   updatePartial<T extends ModelCore>(cls: Class<T>, model: Partial<T>): Promise<T>;
   updatePartialByQuery<T extends ModelCore>(cls: Class<T>, query: ModelQuery<T>, body: Partial<T>): Promise<T>;
-
+  updatePartialView<T extends ModelCore>(cls: Class<T>, o: Partial<T>, view: string): Promise<T>;
+  updatePartialViewByQuery<T extends ModelCore>(cls: Class<T>, o: Partial<T>, view: string, query: ModelQuery<T>): Promise<T>;
   suggestField<T extends ModelCore, U = T>(
     cls: Class<T>, field: ValidStringFields<T>, query: string, filter?: PageableModelQuery<T>
   ): Promise<U[]>;
