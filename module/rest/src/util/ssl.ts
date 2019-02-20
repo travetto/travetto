8075270b24bf77e47a573361709252ede434f712
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2019-02-20 09:24:43 -0500

    Prepping for JWT overhaul

--- module/rest/src/util/ssl.ts
+++ module/rest/src/util/ssl.ts
@@ -4,7 +4,7 @@ const exec = util.promisify(cp.exec);
 
 export class SSLUtil {
   static async generateKeyPair() {
-    const { stdout } = await exec('openssl req -nodes -new -x509 -keyout /dev/stdout -out /dev/stdout -subj "/C=WR/ST=STATE/O=TRAVETTO/OU=REST/CN=APP"');
+    const { stdout } = await exec('openssl req -nodes -new -x509 -keyout /dev/stdout -out /dev/stdout -subj "/C=WR/ST=STATE/O=TRAVETTO/OU=REST/CN=DEV-APP"');
     const lines = stdout.toString().split('\n');
     const sep = lines.findIndex(x => x === '-----BEGIN CERTIFICATE-----');
     const key = lines.slice(0, sep).join('\n');
