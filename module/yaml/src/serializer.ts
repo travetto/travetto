26cd5bebbd8b8db3e3985ee84834f3e0e8b8fa88
Timothy Soehnlin <tim@eaiti.com>
2019-02-23 08:12:29 -0500

    Fixed word wrap

--- module/yaml/src/serializer.ts
+++ module/yaml/src/serializer.ts
@@ -10,42 +10,50 @@ export class Serializer {
     return key;
   }
 
-  static wordWrap(text: string, width: number = 160) {
+  static wordWrap(text: string, width: number = 120) {
     const lines: string[] = [];
     let line: string[] = [];
     let subl: number = 0;
 
-    const pushLine = () => {
+    const push = (x: string) => { subl += x.length; line.push(x); };
+
+    const flushLine = () => {
       if (line.length > 0) {
-        lines.push(line.join('').replace(/\s+$/g, ''));
+        lines.push(line.join('').trimRight());
       }
       line = [];
       subl = 0;
     };
 
-    text.replace(/\S+(\s+|$)/g, part => {
-      if (part.startsWith('\n')) {
-        line = [part];
-        pushLine();
+    for (const sub of text.split(/\n/)) {
+      if (sub.length < width) {
+        push(sub);
       } else {
-        if (subl + part.length > width) {
-          pushLine();
-        }
-        line.push(part);
-        subl += part.length;
+        const offset = sub.replace(/^(\s*)(.*)$/, (all, s) => s);
+        push(offset);
+
+        sub.trim().replace(/\S+(\s+|$)/g, part => {
+          if (subl + part.length > width) {
+            flushLine();
+            push(offset);
+          }
+          push(part);
+
+          return '';
+        });
       }
+      flushLine();
+    }
 
-      return '';
-    });
-
-    pushLine();
     return lines;
   }
 
-  static serialize(o: any, indent = 2, wordwrap = 160, indentLevel = 0) {
+  static serialize(o: any, indent = 2, wordwrap = 120, indentLevel = 0) {
     let out = '';
     const prefix = ' '.repeat(indentLevel);
-    if (typeof o === 'function' || o instanceof RegExp || o instanceof Set || o instanceof Map) {
+    if (o instanceof Error) {
+      out = `${this.serialize(o.stack, indent, wordwrap, indentLevel + indent)}\n`;
+    } else if (typeof o === 'function' || o instanceof RegExp || o instanceof Set || o instanceof Map) {
       throw new Error('Types are not supported');
     } else if (Array.isArray(o)) {
       out = o.map(el => `${prefix}-${this.serialize(el, indent, wordwrap, indentLevel + indent)}`).join('\n');
@@ -55,7 +63,7 @@ export class Serializer {
     } else if (typeof o === 'number' || typeof o === 'boolean' || o === null) {
       out = ` ${o}`;
     } else if (typeof o === 'string') {
-      o = o.replace(/\t/g, ' '.repeat(indentLevel));
+      o = o.replace(/\t/g, prefix);
       const lines = this.wordWrap(o, wordwrap);
       if (lines.length > 1) {
         out = [' >', ...lines.map(x => `${prefix}${x}`)].join('\n');
