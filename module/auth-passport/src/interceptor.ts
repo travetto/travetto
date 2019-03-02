f576bf1b984cefe1f373e8a82ec3a6ac58306f14
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2019-03-01 22:36:33 -0500

    Purging session support

--- module/auth-passport/src/interceptor.ts
+++ module/auth-passport/src/interceptor.ts
@@ -14,17 +14,12 @@ const authenticator = (passport as any as passport.Authenticator<Handler>);
 export class AuthPassportInterceptor extends RestInterceptor {
 
   private init = authenticator.initialize();
-  private session = authenticator.session();
 
   after = AuthInterceptor;
 
   intercept(req: Request, res: Response) {
     return new Promise((resolve, reject) => {
-      this.init(req, res, () => {
-        this.session(req, res, (err?: any) => {
-          err ? reject(err) : resolve();
-        });
-      });
+      this.init(req, res, () => resolve());
     });
   }
 }
--- module/rest-fastify/src/config.ts
+++ module/rest-fastify/src/config.ts
@@ -2,10 +2,5 @@ import { Config } from '@travetto/config';
 
 @Config('rest.fastify')
 export class FastifyConfig {
-  session = {
-    cookie: {
-      secure: false
-    },
-    secret: 'secret1secret2secret3secret4secret5'
-  };
+
 }
