7e5a9d737ca56170cda45e327da236ba9e4ff905
Timothy Soehnlin <tim@eaiti.com>
2019-03-06 08:21:56 -0500

    Testing auth context

--- module/auth-rest/e2e/src/sample.ts
+++ module/auth-rest/e2e/src/sample.ts
@@ -2,6 +2,9 @@ import { Application, InjectableFactory } from '@travetto/di';
 import { RestServer, RestApp } from '@travetto/rest';
 import { KoaRestApp } from '@travetto/rest-koa';
 
+// tslint:disable-next-line: no-import-side-effect
+import '@travetto/rest/extension/context';
+
 @Application('sample')
 export class SampleApp {
 
