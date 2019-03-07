dac25f4b18703b243ac448980ffa23eb2d42fb58
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2019-03-07 14:30:29 -0500

    Resolving test issues, with expected thrown but not, and deep checks for sets/maps/arrays/etc.

--- module/auth-passport/src/interceptor.ts
+++ module/auth-passport/src/interceptor.ts
@@ -17,8 +17,8 @@ export class PassportInterceptor extends RestInterceptor {
 
   after = AuthInterceptor;
 
-  intercept(req: Request, res: Response) {
-    return new Promise((resolve, reject) => {
+  async intercept(req: Request, res: Response) {
+    await new Promise((resolve, reject) => {
       this.init(req, res, () => resolve());
     });
   }
--- module/auth/test/principal.ts
+++ module/auth/test/principal.ts
@@ -1,11 +1,28 @@
 import * as assert from 'assert';
+
+import { AppError } from '@travetto/base';
 import { Suite, Test } from '@travetto/test';
+import { Context } from '@travetto/context';
 
-// import { PrincipalConfig } from '../src/principal-config';
+import { AuthService } from '../src/service';
+import { PrincipalProvider } from '../src/principal';
+import { Identity, Principal } from '../src/types';
 
-class User {
-  id: string;
-  perms: Set<string>;
+const USERS: { [key: string]: Principal } = {
+  a: {
+    id: 'a',
+    permissions: new Set(['1', '2', '3']),
+    details: {}
+  }
+};
+
+class CustomPP extends PrincipalProvider {
+  async resolvePrincipal(ident: Identity): Promise<Principal> {
+    if (!(ident.id in USERS)) {
+      throw new AppError('User is not found', 'notfound');
+    }
+    return USERS[ident.id];
+  }
 }
 
 @Suite()
@@ -13,11 +30,36 @@ export class PrincipalTest {
 
   @Test()
   async verifyTypings() {
-    // const config = new PrincipalConfig(User, {
-    //   id: 'id',
-    //   permissions: 'perms'
-    // });
+    const service = new AuthService();
+    service.contextProvider = new Context();
+    service.principalProvider = new CustomPP();
+    await service.contextProvider.run(async () => {
+      await assert.rejects(() => service.authorize({
+        id: 'b',
+        details: {},
+        permissions: new Set(['1', '2']),
+        provider: 'none'
+      }));
 
-    // assert(config.getId({ id: 'a', perms: new Set() }) === 'a');
+      await assert.doesNotReject(() => service.authorize({
+        id: 'a',
+        details: {},
+        permissions: new Set(['2', '3']),
+        provider: 'none'
+      }));
+
+      const ctx = await service.authorize({
+        id: 'a',
+        details: {},
+        permissions: new Set(),
+        provider: 'none'
+      });
+
+      assert(!!ctx.identity);
+      assert(!!ctx.principal);
+      assert(ctx.identity.id === ctx.principal.id);
+      assert(ctx.identity.provider === 'none');
+      assert(ctx.principal.permissions === new Set(['1', '2', '3']));
+    });
   }
 }
