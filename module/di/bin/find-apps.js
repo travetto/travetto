1cb3e1666160cd69ffca0f050035b4d66f6b6b9c
Timothy Soehnlin <tim@eaiti.com>
2019-03-18 07:28:31 -0400

    Working through ramifications from extension loading

--- module/di/bin/find-apps.js
+++ module/di/bin/find-apps.js
@@ -2,6 +2,6 @@ require(`${process.env.TRV_DI_BASE || '..'}/bin/lib`)
   .computeApps()
   .then(resolved => require('fs').writeSync(1, `${JSON.stringify(resolved)}\n`))
   .catch(err => {
-    console.error(err);
+    console.error(err && err.stack ? err.stack : err);
     process.exit(1);
   });
