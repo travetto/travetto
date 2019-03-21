be8c86f9c23d99fc83c5b4587f1242155bac3c2f
Timothy Soehnlin <tim@eaiti.com>
2019-03-21 14:52:01 -0400

    Supporting app customizers

--- module/rest/e2e/src/app.ts
+++ module/rest/e2e/src/app.ts
@@ -1,15 +1,21 @@
 import { Application, InjectableFactory, Inject } from '@travetto/di';
-import { ContextInterceptor } from '@travetto/context/src/extension/rest';
+import { ContextInterceptor } from '@travetto/context/src/extension/rest.ext';
 
-import { RouteConfig, RestApp, RestInterceptor } from '../..';
+import { RouteConfig, RestApp, RestInterceptor, RestAppCustomizer } from '../..';
 
-class DummyApp extends RestApp {
+type Inner = {
+  use(factory: any): void;
+};
+
+class DummyApp extends RestApp<Inner> {
   raw: any = {};
 
-  async createRaw() { }
-
-  async init() {
-    console.log('Initializing');
+  async createRaw() {
+    return {
+      use(val: any) {
+        console.log('Using', val);
+      }
+    };
   }
 
   listen() {
@@ -17,11 +23,11 @@ class DummyApp extends RestApp {
   }
 
   async registerRoutes(key: string | symbol, path: string, endpoints: RouteConfig[]) {
-    console.log('Registering Controller', path, endpoints);
+    console.log('Registering Controller', path, endpoints.length);
   }
 
   registerInterceptor(inter: RestInterceptor) {
-    console.log('Registering Interceptor', inter);
+    console.log('Registering Interceptor', inter.constructor.name);
   }
 
   async unregisterRoutes(key: string | symbol) {
@@ -33,8 +39,18 @@ class DummyApp extends RestApp {
 export class SampleApp {
 
   @InjectableFactory()
-  static getProvider(): RestApp {
+  static getProvider(): RestApp<Inner> {
     return new DummyApp();
+  }
+
+  @InjectableFactory()
+  static getCustomer(): RestAppCustomizer<Inner> {
+    return new class extends RestAppCustomizer<Inner> {
+      customize(app: Inner) {
+        console.log('Hello');
+        app.use('something');
+      }
+    }();
   }
 
   @Inject()
