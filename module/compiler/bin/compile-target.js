23b8dc54d975996a7a6b339a1cc45fbc16c8e8ad
Timothy Soehnlin <tim@eaiti.com>
2019-04-07 21:05:21 -0400

    Reverting change, as the tsconfig is now in the src-ts folder

--- module/compiler/bin/compile-target.js
+++ module/compiler/bin/compile-target.js
@@ -1,6 +1,6 @@
 require('@travetto/boot/bin/init')
-  .libRequire('@travetto/base').PhaseManager
-  .run().then(() => {
+  .libRequire('@travetto/base')
+  .PhaseManager.run().then(() => {
     const { Compiler } = require('@travetto/compiler/src/compiler');
     return Compiler.compileAll();
   });
