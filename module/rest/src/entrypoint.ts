d12d29ede94efe1cb48f3e7c52b984765165a9b7
Timothy Soehnlin <tim@eaiti.com>
2019-04-18 09:56:09 -0400

    Allowing for plugins to expose app entrypoints directly, only for '.' appRoot

---
+++ module/rest/src/entrypoint.ts
@@ -0,0 +1,8 @@
+import { Application } from './decorator/application';
+import { RestApp } from './app';
+
+@Application('rest')
+class EntryPoint {
+  constructor(private app: RestApp) { }
+  run() { return this.app.run(); }
+}
