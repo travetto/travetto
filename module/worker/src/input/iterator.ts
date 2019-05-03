f37ac14deaaaff6b2abd9cbd6c99a91f83f1dc12
Timothy Soehnlin <tim@eaiti.com>
2019-05-03 09:20:22 -0400

    Supporting async worker pool operations

--- module/worker/src/input/iterator.ts
+++ module/worker/src/input/iterator.ts
@@ -1,16 +1,18 @@
 import { InputSource } from './types';
 
-function isIterator<T>(o: any): o is Iterator<T> {
+type Itr<T> = Iterator<T> | AsyncIterator<T>;
+
+function isIterator<T>(o: any): o is Itr<T> {
   return 'next' in o;
 }
 
 export class IteratorInputSource<X> implements InputSource<X> {
 
-  private src: Iterator<X>;
+  private src: Itr<X>;
   private ondeck: X;
   done = false;
 
-  constructor(src: (() => Iterator<X>) | Iterator<X>) {
+  constructor(src: (() => Itr<X>) | Itr<X>) {
     if (isIterator(src)) {
       this.src = src;
     } else {
@@ -18,21 +20,21 @@ export class IteratorInputSource<X> implements InputSource<X> {
     }
   }
 
-  private primeNext() {
-    const res = this.src.next();
+  private async primeNext() {
+    const res = await this.src.next();
     this.done = res.done;
     this.ondeck = res.value;
   }
 
-  hasNext() {
+  async hasNext() {
     if (this.ondeck === undefined) {
-      this.primeNext();
+      await this.primeNext();
     }
     return !this.done;
   }
 
-  next() {
-    this.hasNext();
+  async next() {
+    await this.hasNext();
 
     const out = this.ondeck;
     delete this.ondeck;
