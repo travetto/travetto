e5b45319b1632527eec34ee22b08f69f76e1fed4
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2019-08-16 10:54:14 -0400

    Resolving tslint issues

--- module/cache/extension/redis.ts
+++ module/cache/extension/redis.ts
@@ -4,33 +4,33 @@ import { CacheStore, CacheEntry } from '../src/store/types';
 
 export class RedisCacheStore extends CacheStore {
 
-  _cl: redis.RedisClient;
+  cl: redis.RedisClient;
 
   constructor(public config: redis.ClientOpts = {}) {
     super();
   }
 
   async postConstruct() {
-    this._cl = new redis.RedisClient(this.config);
+    this.cl = new redis.RedisClient(this.config);
   }
 
   toPromise<V>(fn: (cb: redis.Callback<V>) => void): Promise<V> {
-    return new Promise((res, rej) => fn((err, v) => err ? rej(err) : res(v)));
+    return new Promise((res, rej) => fn((err: Error | undefined, v: V) => err ? rej(err) : res(v)));
   }
 
   async get(key: string): Promise<CacheEntry | undefined> {
-    const val: any = await this.toPromise(this._cl.get.bind(this._cl, key));
+    const val: any = await this.toPromise(this.cl.get.bind(this.cl, key));
     return val && this.postLoad(JSON.parse(val));
   }
 
   async has(key: string): Promise<boolean> {
-    return !!(await this.toPromise(this._cl.exists.bind(this._cl, key)));
+    return !!(await this.toPromise(this.cl.exists.bind(this.cl, key)));
   }
 
   async set(key: string, entry: CacheEntry): Promise<any> {
     entry = await this.prePersist(entry);
 
-    await this.toPromise(this._cl.setnx.bind(this._cl, key, JSON.stringify(entry)));
+    await this.toPromise(this.cl.setnx.bind(this.cl, key, JSON.stringify(entry)));
 
     if (entry.maxAge) {
       await this.touch(key, entry.maxAge + Date.now());
@@ -40,14 +40,14 @@ export class RedisCacheStore extends CacheStore {
   }
 
   async evict(key: string): Promise<boolean> {
-    return !!(await this.toPromise(this._cl.del.bind(this._cl, key)));
+    return !!(await this.toPromise(this.cl.del.bind(this.cl, key)));
   }
 
   async touch(key: string, expiresAt: number): Promise<boolean> {
-    return !!(await this.toPromise(this._cl.pexpireat.bind(this._cl, key, expiresAt)));
+    return !!(await this.toPromise(this.cl.pexpireat.bind(this.cl, key, expiresAt)));
   }
 
   async reset() {
-    await this.toPromise(this._cl.flushall.bind(this._cl));
+    await this.toPromise(this.cl.flushall.bind(this.cl));
   }
 }
