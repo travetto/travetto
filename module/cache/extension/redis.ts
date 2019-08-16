fef82253835fa7f2ca56b5bc17e84e34fbc3712d
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2019-08-16 09:47:06 -0400

    Working on testing, and cleaning up duplicate logic

--- module/cache/extension/redis.ts
+++ module/cache/extension/redis.ts
@@ -1,19 +1,17 @@
 import * as redis from 'redis';
 
-import { SystemUtil } from '@travetto/base';
-
 import { CacheStore, CacheEntry } from '../src/store/types';
 
 export class RedisCacheStore extends CacheStore {
 
-  client: redis.RedisClient;
+  _cl: redis.RedisClient;
 
   constructor(public config: redis.ClientOpts = {}) {
     super();
   }
 
   async postConstruct() {
-    this.client = new redis.RedisClient(this.config)
+    this._cl = new redis.RedisClient(this.config);
   }
 
   toPromise<V>(fn: (cb: redis.Callback<V>) => void): Promise<V> {
@@ -21,50 +19,35 @@ export class RedisCacheStore extends CacheStore {
   }
 
   async get(key: string): Promise<CacheEntry | undefined> {
-    let val: any = await this.toPromise(this.client.get.bind(this.client, key));
-    if (!(val === null || val === undefined)) {
-      val = JSON.parse(val) as CacheEntry;
-      if (val.stream) {
-        val.data = SystemUtil.toReadable(val.data);
-      }
-    }
-    return val;
+    const val: any = await this.toPromise(this._cl.get.bind(this._cl, key));
+    return val && this.postLoad(JSON.parse(val));
   }
 
   async has(key: string): Promise<boolean> {
-    return !!(await this.toPromise(this.client.exists.bind(this.client, key)));
+    return !!(await this.toPromise(this._cl.exists.bind(this._cl, key)));
   }
 
   async set(key: string, entry: CacheEntry): Promise<any> {
-    let data = entry.data;
-    let ret = data;
+    entry = await this.prePersist(entry);
 
-    if (this.isStream(data)) {
-      data = (await SystemUtil.toBuffer(data)).toString('base64');
-      ret = SystemUtil.toReadable(Buffer.from(data, 'base64'));
-      entry.stream = true;
-    }
-
-    const storeValue = JSON.stringify({ ...entry, data });
-
-    await this.toPromise(this.client.setnx.bind(this.client, key, storeValue));
+    await this.toPromise(this._cl.setnx.bind(this._cl, key, JSON.stringify(entry)));
 
     if (entry.maxAge) {
       await this.touch(key, entry.maxAge + Date.now());
     }
 
-    return ret;
+    return this.postLoad(entry).data;
   }
 
   async evict(key: string): Promise<boolean> {
-    return !!(await this.toPromise(this.client.del.bind(this.client, key)));
+    return !!(await this.toPromise(this._cl.del.bind(this._cl, key)));
   }
 
   async touch(key: string, expiresAt: number): Promise<boolean> {
-    return !!(await this.toPromise(this.client.pexpireat.bind(this.client, key, expiresAt)));
+    return !!(await this.toPromise(this._cl.pexpireat.bind(this._cl, key, expiresAt)));
   }
 
   async reset() {
-    await this.toPromise(this.client.flushall.bind(this.client));
+    await this.toPromise(this._cl.flushall.bind(this._cl));
   }
 }
