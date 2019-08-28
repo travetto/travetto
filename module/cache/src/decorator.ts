c1d074bdaab6f18a42e5911e093a86e07eed9898
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2019-08-27 20:17:15 -0400

    Cache is now a transformation as function references are important

--- module/cache/src/decorator.ts
+++ module/cache/src/decorator.ts
@@ -1,62 +1,12 @@
-import { ValidCacheFields, CacheStore } from './store/types';
+import { ValidCacheFields } from './store/types';
 import { CoreCacheConfig, CacheConfig } from './types';
 
 type TypedMethodDecorator<T, U> = (target: T, propertyKey: string, descriptor: TypedPropertyDescriptor<(...params: any[]) => U>) => void;
 
-function generateKey(cache: CacheStore, config: CoreCacheConfig, params: any[]) {
-  const input = config.params ? config.params(params) : params;
-  const keyParams = config.key ? config.key(...input) : input;
-  return cache.computeKey(keyParams);
-}
-
-function initConfig(config: CoreCacheConfig, target: any, fn: Function) {
-  if (!config.keySpace) {
-    config.keySpace = `${target.constructor.name}.${fn.name}`;
-  }
-}
-
 export function Cache<U extends any>(field: ValidCacheFields<U>, config: CacheConfig = {}): TypedMethodDecorator<U, Promise<any>> {
-  return function (target: U, propertyKey: string, descriptor: TypedPropertyDescriptor<(...params: any[]) => Promise<any>>) {
-    const og = descriptor.value!;
-    initConfig(config, target, og);
-
-    descriptor.value = async function (this: U, ...params: any[]) {
-      const cache = this[field] as any as CacheStore;
-      const key = generateKey(cache, config, params);
-
-      let res = await cache.getOptional(config, key);
-
-      if (res === undefined) {
-        let data = await og.apply(this, params);
-        if (config.serialize) {
-          data = config.serialize(data);
-        }
-        res = (await cache.setWithAge(config, { key, data })).data;
-      }
-
-      if (config.deserialize) {
-        res = config.deserialize(res);
-      }
-
-      return res;
-    };
-    Object.defineProperty(descriptor.value, 'name', { value: propertyKey });
-  };
+  return function (target: U, propertyKey: string, descriptor: TypedPropertyDescriptor<(...params: any[]) => Promise<any>>) { }
 }
 
 export function EvictCache<U extends any>(field: ValidCacheFields<U>, config: CoreCacheConfig = {}): TypedMethodDecorator<U, Promise<any>> {
-  return function (target: U, propertyKey: string, descriptor: TypedPropertyDescriptor<(...params: any[]) => Promise<any>>) {
-    const og = descriptor.value!;
-    initConfig(config, target, og);
-
-    descriptor.value = async function (this: U, ...params: any[]) {
-      const cache = this[field] as any as CacheStore;
-      const key = generateKey(cache, config, params);
-
-      const val = await og.apply(this, params);
-      await cache.delete(key);
-      return val;
-    };
-    Object.defineProperty(descriptor.value, 'name', { value: propertyKey });
-  };
+  return function (target: U, propertyKey: string, descriptor: TypedPropertyDescriptor<(...params: any[]) => Promise<any>>) { };
 }
