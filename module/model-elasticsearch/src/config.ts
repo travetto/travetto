2ee6ecc292f640640756683fdee42251baca4645
Zach Roberts <zach@eaiti.com>
2019-10-03 14:44:24 -0400

    add auth to elasticsearch config

--- module/model-elasticsearch/src/config.ts
+++ module/model-elasticsearch/src/config.ts
@@ -1,6 +1,7 @@
 import { Env } from '@travetto/base';
 import { Config } from '@travetto/config';
 import { EsSchemaConfig } from './types';
+import { BasicAuth, ApiKeyAuth } from '@elastic/elasticsearch/lib/pool';
 
 @Config('elasticsearch.model')
 export class ElasticsearchModelConfig {
@@ -12,6 +13,7 @@ export class ElasticsearchModelConfig {
   schemaConfig: EsSchemaConfig = {
     caseSensitive: false
   };
+  auth: BasicAuth | ApiKeyAuth;
 
   indexCreate = {
     number_of_replicas: 0,
