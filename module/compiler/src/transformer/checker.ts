9a12c129a09a603d586af7b4c9540e39e691d374
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-02-13 09:04:27 -0500

    feat(compiler): Working on type shapes

--- module/compiler/src/transformer/checker.ts
+++ module/compiler/src/transformer/checker.ts
@@ -1,44 +1,33 @@
 import * as ts from 'typescript';
 import { CompilerUtil } from '../util';
-import { Documentation } from './types';
+import { Documentation, Type, ExternalType, RealType, UnionType, TupleType, ShapeType } from './types';
+import { Util } from '@travetto/base';
+
+const GLOBAL_SIMPLE = {
+  RegExp,
+  Date,
+  Number,
+  Boolean,
+  String
+};
+
+const GLOBAL_COMPLEX = {
+  Array,
+  Promise,
+  Set,
+  Map
+};
 
 export class TypeChecker {
 
-  constructor(
-    private checker: ts.TypeChecker,
-    private resolveReference: (type: ts.Type) => ts.Expression
-  ) { }
+  constructor(private checker: ts.TypeChecker) { }
 
   getObjectFlags(type: ts.Type): ts.ObjectFlags {
     return (ts as any).getObjectFlags(type);
   }
 
-  shapeTypeToExpression(type: ts.Type): ts.Expression {
-    const properties: ts.PropertyAssignment[] = [];
-    for (const member of this.checker.getPropertiesOfType(type)) {
-      const memberType = this.checker.getDeclaredTypeOfSymbol(member);
-      properties.push(ts.createPropertyAssignment(ts.createIdentifier(member.getName()), this.typeToExpression(memberType)));
-    }
-    return ts.createObjectLiteral(properties);
-  }
-
-  getPrimaryTypeParameter(ref: ts.Type) {
-    return this.checker.getTypeArguments(ref as any)[0];
-  }
-
-  isPromiseType(type: ts.Type) {
-    /* eslint-disable no-bitwise */
-    const objectFlags = this.getObjectFlags(type) ?? 0;
-    if (objectFlags & ts.ObjectFlags.Reference) {
-      if (type.symbol.getName() === 'Promise') {
-        const fileName = type.symbol.declarations?.[0]?.getSourceFile().fileName;
-        if (fileName.includes('node_modules/typescript/lib')) {
-          return true;
-        }
-      }
-    }
-    return false;
-    /* eslint-enable no-bitwise */
+  getAllTypeArguments(ref: ts.Type) {
+    return this.checker.getTypeArguments(ref as any);
   }
 
   getReturnType(node: ts.MethodDeclaration) {
@@ -46,72 +35,8 @@ export class TypeChecker {
     return this.checker.getReturnTypeOfSignature(sig);
   }
 
-  typeToExpression(type: ts.Type): ts.Expression {
-    /* eslint-disable no-bitwise */
-    let expr: ts.Expression | undefined;
-    const flags = type.getFlags();
-    const objectFlags = this.getObjectFlags(type) ?? 0;
-
-    if (objectFlags & ts.ObjectFlags.Reference) {
-      const fileName = type.symbol.declarations?.[0]?.getSourceFile().fileName;
-      if (type.isClass()) { // Real type
-        expr = this.resolveReference(type);
-      } else { // Is interface
-        if (fileName.includes('node_modules/typescript/lib')) { // Global Type
-          const name = type.symbol.getName();
-          if (name === 'Array') {
-            expr = ts.createArrayLiteral([this.typeToExpression(this.getPrimaryTypeParameter(type))]);
-          } else {
-            expr = ts.createIdentifier(name);
-          }
-        } else { // Not global type
-          // TODO: interface not a class
-          // CompilerUtil.log('Interfaced value', type, fileName);
-        }
-      }
-    } else if ((flags & ts.TypeFlags.StringLiteral) || (flags & ts.TypeFlags.String)) {
-      expr = ts.createIdentifier('String');
-    } else if ((flags & ts.TypeFlags.NumberLiteral) || (flags & ts.TypeFlags.Number)) {
-      expr = ts.createIdentifier('Number');
-    } else if ((flags & ts.TypeFlags.BooleanLiteral) || (flags & ts.TypeFlags.Boolean)) {
-      expr = ts.createIdentifier('Boolean');
-    } else if ((flags & ts.TypeFlags.Void) || (flags & ts.TypeFlags.Undefined)) {
-      expr = ts.createIdentifier('undefined');
-    } else if (type.isUnion()) {
-      const types = type.types.filter(x => (flags & ts.TypeFlags.Undefined) === 0);
-      expr = types.slice(1).reduce((fType, stype) => {
-        const fTypeStr = (fType as any)['text']!;
-        CompilerUtil.log(fTypeStr);
-        if (fTypeStr !== 'Object') {
-          const resolved = this.typeToExpression(stype);
-          if ((resolved as any)['text'] !== fTypeStr) {
-            fType = ts.createIdentifier('Object');
-          }
-        }
-        return fType;
-      }, this.typeToExpression(types[0]));
-
-    } else if (objectFlags & ts.ObjectFlags.Tuple) {
-      expr = ts.createArrayLiteral(type.aliasTypeArguments!.map(t => this.typeToExpression(t)));
-    } else if (type.isLiteral()) {
-      expr = this.shapeTypeToExpression(type);
-    }
-
-    if (!expr) {
-      //  CompilerUtil.log('Unable to type', type);
-    }
-    return expr || ts.createIdentifier('Object');
-    /* eslint-enable no-bitwise */
-  }
-
-  resolveType(node: ts.Node): ts.Expression { // Should get replaced with TypeChecker as needed
-    return this.typeToExpression(this.checker.getTypeAtLocation(node));
-  }
-
-  describeByJSDocs(node: ts.Node) {
-    while ('original' in node) {
-      node = (node as any).original as ts.Node;
-    }
+  readJSDocs(type: ts.Type) {
+    const node = type.getSymbol()?.getDeclarations()![0];
     const tags = ts.getJSDocTags(node);
     const docs = (node as any)['jsDoc'];
 
@@ -131,21 +56,120 @@ export class TypeChecker {
     if (tags && tags.length) {
       for (const tag of tags) {
         if (ts.isJSDocReturnTag(tag)) {
-          out.return = {
-            type: tag.typeExpression && this.resolveType(tag.typeExpression.type),
-            description: tag.comment
-          };
+          out.return = tag.comment;
         } else if (ts.isJSDocParameterTag(tag)) {
           out.params!.push({
             name: tag.name && tag.name.getText(),
-            description: tag.comment ?? '',
-            type: tag.typeExpression && this.resolveType(tag.typeExpression.type),
-            required: !tag.isBracketed
+            description: tag.comment ?? ''
           });
         }
       }
     }
 
     return out;
+  }
+
+  getRealType(type: ts.Type): RealType | undefined {
+    /* eslint-disable no-bitwise */
+    const flags = type.getFlags();
+
+    if (flags & ts.TypeFlags.Void) {
+      return { name: 'void', realType: undefined };
+    } else if (flags & ts.TypeFlags.Undefined) {
+      return { name: 'undefined', realType: undefined };
+    }
+
+    const name = type.symbol?.getName() ?? '';
+    const simpleCons = GLOBAL_SIMPLE[name as keyof typeof GLOBAL_SIMPLE];
+    const complexCons = GLOBAL_COMPLEX[name as keyof typeof GLOBAL_COMPLEX];
+    if (simpleCons) {
+      const ret = Util.coerceType(this.checker.typeToString(type), simpleCons as typeof String);
+      return {
+        realType: simpleCons,
+        value: ret
+      };
+    } else if (complexCons) {
+      return {
+        realType: complexCons,
+        typeArguments: this.getAllTypeArguments(type).map(t => this.resolveType(t))
+      } as RealType;
+    }
+    /* eslint-enable no-bitwise */
+  }
+
+  getShapeType(type: ts.Type): ShapeType {
+    const fields: Record<string, Type> = {};
+    for (const member of this.checker.getPropertiesOfType(type)) {
+      const memberType = this.checker.getDeclaredTypeOfSymbol(member);
+      if (!(memberType.getCallSignatures()?.length)) {
+        fields[member.getName()] = this.resolveType(memberType);
+      }
+    }
+    return { fields };
+  }
+
+  getExternalType(type: ts.Type): ExternalType {
+    const sym = type.symbol;
+    const decl = sym.declarations?.[0];
+    const fileName = decl?.getSourceFile().fileName;
+    const name = sym?.getName();
+    const comments = this.readJSDocs(type);
+
+    return {
+      source: fileName,
+      comment: comments.description,
+      name
+    };
+  }
+
+  resolveType(type: ts.Type | ts.Node): Type {
+    if ('getSourceFile' in type) {
+      type = this.checker.getTypeAtLocation(type);
+    }
+    /* eslint-disable no-bitwise */
+    const flags = type.getFlags();
+    const objectFlags = this.getObjectFlags(type) ?? 0;
+
+    if (objectFlags & (ts.ObjectFlags.Reference | ts.ObjectFlags.Class | ts.ObjectFlags.Interface)) {
+      const obj = this.getExternalType(type);
+
+      if (type.isClass()) { // Real type
+        return obj;
+      } else if (obj.source.includes('typescript/lib')) { // Global Type
+        const ret = this.getRealType(type);
+        if (ret) {
+          return ret;
+        }
+      }
+      return this.getShapeType(type); // Handle fall through on interfaces
+    } else if (flags & (
+      ts.TypeFlags.Boolean | ts.TypeFlags.BooleanLiteral |
+      ts.TypeFlags.Number | ts.TypeFlags.NumberLiteral |
+      ts.TypeFlags.String | ts.TypeFlags.StringLiteral |
+      ts.TypeFlags.Void | ts.TypeFlags.Undefined
+    )) {
+      const res = this.getRealType(type);
+      if (res) {
+        return res;
+      }
+    } else if (type.isUnion()) {
+      const types = type.types;
+      return {
+        optional: types.find(x => (flags & ts.TypeFlags.Undefined) > 0),
+        unionTypes: types.map(x => this.resolveType(x))
+      } as UnionType;
+    } else if (objectFlags & ts.ObjectFlags.Tuple) {
+      return {
+        tupleTypes: this.getAllTypeArguments(type).map(t => this.resolveType(t))
+      } as TupleType;
+    } else if (type.isLiteral()) {
+      return this.getShapeType(type);
+    }
+
+    return {
+      realType: Object,
+      name: 'object'
+    } as RealType;
+    /* eslint-enable no-bitwise */
   }
 }
