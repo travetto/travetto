0db137504bd6c17f93f0c1443f7ca0afe1360eba
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-05-11 09:12:33 -0400

    feat(common): Unifying color support

---
+++ module/boot/test/color.ts
@@ -0,0 +1,50 @@
+import * as assert from 'assert';
+
+import { Test, Suite, BeforeEach } from '@travetto/test';
+import { ColorUtil } from '../src';
+
+@Suite()
+export class ColorUtilTest {
+  @BeforeEach()
+  init() {
+    ColorUtil['colorize'] = true;
+  }
+
+  @Test()
+  async colorize() {
+    const output = ColorUtil.color('red', ['bold', 'underline'], 'apple');
+    assert(output !== 'apple');
+    assert(/apple/.test(output));
+    assert(output.includes('\x1b[22'));
+  }
+
+  @Test()
+  async noColor() {
+    ColorUtil['colorize'] = false;
+    const output = ColorUtil.color('red', ['bold', 'underline'], 'apple');
+    assert(output === 'apple');
+    assert(!output.includes('\x1b[22'));
+  }
+
+  @Test()
+  async template() {
+    const tpl = ColorUtil.makeTemplate({
+      name: ColorUtil.makeColorer('blue', 'bold'),
+      age: ColorUtil.makeColorer('red', 'faint')
+    });
+
+    const output = tpl`My name is ${{ name: 'Bob' }} and I'm ${{ age: 20 }} years old`;
+    assert(/My name is.*Bob.*and I'm.*20.*years old/.test(output));
+    assert(output.includes('\x1b[22'));
+  }
+
+  @Test({ shouldThrow: 'Invalid template' })
+  badTemplate() {
+    const tpl = ColorUtil.makeTemplate({
+      name: ColorUtil.makeColorer('blue', 'bold'),
+      age: ColorUtil.makeColorer('red', 'faint')
+    });
+
+    tpl`My name is ${{ name: 'Bob', age: 20 }}`;
+  }
+}
--- module/cli/src/color.ts
+++ module/cli/src/color.ts
@@ -1,53 +1,21 @@
-import { EnvUtil } from '@travetto/boot';
-
-const Codes = {
-  blue: `\x1b[94m`,
-  yellow: `\x1b[93m`,
-  green: `\x1b[92m`,
-  gray: `\x1b[37m\x1b[2m`,
-  red: `\x1b[31m`,
-  cyan: `\x1b[96m`,
-  magenta: `\x1b[95m`,
-  white: `\x1b[97m\x1b[1m`,
-  reset: `\x1b[0m`,
-};
-
-let colorize: boolean;
-
-function colorizeAny(col: string, value: string | number | boolean): string;
-function colorizeAny(col: string, value: any) {
-  if (colorize === undefined) {
-    // Load on demand, synchronously
-    colorize = EnvUtil.isSetTrueOrFalse('FORCE_COLOR', 'NO_COLOR', process.stdout.isTTY);
-  }
-  if (colorize && value !== undefined && value !== null && value !== '') {
-    value = `${col}${value}${Codes.reset}`;
-  }
-  return value;
-}
-
-function c(key: string) {
-  return (v: any) => colorizeAny(key, v);
-}
+import { ColorUtil } from '@travetto/boot';
 
 /**
  * Map of common keys to specific colors
  */
 export const Colors = {
-  input: c(Codes.yellow),
-  output: c(Codes.magenta),
-  path: c(Codes.white),
-  success: c(Codes.green),
-  failure: c(Codes.red),
-  param: c(Codes.green),
-  type: c(Codes.blue),
-  description: c(Codes.gray),
-  title: c(Codes.white),
-  identifier: c(Codes.blue),
-  subtitle: c(Codes.gray)
+  input: ColorUtil.makeColorer('yellow'),
+  output: ColorUtil.makeColorer('magenta'),
+  path: ColorUtil.makeColorer('cyan', 'bold'),
+  success: ColorUtil.makeColorer('green', 'bold'),
+  failure: ColorUtil.makeColorer('red', 'bold'),
+  param: ColorUtil.makeColorer('green', 'bold'),
+  type: ColorUtil.makeColorer('blue', 'bold'),
+  description: ColorUtil.makeColorer('white', 'faint', 'bold'),
+  title: ColorUtil.makeColorer('white', 'bold'),
+  identifier: ColorUtil.makeColorer('blue', 'bold'),
+  subtitle: ColorUtil.makeColorer('white', 'faint')
 };
-
-type Color = keyof typeof Colors;
 
 /**
  * Colorize a string, as a string interpolation
@@ -57,21 +25,4 @@ type Color = keyof typeof Colors;
  * color`${{title: 'Main Title'}} is ${{subtitle: 'Sub Title}}`
  * ```
  */
-export function color(values: TemplateStringsArray, ...keys: (Partial<Record<Color, any>> | string)[]) {
-  if (keys.length === 0) {
-    return values[0];
-  } else {
-    const out = keys.map((el, i) => {
-      const subKeys = Object.keys(el);
-      if (subKeys.length === 1 && subKeys[0] in Colors) {
-        const [k] = subKeys as Color[];
-        el = Colors[k]((el as any)[k]);
-      }
-      return `${values[i] ?? ''}${el ?? ''}`;
-    });
-    if (values.length > keys.length) {
-      out.push(values[values.length - 1]);
-    }
-    return out.join('');
-  }
-}
+export const color = ColorUtil.makeTemplate(Colors);
