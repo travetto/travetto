9eef534f2179a51e5fa54447d2aac99423dc917f
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-05-17 18:38:15 -0400

    fix(rest): Typings

--- module/rest/src/registry/types.ts
+++ module/rest/src/registry/types.ts
@@ -14,29 +14,79 @@ export interface EndpointSimpleType {
 
 export type EndpointIOType = EndpointClassType | EndpointSimpleType;
 
+/**
+ * Describable elementes
+ */
 export interface DescribableConfig {
+  /**
+   * The title
+   */
   title?: string;
+  /**
+   * Description
+   */
   description?: string;
 }
 
+/**
+ * Core configuration for endpoints and controllers
+ */
 interface CoreConfig {
+  /**
+   * The related class
+   */
   class: Class;
+  /**
+   * The actual class instance
+   */
   instance?: any;
-
+  /**
+   * List of filters to run on request
+   */
   filters: Filter[];
+  /**
+   * List of headers to add to the response
+   */
   headers: HeaderMap;
 }
 
+/**
+ * Endpoint configuration
+ */
 export interface EndpointConfig extends RouteConfig, CoreConfig, DescribableConfig {
+  /**
+   * Unique identifier
+   */
   id: string;
+  /**
+   * Endpoint priority, used for conflicting routes
+   */
   priority: number;
+  /**
+   * The name of the method
+   */
   handlerName: string;
+  /**
+   * The response type
+   */
   responseType?: EndpointIOType;
+  /**
+   * The request type
+   */
   requestType?: EndpointIOType;
 }
 
+/**
+ * Controller configuration
+ */
 export interface ControllerConfig extends CoreConfig, DescribableConfig {
+  /**
+   * The base path of the controller
+   */
   basePath: string;
+  /**
+   * List of all endpoints
+   */
   endpoints: EndpointConfig[];
 }
 
