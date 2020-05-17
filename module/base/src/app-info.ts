1152e2c0cb1d8a37cab59a710c3dac7afa9416a6
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-05-17 15:22:50 -0400

    fix(common): Fixing app info, simplifying

--- module/base/src/app-info.ts
+++ module/base/src/app-info.ts
@@ -10,10 +10,7 @@ export const AppInfo = {
   travetto: require('../package.json').version, // Travetto version
   version: pkg.version as string,
   name: pkg.name as string,
-  simpleName: pkg.name.replace(/[@]/g, '').replace(/[\/]/g, '_') as string,
-  package: pkg.name.split('/')[0] as string,
   license: pkg.license as string,
   author: pkg.author as { email: string, name: string },
-  main: pkg.main as string,
-  description: pkg.description as string
+  description: pkg.description as string || 'A Travetto application'
 };
