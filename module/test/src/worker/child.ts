ffdf80eeff2d761e3d9033d8d2fe310c89e69291
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-05-31 19:56:23 -0400

    feat(test): Removing idle support, watcher does this now

--- module/test/src/worker/child.ts
+++ module/test/src/worker/child.ts
@@ -1,28 +1,15 @@
 import type { Compiler } from '@travetto/compiler';
-import { FsUtil, EnvUtil } from '@travetto/boot';
 import { ErrorUtil } from '@travetto/base/src/internal/error';
-import { PhaseManager, ShutdownManager } from '@travetto/base';
+import { PhaseManager, ShutdownManager, ScanApp, AppManifest } from '@travetto/base';
 import { ChildCommChannel } from '@travetto/worker';
 import { Events, RunEvent } from './types';
 
 const FIXED_MODULES = new Set([
   //  'cache', 'openapi',
   'boot', 'base', 'cli', 'config', 'compiler', 'yaml',
-  'worker', 'exec', 'log', 'net', 'jwt', 'image', 'test',
+  'worker', 'command', 'log', 'net', 'jwt', 'image', 'test',
   // 'registry'
-]);
-const IS_SUPPORT_FILE = /support\/(transformer|phase)[.]/;
-const IS_BIN_FILE = '/bin/';
-// @ts-ignore
-const IS_SELF_FILE = __filename.ᚕunix.replace(/.*(test\/.*)([.][tj]s)?$/, (__, name) => name);
-
-/**
- * Get module name from file, ignore node_modules nested
- * Look at :
- *  - src
- *  - index
- */
-const EXTRACT_FILE_MODULE = /^.*travetto[^/]*\/(?:module\/)?([^/]+)\/(?:src\/.*|support\/.*|index[.]ts)$/;
+].map(x => `@travetto/${x}`));
 
 /**
  * Child Worker for the Test Runner.  Receives events as commands
@@ -34,7 +21,7 @@ export class TestChildWorker extends ChildCommChannel<RunEvent> {
   private runs = 0;
 
   constructor() {
-    super(EnvUtil.getTime('TRV_TEST_IDLE_TIMEOUT', 2, 'm'));
+    super();
 
     (async () => {
       const { TestUtil } = await import('../runner/util');
@@ -84,31 +71,14 @@ export class TestChildWorker extends ChildCommChannel<RunEvent> {
   }
 
   /**
-   * Determine if a file is able to be reset from the require cache
-   */
-  isFileResettable(filePath: string) {
-    const frameworkModule = filePath.replace(EXTRACT_FILE_MODULE, (__, mod) => mod);
-
-    return filePath.endsWith('.ts') && (!filePath.endsWith('.d.ts')) && // Only look at .ts files
-      (!frameworkModule || // A user file
-        (
-          !FIXED_MODULES.has(frameworkModule) && // Not a core module
-          !IS_SUPPORT_FILE.test(filePath) && // Not a support file
-          !filePath.includes(IS_BIN_FILE) && // Not a bin file
-          !filePath.includes(IS_SELF_FILE) // Not self
-        )
-      );
-  }
-
-  /**
    * Reset the state to prepare for the next run
    */
   async resetForRun() {
     // Clear require cache of all data loaded minus base framework pieces
     console.debug('Resetting', Object.keys(require.cache).length);
-
-    for (const file of Object.keys(require.cache).map(FsUtil.toUnix)) {
-      if (this.isFileResettable(file)) {
+    const paths = ScanApp.getPaths(AppManifest.roots).filter(x => !FIXED_MODULES.has(x));
+    for (const { file } of ScanApp.findFiles({ paths, includeIndex: true })) {
+      if (!/support\/(transformer|phase)[.]/.test(file)) {
         console.debug(`[${process.pid}]`, 'Unloading', file);
         this.compiler.unload(file);
       }
