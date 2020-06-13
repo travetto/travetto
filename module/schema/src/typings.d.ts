37501aeb424221ce3025cee5ed2d382876209060
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-06-13 01:00:26 -0400

    feat(common): Reusing types

--- module/schema/src/typings.d.ts
+++ module/schema/src/typings.d.ts
@@ -1,13 +1,17 @@
+import type { Class } from '@travetto/registry/src/types';
+
 type DeepPartial<T> = {
   [P in keyof T]?: (T[P] extends (number | string | Date | boolean | undefined) ? (T[P] | undefined) :
     (T[P] extends any[] ? (DeepPartial<T[P][number]> | null | undefined)[] : DeepPartial<T[P]>));
 };
 
-declare interface Function {
-  /**
-   * Will produce a new instance of this class with the provided data bound to it
-   * @param data The data to bind
-   * @param view The optional view to limit the bind to
-   */
-  from<T>(this: { new(...args: any[]): T }, data: DeepPartial<T>, view?: string): T;
+declare global {
+  interface Function {
+    /**
+     * Will produce a new instance of this class with the provided data bound to it
+     * @param data The data to bind
+     * @param view The optional view to limit the bind to
+     */
+    from<T>(this: Class<T>, data: DeepPartial<T>, view?: string): T;
+  }
 }
