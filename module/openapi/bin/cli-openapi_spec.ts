285c078dca30682a0dd699b7478ddbc84794979c
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-07-18 13:35:38 -0400

    feat(openapi): Fixing client generation using published docker container

--- module/openapi/bin/cli-openapi_spec.ts
+++ module/openapi/bin/cli-openapi_spec.ts
@@ -1,28 +1,24 @@
 import * as commander from 'commander';
 import { BasePlugin } from '@travetto/cli/src/plugin-base';
+import { ExecUtil } from '@travetto/boot';
+import { CliUtil } from '@travetto/cli/src/util';
 
 /**
  * CLI for outputting the open api spec to a local file
  */
 export class OpenApiSpecPlugin extends BasePlugin {
   name = 'openapi:spec';
+
   init(cmd: commander.Command) {
     return cmd.option('-o, --output [output]', 'Output files', './openapi.yml');
   }
 
   async action() {
-    process.env.API_SPEC_OUTPUT = this._cmd.output;
+    CliUtil.initAppEnv({ watch: false, debug: '0', envExtra: { API_SPEC_OUTPUT: this._cmd.output } });
+    const result = await ExecUtil.worker(require.resolve('./plugin-openapi_spec')).message;
 
-    const { PhaseManager } = await import('@travetto/base');
-    await PhaseManager.init();
-
-    const { DependencyRegistry } = await import('@travetto/di');
-    const { OpenApiService } = await import('../src/service');
-
-    const instance = await DependencyRegistry.getInstance(OpenApiService);
-    const spec = await instance.spec;
     if (this._cmd.output === '-' || !this._cmd.output) {
-      console.log!(spec);
+      console.log!(result);
     }
   }
 }
