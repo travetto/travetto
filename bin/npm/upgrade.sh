da65988e4a636226ca3c1d722b654b56536ba900
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-09-19 15:14:32 -0400

    chore(deps): Upgrading dependencies

--- bin/npm/upgrade.sh
+++ bin/npm/upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-node -r './bin/npm/.env.js' -r './module/boot/register' -e 'require("./bin/npm/ugprade.ts").run()'
+node -r './bin/npm/.env.js' -r './module/boot/register' -e 'require("./bin/npm/upgrade.ts").run()'
