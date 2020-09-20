25d56d58f98031abc18856a4673c4d51e86e4fdd
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-09-20 18:32:34 -0400

    chore(boot): Removing comments from boot .js files to drop unpacked size by 60%

--- bin/npm/prepare.sh
+++ bin/npm/prepare.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 # Compile boot
-pushd module/boot/src-ts
+pushd module/boot/src-ts > /dev/null
 if [ "$1" == "watch" ]; then
   tsc -w -p . &
 else
   tsc -p .
 fi
-popd
+popd > /dev/null
+
+if [ ! "$1" == "watch" ]; then
+  pushd module/boot/src > /dev/null
+  for f in *.js; do 
+    cat $f |\
+      sed '/\/\*/,/*\//d' |\
+      perl -pe 's|//.*$||g' |\
+      perl -pe 's|^\s+||g' |\
+      perl -pe 's|^$||g' \
+      > $f.clean
+    mv $f.clean $f
+  done
+  popd > /dev/null
+fi
 
 # Wait for all background jobs
 if [ "$1" == "watch" ]; then
