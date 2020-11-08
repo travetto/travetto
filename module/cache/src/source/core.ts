64a611f2785f7cc512b1b42b5000177dee718313
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-11-08 10:41:27 -0500

    feat(cache): Aligning with new model structure

--- module/cache/src/source/core.ts
+++ module/cache/src/source/core.ts
@@ -1,73 +1,95 @@
-import { CacheEntry, CacheConfig, ICacheSource } from '../types';
+import { AppError } from '@travetto/base';
+import { Class } from '@travetto/registry';
+import { ModelExpirable, ModelType } from '@travetto/model-core';
+
+import { CacheConfig } from '../types';
 import { CacheSourceUtil } from './util';
 import { CacheError } from './error';
-
-type OrProm<T> = T | Promise<T>;
 
 /**
  * Cache source
  */
-export abstract class CacheSource<T extends CacheEntry = CacheEntry> implements ICacheSource<T> {
-  abstract get(key: string): OrProm<T | undefined>;
-  abstract has(key: string): OrProm<boolean>;
-  abstract set(key: string, entry: T): OrProm<T>;
-  abstract isExpired(key: string): OrProm<boolean>;
-  abstract touch(key: string, expiresAt: number): OrProm<boolean>;
-  abstract delete(key: string): OrProm<boolean>;
-  abstract keys(): OrProm<Iterable<string>>;
+export class CacheService<T extends ModelType>  {
 
-  clear?(): OrProm<void> | void;
-  postConstruct?(): OrProm<void>;
+  /**
+   * Time of last culling
+   */
+  lastCullCheck = Date.now();
+  /**
+   * Cull rate
+   */
+  cullRate = 10 * 60000; // 10 minutes
+
+  constructor(private cls: Class<T>, private modelService: ModelExpirable) { }
 
   computeKey(params: any) {
     return CacheSourceUtil.computeKey(params);
   }
 
   async getAndCheckAge(config: CacheConfig, key: string) {
-    const entry = await this.get(key);
-    const now = Date.now();
-    if (entry === undefined) { // Missing
-      throw new CacheError('Key not found', 'notfound');
-    }
-    if (entry.expiresAt && entry.expiresAt < now) {
-      await this.delete(key);
+    const { expiresAt, expired, maxAge } = await this.modelService.getExpiry(this.cls, key);
+
+    if (expired) {
+      await this.modelService.delete(this.cls, key);
       throw new CacheError('Key expired', 'data');
     }
 
     // If half way to eviction, not perfect, but will reduce the amount of unnecessary updates
-    if (config.extendOnAccess && entry.maxAge && entry.expiresAt) {
-      const delta = entry.expiresAt - now;
-      const threshold = entry.maxAge / 2;
+    if (config.extendOnAccess) {
+      const delta = expiresAt - Date.now();
+      const threshold = maxAge / 2;
       if (delta < threshold) {
-        await this.touch(key, Date.now() + entry.maxAge); // Do not wait
+        await this.modelService.setExpiry(this.cls, key, maxAge); // Do not wait
       }
     }
 
-    return entry.data;
+    return await this.modelService.get(this.cls, key);
   }
 
-  setWithAge(config: CacheConfig, entry: Partial<T> & { data: any, key: string }) {
-    return this.set(entry.key, {
-      ...entry,
-      issuedAt: Date.now(),
-      expiresAt: config.maxAge ? (Date.now() + config.maxAge) : undefined,
-      maxAge: config.maxAge,
-    } as T);
+  async setWithAge(config: CacheConfig, key: string, entry: T) {
+    entry.id = key;
+
+    let store;
+
+    if (config.maxAge) {
+      store = await this.modelService.upsertWithExpiry(this.cls, entry, config.maxAge);
+    } else {
+      store = await this.modelService.upsert(this.cls, entry);
+    }
+
+    return store;
   }
 
   async getOptional(config: CacheConfig, key: string) {
     let res: any;
-    const has = await this.has(key);
 
-    if (has) {
-      try {
-        res = await this.getAndCheckAge(config, key);
-      } catch (err) {
-        if (!(err instanceof CacheError)) {
-          throw err;
-        }
+    try {
+      res = await this.getAndCheckAge(config, key);
+    } catch (err) {
+      if (!(err instanceof CacheError) && !(err instanceof AppError && err.category !== 'notfound')) {
+        throw err;
       }
     }
     return res;
+  }
+
+  /**
+   * Cull expired data
+   */
+  async cull(force = false) {
+    if (!this.modelService.deleteExpired || (!force && (Date.now() - this.lastCullCheck) < this.cullRate)) {
+      return;
+    }
+
+    this.lastCullCheck = Date.now();
+    await this.modelService.deleteExpired(this.cls);
+  }
+
+  /**
+   * Evict an entry by key
+   * @param key The key to evict
+   */
+  async evict(key: string) {
+    return this.modelService.delete(this.cls, key);
   }
 }
