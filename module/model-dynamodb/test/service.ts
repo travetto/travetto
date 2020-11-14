1a8cc993719c0ffc6debb744bc0a0ac400b06dcc
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-11-14 18:16:21 -0500

    feat(model-s3): Adding in s3 support

---
+++ module/model-dynamodb/test/service.ts
@@ -0,0 +1,18 @@
+import { Suite } from '@travetto/test';
+import { DynamoDBModelConfig, DynamoDBModelService } from '..';
+import { ModelCrudSuite } from '@travetto/model-core/test/lib/crud';
+import { ModelExpirySuite } from '@travetto/model-core/test/lib/expiry';
+
+@Suite()
+export class DynamoDBCrudSuite extends ModelCrudSuite {
+  constructor() {
+    super(DynamoDBModelService, DynamoDBModelConfig);
+  }
+}
+
+// @Suite()
+// export class DynamoDBExpirySuite extends ModelExpirySuite {
+//   constructor() {
+//     super(DynamoDBModelService, DynamoDBModelConfig);
+//   }
+// }
