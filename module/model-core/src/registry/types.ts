e1706d3357f804090a662e6443f863e2d07107af
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-11-21 09:35:23 -0500

    feat(model-core): Working on indexing

--- module/model-core/src/registry/types.ts
+++ module/model-core/src/registry/types.ts
@@ -1,5 +1,21 @@
 import { Class } from '@travetto/registry';
 
+type Primitive = number | boolean | string | Date;
+
+type ValidFieldNames<T> = {
+  [K in keyof T]:
+  (T[K] extends (Primitive | undefined) ? K :
+    (T[K] extends (Function | undefined) ? never :
+      K))
+}[keyof T];
+
+type RetainFields<T> = Pick<T, ValidFieldNames<T>>;
+
+type SortClauseRaw<T> = {
+  [P in keyof T]?:
+  T[P] extends object ? SortClauseRaw<RetainFields<T[P]>> : (1 | -1 | boolean);
+};
+
 /**
  * Model options
  */
@@ -38,12 +54,12 @@ export interface IndexConfig<T> {
   /**
    * Index name
    */
-  name?: string;
+  name: string;
 
   /**
    * Fields and sort order
    */
-  fields: Record<keyof T, -1 | 1>[];
+  fields: SortClauseRaw<T>[];
 
   /**
    * Is the index unique?
