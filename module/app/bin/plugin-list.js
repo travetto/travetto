518b1a4da46e0947b38851a988d4dddf31b3cc42
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2020-12-17 08:06:36 -0500

    Fixing launching

--- module/app/bin/plugin-list.js
+++ module/app/bin/plugin-list.js
@@ -1,4 +1,4 @@
 // Entry point for the direct execution
 process.env.TRV_DEBUG = process.env.TRV_DEBUG || '0';
 require('@travetto/boot/register');
-require('./lib/list').AppListManager.run(...process.argv.slice(2));
+require('./lib/list').CliAppListUtil.run(...process.argv.slice(2));
