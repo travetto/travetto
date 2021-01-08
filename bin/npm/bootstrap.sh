564bb63d9c75f6b666b2cafbcf02d096fc3ce43b
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2021-01-08 18:34:17 -0500

    chore(npm): Streamlining scripts

--- bin/npm/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo -n "Lerna clean ... "
-npx lerna clean --yes
-echo "done."
-echo -n "Lerna bootstrap ... "
-npx lerna bootstrap --hoist
-find module -name @travetto -type d | xargs rm -rf
-echo "done."
-rm -f package-lock.json
---
+++ bin/npm/hoist.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+npx lerna bootstrap --hoist
