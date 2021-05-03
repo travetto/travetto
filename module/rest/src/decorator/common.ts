9dc5c3845a27244d778e3a10367ef72e90546b6e
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2021-05-02 21:01:21 -0400

    fix(rest): Ensuring cache-control is in seconds

--- module/rest/src/decorator/common.ts
+++ module/rest/src/decorator/common.ts
@@ -37,9 +37,9 @@ type HeaderSet = ReturnType<typeof SetHeaders>;
 export function CacheControl(value: RelativeTime): HeaderSet;
 export function CacheControl(value: number, unit?: TimeUnit): HeaderSet;
 export function CacheControl(value: number | RelativeTime, unit: TimeUnit = 's'): HeaderSet {
-  const delta = TimeUtil.toMillis(value as number, unit);
+  const delta = Math.trunc(TimeUtil.toMillis(value as number, unit) / 1000);
   return SetHeaders({
-    Expires: delta === 0 ? '-1' : () => `${new Date(delta + Date.now()).toUTCString()}`,
+    Expires: delta === 0 ? '-1' : () => new Date(delta * 1000 + Date.now()).toUTCString(),
     'Cache-Control': () => delta === 0 ? 'max-age=0,no-cache' : `max-age=${delta}`
   });
 }
---
+++ module/rest/test/decorator.ts
@@ -0,0 +1,36 @@
+import * as assert from 'assert';
+
+import { Suite, Test, BeforeAll } from '@travetto/test';
+import { RootRegistry } from '@travetto/registry';
+import { Util } from '@travetto/base';
+
+import { ControllerRegistry, CacheControl } from '../';
+import { Controller } from '../src/decorator/controller';
+import { Patch } from '../src/decorator/endpoint';
+
+@Controller('/test')
+class TestController {
+  @CacheControl('1s')
+  @Patch()
+  async patch() { }
+}
+
+@Suite()
+export class ConfigureTest {
+
+  @BeforeAll()
+  async init() {
+    await RootRegistry.init();
+  }
+
+  @Test()
+  async verifyMaxAge() {
+    const cacher = ControllerRegistry.get(TestController).endpoints[0].headers['Cache-Control'];
+    assert(Util.isFunction(cacher));
+    assert(cacher() === 'max-age=1');
+
+    const expires = ControllerRegistry.get(TestController).endpoints[0].headers['Expires'];
+    assert(Util.isFunction(expires));
+    assert(expires() === new Date(1000 + Date.now()).toUTCString());
+  }
+}
