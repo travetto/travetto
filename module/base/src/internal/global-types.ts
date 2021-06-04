d9cc781a9e038fcb525472a75e9404d3f0c08987
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2021-06-03 20:13:26 -0400

    feat(boot): Allowing flexibility on prototype methods

--- module/base/src/internal/global-types.ts
+++ module/base/src/internal/global-types.ts
@@ -1,3 +1,4 @@
+import { EnvUtil } from '@travetto/boot/src';
 import { StacktraceUtil } from '../stacktrace';
 
 declare global {
@@ -10,41 +11,40 @@ declare global {
 
 export type Primitive = number | boolean | string | Date | Error;
 
+function addFn(proto: object, name: string, fn: Function) {
+  Object.defineProperty(proto, name, { configurable: !EnvUtil.isReadonly(), writable: false, enumerable: false, value: fn });
+}
+
 // Enable maps to be serialized as json
-Map.prototype.toJSON = function (this: Map<unknown, unknown>) {
+addFn(Map.prototype, 'toJSON', function (this: Map<unknown, unknown>) {
   const out = {} as Record<string, unknown>;
   for (const [k, v] of this.entries()) {
     out[typeof k === 'string' ? k : `${k}`] = v;
   }
   return out;
-};
+});
 
 // Enable sets to be serialized as JSON
-Set.prototype.toJSON = function (this: Set<unknown>) {
+addFn(Set.prototype, 'toJSON', function (this: Set<unknown>) {
   return [...this.values()];
-};
+});
 
 // Add .toJSON to the default Error as well
-Error.prototype.toJSON = function (extra?: Record<string, unknown>) {
+addFn(Error.prototype, 'toJSON', function (this: Error, extra?: Record<string, unknown>) {
   const stack = StacktraceUtil.simplifyStack(this);
   return {
     message: this.message,
     ...extra,
     stack: stack.substring(stack.indexOf('\n') + 1)
   };
-};
+});
 
 // Add .toArray to async iterables
 const proto = Object.getPrototypeOf(Object.getPrototypeOf((async function* () { })()));
-Object.defineProperty(proto, 'toArray', {
-  configurable: false,
-  writable: false,
-  enumerable: false,
-  async value() {
-    const out = [];
-    for await (const item of this) {
-      out.push(item);
-    }
-    return out;
+addFn(proto, 'toArray', async function (this: AsyncGenerator<any>) {
+  const out = [];
+  for await (const item of this) {
+    out.push(item);
   }
+  return out;
 });
