af46dbbc8217c9ca58e27fa4d60eeadb11be998a
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-06-27 10:25:30 -0400

    chore(test): Fixing test to work more reliably

--- module/command/test/docker.ts
+++ module/command/test/docker.ts
@@ -6,7 +6,7 @@ import { DockerContainer, CommandUtil } from '@travetto/command';
 @Suite()
 export class DockerTest {
 
-  @Test()
+  @Test({ timeout: 15000 })
   async test() {
     const port = Math.trunc(Math.random() * 40000) + 10000;
     const container = new DockerContainer('nginx:latest')
@@ -14,6 +14,7 @@ export class DockerTest {
       .forceDestroyOnShutdown();
     try {
       container.run();
+      await new Promise(r => setTimeout(r, 3000));
       await assert.doesNotReject(() => CommandUtil.waitForPort(port));
     } finally {
       await container.stop();
