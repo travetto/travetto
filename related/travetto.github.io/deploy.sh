9e8d77eb0806f0276ac95e464f819df7edababe5
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-08-06 10:05:39 -0400

    chore(website): Upgrading to angular 14

--- related/travetto.github.io/deploy.sh
+++ related/travetto.github.io/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf dist/*
-npx ng build --prod
+npx ng build -c production
 aws s3 sync --exclude index.html --metadata-directive REPLACE --cache-control 'public,max-age=31557600' dist/ s3://travetto-website
 aws s3 sync --include index.html dist/ s3://travetto-website
 aws cloudfront create-invalidation --distribution-id E340S3PX1SNG1Y --paths '/*'
