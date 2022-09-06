8eba1dca93f6da8212c53af338f19dd2ca08be25
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-09-05 21:25:57 -0400

    fix(auth-rest): Fix bad naming on max age field, and make id required

--- module/auth-rest/src/internal/principal-target-schema.ts
+++ module/auth-rest/src/internal/principal-target-schema.ts
@@ -2,10 +2,10 @@ import { SchemaRegistry } from '@travetto/schema';
 import { PrincipalTarget } from '@travetto/auth/src/internal/types';
 
 const optional = (): { required: { active: false } } => ({ required: { active: false } });
-SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, 'id', String, {});
+SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, 'id', String, { required: { active: true } });
 SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, 'expiresAt', Date, optional());
 SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, 'issuedAt', Date, optional());
-SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, 'mageAge', Number, optional());
+SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, 'maxAge', Number, optional());
 SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, 'issuer', String, optional());
 SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, 'permissions', [String], optional());
 SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, 'details', Object, optional());
