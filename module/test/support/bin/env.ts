e38607889257859a0999d5d1125c2391ca997cb9
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-10-02 08:04:12 -0400

    chore(test): Cleaning up common code

---
+++ module/test/support/bin/env.ts
@@ -0,0 +1,16 @@
+import { EnvInit } from '@travetto/base/support/bin/init';
+import { Host } from '@travetto/boot';
+
+export function envInit(localOptional = true, dynamic: boolean = false): void {
+  EnvInit.init({
+    ...(dynamic ? { dynamic } : {}),
+    debug: '0',
+    set: { TRV_LOG_TIME: '0' },
+    append: {
+      TRV_RESOURCES: `${Host.PATH.test}/${Host.PATH.resources}`,
+      TRV_PROFILES: 'test',
+      TRV_SRC_LOCAL: `${localOptional ? '^' : ''}${Host.PATH.test}`,
+      TRV_SRC_COMMON: `^${Host.PATH.testSupport}`
+    }
+  });
+}
--- module/test/support/main.test-direct.ts
+++ module/test/support/main.test-direct.ts
@@ -1,20 +1,9 @@
-import { EnvInit } from '@travetto/base/support/bin/init';
-import { Host } from '@travetto/boot';
-
+import { envInit } from './bin/env';
 import { runTests } from './bin/run';
 
 // Direct entry point
 export function main(...args: string[]): Promise<void> {
-  EnvInit.init({
-    debug: '0',
-    set: { TRV_LOG_TIME: '0' },
-    append: {
-      TRV_RESOURCES: `${Host.PATH.test}/${Host.PATH.resources}`,
-      TRV_PROFILES: 'test',
-      TRV_SRC_LOCAL: `^${Host.PATH.test}`,
-      TRV_SRC_COMMON: `^${Host.PATH.testSupport}`
-    }
-  });
+  envInit();
 
   return runTests({
     args,
