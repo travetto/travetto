a1ac6e644a153fd393c6f37ed6ec592da04b4a92
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-10-17 08:07:05 -0400

    chore(test): Removing final traces of "isolated" tests

--- related/vscode-plugin/src-ts/feature/log/console/index.ts
+++ related/vscode-plugin/src-ts/feature/log/console/index.ts
@@ -40,14 +40,14 @@ export class LogFeature extends BaseFeature {
     const cwd = await fs.readlink(`/proc/${await context.terminal.processId}/cwd`);
     context.line
       // Log reference
-      .replace(/(@trv:[^\/]+|(?:[.][/])(?:src|support|bin|test(?:-support|-isolated)?))\/([a-z\/.\-]+)(:\d+|￮[$A-Z][a-zA-Z0-9]+)/g, (_, mod, path, suffix) => {
+      .replace(/(@trv:[^\/]+|(?:[.][/])(?:src|support|bin|test)\/([a-z\/.\-]+)(:\d+|￮[$A-Z][a-zA-Z0-9]+)/g, (_, mod, path, suffix) => {
         let file: string;
         if (!path.endsWith('.ts')) {
           path = `${path}.ts`;
         }
         if (mod.startsWith('@trv')) {
           mod = mod.split('@trv:')[1];
-          if (!/^(support|bin|test|test(?:-support|-isolated)?)/.test(path)) {
+          if (!/^(support|bin|test|test)/.test(path)) {
             path = `src/${path}`;
           }
           file = Workspace.resolve(`@travetto/${mod}/${path}`);
