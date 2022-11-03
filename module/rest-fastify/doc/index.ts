3c6ecf971de63f8947162372fd4994e650ccbe60
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-11-03 18:46:22 -0400

    chore(doc): Moving rest docs to sub folder

--- module/rest-fastify/doc/index.ts
+++ module/rest-fastify/doc/index.ts
@@ -8,10 +8,10 @@ The module is an ${lib.Fastify} provider for the ${mod.Rest} module.  This modul
 
 ${d.Section('Customizing Rest App')}
 
-${d.Code('Customizing the Fastify App', 'doc/customize.ts')}
+${d.Code('Customizing the Fastify App', 'src/customize.ts')}
 
 ${d.Section('Default Middleware')}
 When working with an ${lib.Fastify} applications, the module provides what is assumed to be a sufficient set of basic filters. Specifically:
 
-${d.Snippet('Configured Middleware', 'src/server.ts', /const app\s*=/, /addContentTypeParser/)}
+${d.Snippet('Configured Middleware', '@travetto/rest-fastify/src/server.ts', /const app\s*=/, /addContentTypeParser/)}
 `;
--- module/rest-model/doc/index.ts
+++ module/rest-model/doc/index.ts
@@ -1,15 +1,15 @@
 import { d, mod } from '@travetto/doc';
 
-import { ModelRoutes } from './src/model';
+import { ModelRoutes } from '@travetto/rest-model';
 
 export const text = d`
 ${d.Header()}
 
 To facilitate common RESTful patterns, the module exposes  ${mod.Model} support in the form of ${ModelRoutes}.
 
-${d.Code('ModelRoutes example', 'doc/controller-with-model.ts')}
+${d.Code('ModelRoutes example', 'src/controller-with-model.ts')}
 
 is a shorthand that is equal to:
 
-${d.Code('Comparable UserController, built manually', 'doc/controller-without-model.ts')}
+${d.Code('Comparable UserController, built manually', 'src/controller-without-model.ts')}
 `;
