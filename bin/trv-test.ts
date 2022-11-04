ad31e90933acbb8cf57ae177e72f194c7dcf9a31
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-11-04 08:33:26 -0400

    chore(test): Making all tests runnable again

--- bin/trv-test.ts
+++ bin/trv-test.ts
@@ -1,35 +1,19 @@
 import '@arcsine/nodesh';
 import * as cp from 'child_process';
-
-import type { TestEvent } from '@travetto/test';
-
-import { Git } from './package/git';
-import { Packages } from './package/packages';
-import { Util } from './package/util';
+import * as path from 'path';
 
 async function run(): Promise<boolean> {
-  console.error('Starting tests');
-  const { TestConsumerRegistry } = await import('@travetto/test/src/consumer/registry');
-  await TestConsumerRegistry.manualInit();
-
-  const emitter = await TestConsumerRegistry.getInstance(process.env.TRV_TEST_FORMAT || 'tap');
-  const { RunnableTestConsumer } = await import('@travetto/test/src/consumer/types/runnable');
-
-  const consumer = new RunnableTestConsumer(emitter);
-
-  return (process.env.TRV_ALL === '1' ? Packages.yieldPublicPackages() : Git.yieldChangedPackages())
-    .$parallel(async p => {
-      const args = ['test', '-f', 'exec', '-c', '3'];
-      const proc = cp.spawn('trv', args, { cwd: p._.folder, stdio: [0, 'pipe', 2, 'ipc'], shell: false });
-      const result = Util.enhanceProcess(proc, 'trv test');
-
-      proc.on('message', (ev: TestEvent) => consumer.onEvent(ev));
-      proc.on('error', e => {
-        console.error(e);
-        proc.kill('SIGTERM');
-      });
-      await result;
-    }, { concurrent: 4 }).then(x => consumer.summarizeAsBoolean());
+  const res = cp.spawnSync('trv', [
+    'test:all',
+    '-m', 'scan',
+    path.resolve('module'),
+    path.resolve('global-test')
+  ], {
+    cwd: path.resolve('module/test'),
+    shell: false,
+    stdio: [0, 1, 2]
+  });
+  return !res.status || res.status === 0;
 }
 
 run();
