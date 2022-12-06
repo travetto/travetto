893ce59b99cc482bb9ad3e700167a4bc731e836a
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-12-06 15:18:17 -0500

    chore(doc): boot cleaning

--- module/boot/README.ts
+++ module/boot/README.ts
@@ -19,6 +19,6 @@ ${d.Section('Shutdown Management')}
 Another key lifecycle is the process of shutting down. The framework provides centralized functionality for running operations on shutdown. Primarily used by the framework for cleanup operations, this provides a clean interface for registering shutdown handlers. The code overrides ${d.Method('process.exit')} to properly handle ${d.Input('SIGKILL')} and ${d.Input('SIGINT')}, with a default threshold of 3 seconds. In the advent of a ${d.Input('SIGTERM')} signal, the code exits immediately without any cleanup.
 
 As a registered shutdown handler, you can do.
-${d.Code('Registering a shutdown handler', 'src/shutdown.ts')}
+${d.Code('Registering a shutdown handler', 'doc/shutdown.ts')}
 
 `;
