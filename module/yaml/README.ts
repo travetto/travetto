ba7bfd77e9bc1de4fb29d044581d038e5d921257
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-12-06 22:34:10 -0500

    chore(doc): Updating docs for yaml

---
+++ module/yaml/README.ts
@@ -0,0 +1,16 @@
+import { d, lib } from '@travetto/doc';
+import { YamlUtil } from './src/util';
+
+export const text = () => d`
+${d.Header()}
+
+In the desire to provide a minimal footprint, the framework provides a minimal ${lib.YAML} parser/serializer to handle standard configuration structure.
+
+${d.Ref(YamlUtil.name, 'src/util.ts')} is the main access point for this module, and will expose two method, ${d.Method('parse')} and ${d.Method('serialize')}.
+
+${d.Code('Simple YAML Parsing', 'doc/parse.ts')}
+${d.Execute('Simple YAML Parsing', 'trv', ['main', 'support/main.parse.ts'], { cwd: './doc-exec' })}
+
+${d.Code('Simple YAML Serialization', 'doc/serialize.ts')}
+${d.Execute('Simple YAML Serialization', 'trv', ['main', 'support/main.serialize.ts'], { cwd: './doc-exec' })}
+`;
