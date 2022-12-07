0dac0979bc01f614249c03d68f53a8f7580655b3
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-12-06 21:32:01 -0500

    chore(doc): Working on scheme

--- module/schema/doc-exec/support/main.custom-type-output.ts
+++ module/schema/doc-exec/support/main.custom-type-output.ts
@@ -1,7 +1,10 @@
+import { RootRegistry } from '@travetto/registry';
 import { SchemaValidator } from '@travetto/schema';
-import { LocationAware } from './custom-type-usage';
+import { LocationAware } from '@travetto/schema/doc/custom-type-usage';
 
 export async function main(): Promise<void> {
+  await RootRegistry.init();
+
   const la = LocationAware.from({
     name: 'bob',
     // @ts-expect-error
--- module/schema/doc/src/person-output.ts
+++
@@ -1,5 +0,0 @@
-import { Test } from './person-binding';
-
-export async function main(): Promise<void> {
-  console.log!(Test());
-}
