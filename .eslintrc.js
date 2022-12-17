f82a0cfea0b4cbf4b4773d52337e4f22cc0b5ff0
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-12-16 20:41:52 -0500

    feat(lint): Bringing ability to have eslint plugins in monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
 const path = require('path');
 const out = process.env.TRV_OUTPUT = path.resolve('.trv_output');
-process.env.TRV_MANIFEST = '@travetto/mono-repo';
+process.env.TRV_MANIFEST = require('./package.json').name;
 module.exports = require(path.resolve(out, 'node_modules', '@travetto/eslint-plugin/support/eslintrc')).config;
