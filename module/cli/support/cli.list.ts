6352c96480a15aad514b41fb8b00fc786d7ffb1e
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2022-12-19 10:49:36 -0500

    feat(repo): Moving listing of modules into base cli

---
+++ module/cli/support/cli.list.ts
@@ -0,0 +1,47 @@
+import { CliCommand, CliModuleUtil, OptionConfig } from '@travetto/cli';
+import { RootIndex } from '@travetto/manifest';
+
+type Options = {
+  changed: OptionConfig<boolean>;
+  graph: OptionConfig<boolean>;
+};
+
+/**
+ * `npx trv list`
+ *
+ * Allows for listing of modules
+ */
+export class RepoListCommand extends CliCommand<Options> {
+
+  name = 'list';
+
+  isActive(): boolean {
+    return !!RootIndex.manifest.monoRepo;
+  }
+
+  getOptions(): Options {
+    return {
+      changed: this.boolOption({ desc: 'Only show changed modules', def: false }),
+      graph: this.boolOption({ desc: 'Show as a digraph', def: false })
+    };
+  }
+
+  async action(...args: unknown[]): Promise<void> {
+    const mods = await CliModuleUtil.findModules(this.cmd.changed ? 'changed' : 'all');
+    if (!this.cmd.graph) {
+      for (const mod of mods.map(x => x.workspaceRelative).sort()) {
+        console.log!(mod);
+      }
+    } else {
+      console.log!('digraph g {');
+      for (const el of mods) {
+        for (const dep of el.parents) {
+          if (dep !== RootIndex.mainPackage.name) {
+            console.log!(`  "${dep}" -> "${el.name}";`);
+          }
+        }
+      }
+      console.log!('}');
+    }
+  }
+}
