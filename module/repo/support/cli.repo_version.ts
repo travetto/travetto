01a3ad7e896fe596d8ea374ca9083b0c2a408f32
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-01-19 11:27:16 -0500

    feat(repo): Cleaning up versioning

--- module/repo/support/cli.repo_version.ts
+++ module/repo/support/cli.repo_version.ts
@@ -5,6 +5,7 @@ import { Npm, SemverLevel } from './bin/npm';
 
 type VersionOptions = {
   changed: OptionConfig<boolean>;
+  force: OptionConfig<boolean>;
 };
 
 /**
@@ -22,20 +23,23 @@ export class RepoVersionCommand extends CliCommand<VersionOptions> {
 
   getOptions(): VersionOptions {
     return {
-      changed: this.boolOption({ desc: 'Only version changed modules', def: true })
+      changed: this.boolOption({ desc: 'Only version changed modules', def: true }),
+      force: this.boolOption({ desc: 'Force operation, even in a dirty workspace', def: false })
     };
   }
 
   async action(level: SemverLevel, prefix?: string): Promise<void> {
-    if (await CliScmUtil.isWorkspaceDirty()) {
+    if (!this.cmd.force && await CliScmUtil.isWorkspaceDirty()) {
       console.error!('Cannot update versions with uncommitted changes');
       return this.exit(1);
     }
 
-    const modules = (await CliModuleUtil.findModules(this.cmd.changed ? 'changed' : 'all')).filter(x => !x.internal);
+    const allModules = await CliModuleUtil.findModules(this.cmd.changed ? 'changed' : 'all');
+
+    const modules = allModules.filter(x => !x.internal);
 
     // Do we have valid changes?
-    if (modules.length) {
+    if (!modules.length) {
       console.error!('No modules available for versioning');
       return this.exit(1);
     }
