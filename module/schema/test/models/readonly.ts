d4a156b9c824ea14941df961d0ec6203149da40d
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-01-23 16:07:06 -0500

    feat(schema): Moar tests!

---
+++ module/schema/test/models/readonly.ts
@@ -0,0 +1,10 @@
+import { Schema } from '../../src/decorator/schema';
+
+@Schema()
+export class ReadonlyUser {
+  readonly name: string;
+
+  readonly profile: {
+    readonly age: number;
+  }
+}
---
+++ module/schema/test/readonly.ts
@@ -0,0 +1,27 @@
+import assert from 'assert';
+
+import { Test, Suite, BeforeAll } from '@travetto/test';
+import { RootRegistry } from '@travetto/registry';
+import { ReadonlyUser } from './models/readonly';
+
+@Suite('Readonly')
+class ReadonlySuite {
+
+  @BeforeAll()
+  async init() {
+    await RootRegistry.init();
+  }
+
+  @Test('Validate bind')
+  validateBind() {
+    const person = ReadonlyUser.from({
+      name: 'Test',
+      profile: {
+        age: 20
+      }
+    });
+
+    assert(person.name === undefined);
+    assert(person.profile === undefined);
+  }
+}
