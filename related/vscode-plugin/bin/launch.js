117a400da4fb2d82922dfedd891c22c43b8f280d
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-02-17 16:56:53 -0500

    feat(vscode-plugin): Getting root detection to be a little more generic

--- related/vscode-plugin/bin/launch.js
+++ related/vscode-plugin/bin/launch.js
@@ -1,11 +1,16 @@
 import path from 'path';
 
 /**
- * @param {vscode.ExtensionContext} context
+ * @param {import('vscode').ExtensionContext} context
  * @returns {Promise<void>}
  */
 async function activate(context) {
-  process.env.TRV_MANIFEST = path.resolve(__dirname, '..');
+  let root = context.extension.extensionPath;
+  try {
+    // eslint-disable-next-line no-undef
+    root = __dirname.replace(/node_modules.*$/, '');
+  } catch { }
+  process.env.TRV_MANIFEST = path.resolve(root, 'node_modules', context.extension.packageJSON.name);
   (await import('@travetto/base/support/init.js')).init(false);
   return (await import('../src/extension.js')).activate(context);
 }
