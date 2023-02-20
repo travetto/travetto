82e99e16e971717488793b919ccbc5b7b40ef94d
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-02-20 09:00:49 -0500

    feat(scaffold): Missing dependency

--- module/scaffold/bin/trv-scaffold.js
+++ module/scaffold/bin/trv-scaffold.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 process.argv = [...process.argv.slice(0, 2), 'scaffold', ...process.argv.slice(2)];
-require('@travetto/compiler/bin/trv');
+import '@travetto/compiler/bin/trv';
