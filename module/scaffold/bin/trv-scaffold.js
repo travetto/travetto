158968b920a154be7d3f09e6deb82a59e4854623
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-02-20 09:15:50 -0500

    feat(scaffold): Fixing execution behavior

--- module/scaffold/bin/trv-scaffold.js
+++ module/scaffold/bin/trv-scaffold.js
@@ -2,7 +2,7 @@
 (async function () {
   process.argv = [...process.argv.slice(0, 2), 'scaffold', ...process.argv.slice(2)];
   if (__filename.includes('npx')) {
-    process.env.TRV_CLI_CWD = __filename.split('/node_modules')[0];
+    process.env.TRV_COMPILER_CWD = __filename.split('/node_modules')[0];
   }
   await import('@travetto/compiler/bin/trv.js');
 })();
