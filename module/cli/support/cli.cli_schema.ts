d5e0b42654904bf3019940410a5a438cdfdca2b6
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-03-17 07:43:54 -0400

    feat(cli): Adding in endpoint to produce schema for external tooling

---
+++ module/cli/support/cli.cli_schema.ts
@@ -0,0 +1,35 @@
+import { CliCommand } from '../src/decorators';
+import { CliCommandRegistry, CliCommandSchema, CliCommandSchemaUtil, CliValidationError } from '../__index__';
+
+/**
+ * Generates the schema for all CLI operations
+ */
+@CliCommand()
+export class CliSchemaCommand {
+
+  async #getSchema(name: string): Promise<CliCommandSchema> {
+    const inst = await CliCommandRegistry.getInstance(name);
+    return CliCommandSchemaUtil.getSchema(inst!);
+  }
+
+  async validate(name?: string): Promise<CliValidationError | undefined> {
+    if (name && !CliCommandRegistry.getCommandMapping().has(name)) {
+      return {
+        kind: 'invalid',
+        path: 'name',
+        message: `${name} is not a valid cli command`
+      };
+    }
+  }
+
+  async main(name?: string): Promise<void> {
+    if (name) {
+      console.log(JSON.stringify(await this.#getSchema(name), null, 2));
+    } else {
+      const names = [...CliCommandRegistry.getCommandMapping().keys()];
+      const pairs = names.map(x => this.#getSchema(x).then(schema => [x, schema]));
+      const data = Object.fromEntries(await Promise.all(pairs));
+      console.log(JSON.stringify(data, null, 2));
+    }
+  }
+}
