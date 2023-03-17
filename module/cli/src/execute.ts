0c5e4d29517189db74d985928996a3dba3e247b2
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-03-17 11:46:06 -0400

    chore(lint): Cleaning up lint issues

--- module/cli/src/execute.ts
+++ module/cli/src/execute.ts
@@ -45,7 +45,6 @@ export class ExecutionManager {
 
     const file = process.env.TRV_CLI_IPC!;
     const data = await cmd.jsonIpc!(...known);
-    const name = CliCommandRegistry.getName(cmd, true);
     const payload = JSON.stringify({ type: '@travetto/cli:run', data });
     await mkdir(path.dirname(file), { recursive: true });
     await appendFile(file, `${payload}\n`);
