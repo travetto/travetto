7e5ee6d14f8886e8092bfa36cb0535cac78513f6
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-03-19 22:10:07 -0400

    feat(openapi): Removing execsync call

--- module/openapi/support/bin/help.ts
+++ module/openapi/support/bin/help.ts
@@ -1,9 +1,9 @@
 import fs from 'fs/promises';
-import cp from 'child_process';
 
 import { path, RootIndex } from '@travetto/manifest';
 import { cliTpl } from '@travetto/cli';
 import { OpenApiClientPresets } from './presets';
+import { ExecUtil } from '@travetto/base';
 
 /**
  * Help utility for openapi client command
@@ -13,8 +13,9 @@ export class OpenApiClientHelp {
   static async getListOfFormats(dockerImage: string): Promise<string[]> {
     const formatCache = path.resolve(RootIndex.manifest.workspacePath, RootIndex.manifest.toolFolder, 'trv-openapi-formats.json');
     if (!await fs.stat(formatCache).catch(() => false)) {
-      const stdout = cp.execSync(`docker run --rm ${dockerImage} list`, { stdio: ['pipe', 'pipe'], encoding: 'utf8' }).trim();
-      const lines = stdout
+      const stdout = ExecUtil.spawn('docker', ['run', '--rm', dockerImage, 'list']);
+      const res = await stdout.result;
+      const lines = res.stdout
         .split('DOCUMENTATION')[0]
         .trim()
         .split(/\n/g)
