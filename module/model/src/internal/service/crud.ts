2d1c10a0e392afaa4976fd50f094af9ecf4872fe
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-04-02 13:32:04 -0400

    fix(model): Missing value

--- module/model/src/internal/service/crud.ts
+++ module/model/src/internal/service/crud.ts
@@ -21,7 +21,7 @@ export class ModelCrudUtil {
   /**
    * Build a uuid generator
    */
-  static uuidSource(len?: number): ModelIdSource {
+  static uuidSource(len: number = 32): ModelIdSource {
     const create = (): string => Util.uuid(len);
     const valid = (id: string): boolean => id.length === len && /^[0-9a-f]+$/i.test(id);
     return { create, valid };
