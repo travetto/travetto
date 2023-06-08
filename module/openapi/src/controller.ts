15f239447dff834dcc605c088058f74d0bed5c77
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-06-08 14:39:07 -0400

    feat(rest+openapi):  Fixing issues with minor bugs

--- module/openapi/src/controller.ts
+++ module/openapi/src/controller.ts
@@ -4,6 +4,13 @@ import { YamlUtil } from '@travetto/yaml';
 
 import { OpenApiService } from './service';
 
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Method': 'GET',
+  'Access-Control-Allow-Headers': '*',
+  'Access-Control-Allow-Credentials': 'true'
+};
+
 /**
  * Basic controller for surfacing the api spec
  */
@@ -15,12 +22,13 @@ export class OpenApiController {
   service: OpenApiService;
 
   @Get('openapi.json')
+  @SetHeaders(CORS_HEADERS)
   async getSpec(): Promise<object> {
     return this.service.getSpec(); // Force output to be simple
   }
 
   @Get('openapi.yaml')
-  @SetHeaders({ 'Content-Type': 'text/vnd.yaml' })
+  @SetHeaders({ 'Content-Type': 'text/vnd.yaml', ...CORS_HEADERS })
   async getYmlSpec(): Promise<string> {
     return YamlUtil.serialize(await this.service.getSpec()); // Force output to be simple
   }
