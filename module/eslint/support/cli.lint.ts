969c999acd2d7497f74968fa725a76132602a870
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-07-17 17:08:48 -0400

    feat(eslint): Allow for linting starting from a given commit hash

--- module/eslint/support/cli.lint.ts
+++ module/eslint/support/cli.lint.ts
@@ -24,7 +24,8 @@ export class LintCommand implements CliCommandShape {
   async main(): Promise<void> {
     let files: string[];
     if (this.since) {
-      files = await CliScmUtil.findChangedFilesSince(this.since);
+      files = (await CliScmUtil.findChangedFilesSince(this.since))
+        .filter(x => !x.endsWith('package.json') && !x.endsWith('package-lock.json'));
     } else {
       const mods = await CliModuleUtil.findModules(this.changed ? 'changed' : 'all');
       files = mods.filter(x => x.local).map(x => x.sourcePath);
