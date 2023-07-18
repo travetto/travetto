42e91a1047fbbea1f50ace17a2f302c3cc037c6c
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-07-17 20:39:46 -0400

    feat(s3): Moving away from localstack to use adobe/s3mock

--- module/model-s3/support/service.s3.ts
+++ module/model-s3/support/service.s3.ts
@@ -1,28 +1,11 @@
-import { mkdirSync } from 'fs';
-import os from 'os';
-
 import type { CommandService } from '@travetto/command';
 
-const temp = `${os.tmpdir()}/local-stack`;
-try {
-  mkdirSync(temp);
-} catch { }
-
-const version = '2.0.1';
+const version = '3.0.0';
 
 export const service: CommandService = {
   name: 's3',
   version,
   privileged: true,
-  env: {
-    TEST_AWS_ACCOUNT_ID: '000000000000',
-    LOCALSTACK_HOSTNAME: 'localhost',
-    DEFAULT_REGION: 'us-east-1',
-    HOST_TMP_FOLDER: temp
-  },
-  volumes: {
-    [temp]: '/var/lib/localstack'
-  },
-  ports: { 4566: 4566, 4571: 4571, 8080: 8080, 8081: 8081 },
-  image: `localstack/localstack:${version}`
+  ports: { 9091: 4566 },
+  image: `adobe/s3mock:${version}`
 };
