df9c2b51cbe4220cc9672a7de52033002413d79f
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-12-08 12:23:19 -0500

    feat(manifest): Exposing root manifest

--- module/eslint/support/cli.lint_register.ts
+++ module/eslint/support/cli.lint_register.ts
@@ -1,7 +1,7 @@
 import fs from 'fs/promises';
 
 import { CliCommandShape, CliCommand } from '@travetto/cli';
-import { path, RuntimeContext } from '@travetto/manifest';
+import { RuntimeContext } from '@travetto/manifest';
 
 import { buildEslintConfig } from './bin/eslint-config-file';
 
@@ -13,7 +13,7 @@ export class LintConfigureCommand implements CliCommandShape {
 
   async main(): Promise<void> {
     const content = buildEslintConfig();
-    const output = path.resolve(RuntimeContext.workspacePath, 'eslint.config.js');
+    const output = RuntimeContext.workspaceRelative('eslint.config.js');
     await fs.writeFile(output, content.replaceAll(RuntimeContext.workspacePath, '.').trim());
 
     console.log(`Wrote eslint config to ${output}`);
