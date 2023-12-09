fd73b595490e9a8f36b35c375a0c126319dcc1b3
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2023-12-09 09:07:18 -0500

    feat(manifest): Rename root index

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,10 @@
 process.env.TRV_MANIFEST = './.trv/output/node_modules/@travetto/mono-repo';
+
 const { buildConfig } = require('./.trv/output/node_modules/@travetto/eslint/support/bin/eslint-config.js');
 const { RuntimeIndex } = require('./.trv/output/node_modules/@travetto/manifest/__index__.js');
+
 const pluginFiles = RuntimeIndex.find({ folder: f => f === 'support', file: f => /support\/eslint[.]/.test(f) });
 const plugins = pluginFiles.map(x => require(x.outputFile));
 const config = buildConfig(plugins);
+
 module.exports = config;
---
+++ module/eslint/resources/eslint-config-file.mjs
@@ -0,0 +1,10 @@
+process.env.TRV_MANIFEST = '%MANIFEST_FILE%';
+
+const { buildConfig } = await import('@travetto/eslint/support/bin/eslint-config');
+const { RuntimeIndex } = await import('@travetto/manifest/__index__');
+
+const pluginFiles = RuntimeIndex.find({ folder: f => f === 'support', file: f => /support\/eslint[.]/.test(f) });
+const plugins = await Promise.all(pluginFiles.map(x => import(x.outputFile)))
+const config = buildConfig(plugins);
+
+export default config;
