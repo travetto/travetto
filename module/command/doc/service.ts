4d906beddb9d744e63201067c2571f9ccedc9cb7
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2024-01-10 08:18:41 -0500

    feat(exec): Init commit

--- module/command/doc/service.ts
+++ module/command/doc/service.ts
@@ -16,6 +16,6 @@ export class ImageCompressor {
     // Feed from process to file system
     state.stdout!.pipe(createWriteStream(out));
 
-    await state.complete;
+    await state.result;
   }
 }
