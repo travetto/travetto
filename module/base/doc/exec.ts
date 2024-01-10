54602e2532ffa7ef9f4923e9acec66dab7884436
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2024-01-10 07:44:15 -0500

    feat(exec): Init commit

--- module/base/doc/exec.ts
+++ module/base/doc/exec.ts
@@ -1,7 +1,7 @@
 import { Spawn } from '@travetto/base';
 
 export async function executeListing() {
-  const { result: success } = await Spawn.exec('ls');
-  const final = await success;
+  const { result } = await Spawn.exec('ls');
+  const final = await result;
   console.log('Listing', { lines: final.stdout.split('\n') });
 }
