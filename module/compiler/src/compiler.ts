a1b35e99178326be9b9d253d368b09e5c7d71e7e
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2024-01-25 09:10:53 -0500

    feat(compiler): Ensure we always quit, even  if we hit a snag

--- module/compiler/src/compiler.ts
+++ module/compiler/src/compiler.ts
@@ -80,6 +80,7 @@ export class Compiler {
     process.removeAllListeners('disconnect');
     process.removeAllListeners('message');
     this.#ctrl.abort();
+    setTimeout(() => process.exit(), 1000).unref(); // Allow upto 1s to shutdown gracefully
   }
 
   /**
