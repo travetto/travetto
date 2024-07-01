16ffee045611a09c7b00eaaeaec648dfe01aebfd
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2024-06-30 20:57:59 -0400

    feat(asset): Separating from rest

--- module/asset/src/response.ts
+++ module/asset/src/response.ts
@@ -2,6 +2,18 @@ import { Readable } from 'node:stream';
 
 import { StreamMeta, StreamRange } from '@travetto/model';
 import { path } from '@travetto/manifest';
+import { TypedObject } from '@travetto/base';
+
+const FIELD_TO_HEADER: Record<keyof StreamMeta, string> = {
+  contentType: 'content-type',
+  contentEncoding: 'content-encoding',
+  cacheControl: 'cache-control',
+  contentLanguage: 'content-language',
+  size: 'content-length',
+  hash: '',
+  filename: '',
+  title: ''
+};
 
 /**
  * A stream response
@@ -33,22 +45,15 @@ export class StreamResponse {
 
   headers(): Record<string, string> {
     const headers: Record<string, string> = {};
-    headers['content-type'] = this.meta.contentType;
+    for (const [f, v] of TypedObject.entries(FIELD_TO_HEADER)) {
+      if (this.meta[f] && v) {
+        headers[v] = `${this.meta[f]}`;
+      }
+    }
     if (this.meta.filename) {
       headers['content-disposition'] = `attachment;filename=${path.basename(this.meta.filename)}`;
     }
-    if (this.meta.contentEncoding) {
-      headers['content-encoding'] = this.meta.contentEncoding;
-    }
-    if (this.meta.contentLanguage) {
-      headers['content-language'] = this.meta.contentLanguage;
-    }
-    if (this.meta.cacheControl) {
-      headers['cache-control'] = this.meta.cacheControl;
-    }
-    if (!this.range) {
-      headers['content-length'] = `${this.meta.size}`;
-    } else {
+    if (this.range) {
       headers['accept-ranges'] = 'bytes';
       headers['content-range'] = `bytes ${this.range.start}-${this.range.end}/${this.meta.size}`;
       headers['content-length'] = `${this.range.end - this.range.start + 1}`;
