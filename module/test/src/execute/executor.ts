97e0b111bd2c93071d05cd7c09b3b15074d4cbe2
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2024-08-05 19:43:10 -0400

    feat(common): Release cleanup 3

--- module/test/src/execute/executor.ts
+++ module/test/src/execute/executor.ts
@@ -66,7 +66,7 @@ export class TestExecutor {
    * Fail an entire file, marking the whole file as failed
    */
   static failFile(consumer: TestConsumer, imp: string, err: Error): void {
-    const name = path.basename(imp).replace(/[.]js$/, '.ts');
+    const name = path.basename(imp);
     const classId = `${RuntimeIndex.getFromImport(imp)?.id}￮${name}`;
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
     const suite = { class: { name }, classId, duration: 0, lineStart: 1, lineEnd: 1, import: imp, } as SuiteConfig & SuiteResult;
