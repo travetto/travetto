26a7e3971d4b9b71313b40a723eb107de6c62b45
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2024-09-30 07:46:03 -0400

    feat(s3): Signed urls

--- module/model-s3/src/service.ts
+++ module/model-s3/src/service.ts
@@ -2,7 +2,7 @@ import { Readable } from 'node:stream';
 import { text as toText } from 'node:stream/consumers';
 import { Agent } from 'node:https';
 
-import { S3, CompletedPart, type CreateMultipartUploadRequest, GetObjectCommand, PutObjectCommand, PutObjectCommandInput } from '@aws-sdk/client-s3';
+import { S3, CompletedPart, type CreateMultipartUploadRequest, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 import type { MetadataBearer } from '@aws-sdk/types';
 import { NodeHttpHandler } from '@smithy/node-http-handler';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
