7f6302244f4e6a9a398652198a67701845a2c971
Timothy Soehnlin <timothy.soehnlin@gmail.com>
2024-11-26 17:30:58 -0500

    feat(compiler): Externalizing js code to ts when possible

--- module/compiler/bin/trvc.js
+++ module/compiler/bin/trvc.js
@@ -1,45 +1,39 @@
 #!/usr/bin/env node
+const help = `
+npx trvc [command]
 
-// @ts-check
-const { getEntry } = require('./common.js');
+Available Commands:
+ * start|watch                - Run the compiler in watch mode
+ * stop                       - Stop the compiler if running
+ * restart                    - Restart the compiler in watch mode
+ * build                      - Ensure the project is built and upto date
+ * clean                      - Clean out the output and compiler caches
+ * info                       - Retrieve the compiler information, if running
+ * event <log|progress|state> - Watch events in realtime as newline delimited JSON
+ * exec <file> [...args]      - Allow for compiling and executing an entrypoint file
+ * manifest --prod [output]   - Generate the project manifest
+`;
 
-const help = () => [
-  'npx trvc [command]',
-  '',
-  'Available Commands:',
-  ' * start|watch                - Run the compiler in watch mode',
-  ' * stop                       - Stop the compiler if running',
-  ' * restart                    - Restart the compiler in watch mode',
-  ' * build                      - Ensure the project is built and upto date',
-  ' * clean                      - Clean out the output and compiler caches',
-  ' * info                       - Retrieve the compiler information, if running',
-  ' * event <log|progress|state> - Watch events in realtime as newline delimited JSON',
-  ' * exec <file> [...args]      - Allow for compiling and executing an entrypoint file',
-  ' * manifest --prod [output]   - Generate the project manifest',
-].join('\n');
+const toJson = (/** @type {number} */ depth) => v => process.stdout.write(`${JSON.stringify(v, undefined, depth)}\n`) ||
+  new Promise(r => process.stdout.once('drain', r));
 
-getEntry().then(async (ops) => {
+require('./entry.common.js').load(ops => {
   const [op, ...all] = process.argv.slice(2);
   const args = all.filter(x => !x.startsWith('-'));
-  const flags = all.filter(x => x.startsWith('-'));
 
   switch (op) {
     case undefined:
-    case 'help': return console.log(`\n${help()}\n`);
-    case 'restart': return ops.restart();
-    case 'stop': return ops.stop();
-    case 'info': return ops.info().then(v => console.log(JSON.stringify(v, null, 2)));
-    case 'event': return ops.events(args[0], v => {
-      if (!process.stdout.write(`${JSON.stringify(v)}\n`)) {
-        return new Promise(r => process.stdout.once('drain', r));
-      }
-    });
+    case 'help': return console.log(help);
+    case 'info': return ops.info().then(toJson(2));
+    case 'event': return ops.events(args[0], toJson(0));
+    case 'manifest': return ops.manifest(args[0], all.some(x => x === '--prod'));
+    case 'exec': return ops.exec(args[0], all.slice(1));
+    case 'build': return ops.build();
     case 'clean': return ops.clean();
-    case 'manifest': return ops.manifest(args[0], flags.some(x => x === '--prod'));
-    case 'exec': return ops.getLoader().then(v => v(args[0], all.slice(1)));
     case 'start':
     case 'watch': return ops.watch();
-    case 'build': return ops.build();
-    default: console.error(`Unknown trvc operation: ${op}\n`); return console.error(help());
+    case 'stop': return ops.stop();
+    case 'restart': return ops.restart();
+    default: return console.error(`\nUnknown trvc operation: ${op}\n${help}`);
   }
 });
--- module/scaffold/bin/trv-scaffold.js
+++ module/scaffold/bin/trv-scaffold.js
@@ -1,22 +1,25 @@
 #!/usr/bin/env node
+// @ts-check
 
 async function getScaffoldCwd() {
-  let cwd = process.cwd();
   if (process.env.npm_lifecycle_script === 'trv-scaffold') { // Is npx  run
     const { delimiter } = await import('node:path');
-    const parts = process.env.PATH.split(delimiter);
+    const parts = process.env.PATH?.split(delimiter) ?? [];
     const loc = parts.find(p => p.includes('npx') && p.includes('.bin'));
     if (loc) {
-      cwd = loc.split('/node_modules')[0];
+      return loc.split('/node_modules')[0];
     }
   }
-  return cwd;
+  return process.cwd();
 }
 
+/**
+ * @param {string} cwd
+ */
 async function getVersion(cwd) {
   const fs = await import('node:fs/promises');
   const pkg = JSON.parse(await fs.readFile(`${cwd}/package.json`, 'utf8'));
-  const version = pkg.dependencies['@travetto/scaffold'].replace(/[^~]?(\d+[.]\d+)[.]\d+/, (_, v) => `${v}.0`).replace(/-([a-z]+)[.]\d+$/, (_, v) => `${v}.0`);
+  const version = `${pkg.dependencies['@travetto/scaffold']}`.replace(/\d+$/, '0');
   return version;
 }
 
@@ -26,7 +29,7 @@ async function getVersion(cwd) {
 
   const { spawn, execSync } = await import('node:child_process');
   // Ensure we install the compiler first
-  execSync(`npm i @travetto/compiler@^${version}`, { stdio: 'pipe', cwd: scaffoldCwd });
+  execSync(`npm i @travetto/compiler@${version}`, { stdio: 'pipe', cwd: scaffoldCwd });
 
   spawn('npx', [
     'trvc', 'exec',
@@ -39,7 +42,7 @@ async function getVersion(cwd) {
     cwd: scaffoldCwd,
     env: {
       ...process.env,
-      TRV_QUIET: 1,
+      TRV_QUIET: '1',
     }
   });
 })();
