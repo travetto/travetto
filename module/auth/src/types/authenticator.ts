cc8bb01cef312cc19b51185d51da8312cc403109
arciisine <timothy.soehnlin@gmail.com>
2025-01-23 08:30:05 -0500

    feat(auth): Cleanup v2

--- module/auth/src/types/authenticator.ts
+++ module/auth/src/types/authenticator.ts
@@ -1,11 +1,11 @@
 import { Principal } from './principal';
 
 /**
- * Supports validation payload of type I into an authenticated principal
+ * Supports validation payload of type T into an authenticated principal
  *
  * @concrete ../internal/types#AuthenticatorTarget
  */
-export interface Authenticator<I = unknown, R = unknown, P extends Principal = Principal> {
+export interface Authenticator<T = unknown, C = unknown, P extends Principal = Principal> {
   /**
    * Verify the payload, ensuring the payload is correctly identified.
    *
@@ -13,5 +13,5 @@ export interface Authenticator<I = unknown, R = unknown, P extends Principal = P
    * @returns undefined if authentication is valid, but incomplete (multi-step)
    * @throws AppError if authentication fails
    */
-  authenticate(input: I, request?: R): Promise<P | undefined> | P | undefined;
+  authenticate(payload: T, context?: C): Promise<P | undefined> | P | undefined;
 }
