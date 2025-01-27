4cf72409ac4d1038271164f8aaa9cb1f25486d2b
arciisine <timothy.soehnlin@gmail.com>
2025-01-27 12:24:10 -0500

    feat(rest): breakout

--- module/auth-rest/src/trv.d.ts
+++ module/auth-rest/src/trv.d.ts
@@ -1,7 +1,7 @@
 import { Principal } from '@travetto/auth';
 
 /**
- * Declare the principal on the request
+ * Declare the authenticated principal on the request
  */
 declare module '@travetto/rest' {
   interface Request {
