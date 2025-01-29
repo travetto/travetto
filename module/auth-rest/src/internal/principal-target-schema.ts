80ac53237e570c56fc04a1b75c3854881aa1f410
arciisine <timothy.soehnlin@gmail.com>
2025-01-29 12:40:49 -0500

    feat(rest): Cleaning up codecs

---
+++ module/auth-rest/src/internal/principal-target-schema.ts
@@ -0,0 +1,19 @@
+import { ClassList, SchemaRegistry } from '@travetto/schema';
+import { PrincipalTarget } from '@travetto/auth/src/internal/types';
+import { Class, TypedObject } from '@travetto/runtime';
+
+const FIELDS: Record<keyof PrincipalTarget, Class | ClassList> = {
+  id: String,
+  expiresAt: Date,
+  issuedAt: Date,
+  maxAge: Number,
+  issuer: String,
+  permissions: [String],
+  details: Object,
+};
+
+for (const [field, type] of TypedObject.entries(FIELDS)) {
+  SchemaRegistry.registerPendingFieldConfig(PrincipalTarget, field, type, { required: { active: field === 'id' } });
+}
+
+SchemaRegistry.register(PrincipalTarget, { class: PrincipalTarget });
