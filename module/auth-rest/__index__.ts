5fc052b7fa28b3db6ec7a957504e324150ea984d
arciisine <timothy.soehnlin@gmail.com>
2025-01-31 09:12:07 -0500

    feat(rest): Cleaning up codecs

--- module/auth-rest/__index__.ts
+++ module/auth-rest/__index__.ts
@@ -2,5 +2,7 @@ export * from './src/interceptors/read-write';
 export * from './src/interceptors/login';
 export * from './src/interceptors/logout';
 export * from './src/interceptors/verify';
+export * from './src/codec';
+export * from './src/config';
 export * from './src/decorator';
 export * from './src/types';
