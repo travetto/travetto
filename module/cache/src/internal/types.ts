aabf8423b8cecee5595df7d8e9144c8fd6be629b
arciisine <timothy.soehnlin@gmail.com>
2025-02-28 09:01:07 -0500

    feat(model): Taking it all inside

--- module/cache/src/internal/types.ts
+++
@@ -1,9 +0,0 @@
-import { CacheConfig, CoreCacheConfig } from '../types';
-
-export const CacheConfigSymbol = Symbol.for('@travetto/cache:cache');
-export const EvictConfigSymbol = Symbol.for('@travetto/cache:evict');
-
-export interface CacheAware {
-  [CacheConfigSymbol]?: Record<string, CacheConfig>;
-  [EvictConfigSymbol]?: Record<string, CoreCacheConfig>;
-}
