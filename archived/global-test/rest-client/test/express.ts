bc0cd1d2dbf13bc40e6ee7964487ca5f42fd8c0e
arciisine <timothy.soehnlin@gmail.com>
2025-02-28 17:25:14 -0500

    docs(angular): Remove references to rest-client as its been deprecated

---
+++ archived/global-test/rest-client/test/express.ts
@@ -0,0 +1,26 @@
+import { RestClientServerSuite } from '@travetto/rest-client/support/test/server';
+import { Suite } from '@travetto/test';
+import { ExpressRestServer } from '@travetto/rest-express';
+import { InjectableFactory } from '@travetto/di';
+import { RestApplication, RestServer } from '@travetto/rest';
+
+const EXPRESS = Symbol.for('express');
+
+class Config {
+  @InjectableFactory()
+  static getServer(): RestServer {
+    return new ExpressRestServer();
+  }
+
+  @InjectableFactory(EXPRESS)
+  static getApp(dep: ExpressRestServer): RestApplication {
+    return new class extends RestApplication {
+      server = dep;
+    }();
+  }
+}
+
+@Suite({ tags: ['expensive'] })
+export class ExpressRestClientTest extends RestClientServerSuite {
+  qualifier = EXPRESS;
+}
---
+++ archived/global-test/rest-client/test/koa.ts
@@ -0,0 +1,26 @@
+import { RestClientServerSuite } from '@travetto/rest-client/support/test/server';
+import { Suite } from '@travetto/test';
+import { KoaRestServer } from '@travetto/rest-koa';
+import { InjectableFactory } from '@travetto/di';
+import { RestApplication, RestServer } from '@travetto/rest';
+
+const KOA = Symbol.for('koa');
+
+class Config {
+  @InjectableFactory()
+  static getServer(): RestServer {
+    return new KoaRestServer();
+  }
+
+  @InjectableFactory(KOA)
+  static getApp(dep: KoaRestServer): RestApplication {
+    return new class extends RestApplication {
+      server = dep;
+    }();
+  }
+}
+
+@Suite({ tags: ['expensive'] })
+export class KoaRestClientTest extends RestClientServerSuite {
+  qualifier = KOA;
+}
