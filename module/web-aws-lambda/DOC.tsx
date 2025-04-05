ffe338e396d03442c8569cb910e74c902e3e273e
arciisine <timothy.soehnlin@gmail.com>
2025-04-04 22:05:56 -0400

    feat(web): Rename http to web

--- module/web-aws-lambda/DOC.tsx
+++ module/web-aws-lambda/DOC.tsx
@@ -1,9 +1,9 @@
 /** @jsxImportSource @travetto/doc */
-import { d, c } from '@travetto/doc';
+import { c } from '@travetto/doc';
 
 export const text = <>
   <c.StdHeader />
-  The module provides support basic support with AWS lambdas. When using one of the specific web modules (e.g. {d.mod('WebNode')}), you can install the appropriate lambda-related dependencies installed (e.g. {d.library('ServerlessExpress')}) to enable integration with AWS.  Nothing in the code needs to be modified to support the AWS integration, but there are some limitations of using AWS Lambdas as HTTP handlers.
+  The module provides support basic support with AWS lambdas.  Nothing in the code needs to be modified to support the AWS integration, but there are some limitations of using AWS Lambdas as HTTP handlers.
 
   <c.Section title='CLI - Packaging Lambdas'>
     <c.Execution title='Invoking a Package Build' cmd='trv' args={['pack:lambda', '-h']} />
