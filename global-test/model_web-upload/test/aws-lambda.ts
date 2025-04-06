a1932f05aaf03b61d46a2ac720eac94e5c9891a4
arciisine <timothy.soehnlin@gmail.com>
2025-04-06 10:49:35 -0400

    feat(web): Making router the focus

--- global-test/model_web-upload/test/aws-lambda.ts
+++ global-test/model_web-upload/test/aws-lambda.ts
@@ -1,12 +1,10 @@
 import { Suite } from '@travetto/test';
 
 import { LocalAwsLambdaWebRouter } from '@travetto/web-aws-lambda/support/test/router.ts';
-import { BasicWebRouter } from '@travetto/web/support/test/test-router.ts';
 
 import { ModelBlobWebUploadServerSuite } from './server.ts';
 
 @Suite()
 export class AwsLambdaWebUploadTest extends ModelBlobWebUploadServerSuite {
-  routerType = BasicWebRouter;
-  appType = LocalAwsLambdaWebRouter;
+  routerType = LocalAwsLambdaWebRouter;
 }
