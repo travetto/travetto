d006c6852d89ee1e7f67a0ab39c06a94d59b4590
arciisine <timothy.soehnlin@gmail.com>
2025-04-05 21:10:58 -0400

    feat/test): Moving to standard

--- module/auth-web-session/test/standard.ts
+++ module/auth-web-session/test/standard.ts
@@ -1,11 +1,25 @@
 import { Suite } from '@travetto/test';
 import { WebInternalSymbol } from '@travetto/web';
+import { InjectableFactory } from '@travetto/di';
+import { SessionModelSymbol } from '@travetto/auth-session';
+import { MemoryModelConfig, MemoryModelService } from '@travetto/model-memory';
+
 import { BasicWebServerSupport } from '@travetto/web/support/test/server-support.ts';
+
 
 import { AuthWebSessionServerSuite } from '../support/test/server.ts';
 
+
+class Config {
+  @InjectableFactory({ primary: true, qualifier: SessionModelSymbol })
+  static provider() {
+    return new MemoryModelService(new MemoryModelConfig());
+  }
+}
+
+
 @Suite()
-export class StandardAuthWebTest extends AuthWebSessionServerSuite {
+export class StandardAuthWebSessionTest extends AuthWebSessionServerSuite {
   type = BasicWebServerSupport;
   qualifier = WebInternalSymbol;
 }
