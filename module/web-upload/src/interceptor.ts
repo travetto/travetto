68cfdd7e8d58c0fd71fba55dfe05dc79bb752d16
arciisine <timothy.soehnlin@gmail.com>
2025-04-12 13:45:43 -0400

    feat(request): Streamlining

--- module/web-upload/src/interceptor.ts
+++ module/web-upload/src/interceptor.ts
@@ -1,18 +1,21 @@
 import { Inject, Injectable } from '@travetto/di';
 import {
   BodyParseInterceptor, WebInterceptor, WebInterceptorCategory, WebChainedContext,
-  EndpointConfig, WebResponse, WebInternalSymbol
+  EndpointConfig, WebResponse, WebInternalSymbol,
+  CompressInterceptor
 } from '@travetto/web';
 
 import { WebUploadConfig } from './config.ts';
 import { WebUploadUtil } from './util.ts';
 import { FileMap } from './types.ts';
+import { DecompressInterceptor } from '@travetto/web/src/interceptor/decompress.ts';
 
 @Injectable()
 export class WebUploadInterceptor implements WebInterceptor<WebUploadConfig> {
 
   category: WebInterceptorCategory = 'request';
-  dependsOn = [BodyParseInterceptor];
+  runsBefore = [BodyParseInterceptor];
+  dependsOn = [DecompressInterceptor];
 
   @Inject()
   config: WebUploadConfig;
