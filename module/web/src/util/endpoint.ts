423f522cffbedc2784d7e9a9d936de2e51a1c1de
arciisine <timothy.soehnlin@gmail.com>
2025-05-11 17:50:08 -0400

    Finishing tests

--- module/web/src/util/endpoint.ts
+++ module/web/src/util/endpoint.ts
@@ -144,14 +144,14 @@ export class EndpointUtil {
       const params = await this.extractParameters(endpoint, request);
       const body = await endpoint.endpoint.apply(endpoint.instance, params);
       const headers = endpoint.finalizedResponseHeaders;
-      const response = body instanceof WebResponse ? body : new WebResponse({ body, headers, context: { ...endpoint.responseContext } });
-      if (response === body) {
-        for (const [k, v] of headers) { response.headers.setIfAbsent(k, v); }
+      let response: WebResponse;
+      if (body instanceof WebResponse) {
+        for (const [k, v] of headers) { body.headers.setIfAbsent(k, v); }
         // Rewrite context
-        Object.assign(response.context, {
-          ...endpoint.responseContext,
-          ...response.context
-        });
+        Object.assign(body.context, { ...endpoint.responseContext, ...body.context });
+        response = body;
+      } else {
+        response = new WebResponse({ body, headers, context: { ...endpoint.responseContext } });
       }
       return endpoint.responseFinalizer?.(response) ?? response;
     } catch (err) {
