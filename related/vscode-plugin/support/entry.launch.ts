f03d9d2c4c58026c3e8f9e4142d5c355623827f4
arciisine <timothy.soehnlin@gmail.com>
2026-01-04 21:46:43 -0500

    Fixing vscode patterns

--- related/vscode-plugin/support/entry.launch.ts
+++ related/vscode-plugin/support/entry.launch.ts
@@ -1,15 +1,4 @@
-// @trv-no-transform
-import fs from 'node:fs';
-import path from 'node:path';
-import vscode from 'vscode';
-
-const extensionPath = vscode.extensions.getExtension('arcsine.travetto-plugin')?.extensionUri.path;
-
-const found = [
-  path.resolve(extensionPath ?? '.', 'dist', 'manifest.json'),
-  __dirname.replace(/support$/, 'manifest.json'),
-].find(file => fs.existsSync(file));
-
-process.env.TRV_MANIFEST = found;
+// @ts-expect-error - Fix vscode navigator problem
+try { delete globalThis.navigator; } catch { }
 
 export * from '../src/extension.js';
