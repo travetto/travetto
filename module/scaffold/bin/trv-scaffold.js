e0b88bc37f1019eb3684dd4d8e7e958115173d50
arciisine <timothy.soehnlin@gmail.com>
2026-01-11 22:49:17 -0500

    Feat scaffold updating

--- module/scaffold/bin/trv-scaffold.js
+++ module/scaffold/bin/trv-scaffold.js
@@ -1,29 +1,17 @@
 #!/usr/bin/env node
 // @ts-check
 import { delimiter } from 'node:path';
-import fs from 'node:fs';
-import { spawn, execSync } from 'node:child_process';
 
-let workingDirectory = process.cwd();
+await import('@travetto/compiler/bin/hook.js');
+
+const { invoke } = await import('@travetto/compiler/support/invoke.ts');
 
 if (process.env.npm_lifecycle_script?.includes('trv-scaffold')) { // Is npx  run
   const parts = process.env.PATH?.split(delimiter) ?? [];
   const item = parts.find(part => part.includes('npx') && part.includes('.bin'));
   if (item) {
-    workingDirectory = item.split('/node_modules')[0];
+    process.chdir(item.split('/node_modules')[0]);
   }
 }
 
-const pkg = JSON.parse(fs.readFileSync(`${workingDirectory}/package.json`, 'utf8'));
-const pkgVersion = `${pkg.dependencies?.['@travetto/scaffold']}`.replace(/\d+$/, '0');
-
-// Ensure we install the compiler first
-execSync(`npm i @travetto/compiler@${pkgVersion}`, { stdio: 'pipe', cwd: workingDirectory });
-spawn('npx', [
-  'trvc', 'exec', '@travetto/cli/support/entry.trv.js',
-  'scaffold', '-c', process.cwd(), ...process.argv.slice(2)
-], {
-  stdio: 'inherit',
-  cwd: workingDirectory,
-  env: { ...process.env, TRV_QUIET: '1', }
-});
+await invoke('exec', ['@travetto/cli/support/entry.trv.js', 'scaffold', '-c', process.cwd(), ...process.argv.slice(2)]);
