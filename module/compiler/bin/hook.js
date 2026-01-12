515ce0d12574f842fc38a3cec9a9e70f3188f39a
arciisine <timothy.soehnlin@gmail.com>
2026-01-11 20:18:07 -0500

    feat(compiler): Suppress type stripping warnings

--- module/compiler/bin/hook.js
+++ module/compiler/bin/hook.js
@@ -3,13 +3,19 @@ import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 
 process.setSourceMapsEnabled(true); // Ensure source map during compilation/development
-process.env.NODE_OPTIONS = `${process.env.NODE_OPTIONS ?? ''} --enable-source-maps --disable-warning=ExperimentalWarning`; // Ensure it passes to children
+process.env.NODE_OPTIONS = `${process.env.NODE_OPTIONS ?? ''} --enable-source-maps`; // Ensure it passes to children
+const ogEmitWarning = process.emitWarning;
 
 registerHooks({
   load: (url, context, nextLoad) => {
     if (/[.]tsx?$/.test(url)) {
-      const source = readFileSync(fileURLToPath(url), 'utf8');
-      return { format: 'module', source: stripTypeScriptTypes(source), shortCircuit: true };
+      try {
+        process.emitWarning = () => { }; // Suppress ts-node experimental warnings
+        const source = readFileSync(fileURLToPath(url), 'utf8');
+        return { format: 'module', source: stripTypeScriptTypes(source), shortCircuit: true };
+      } finally {
+        process.emitWarning = ogEmitWarning;
+      }
     } else {
       return nextLoad(url, context);
     }
