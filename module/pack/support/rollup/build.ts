7e512c6145d3f726bd7dd546d8c83053a620f0b1
arciisine <timothy.soehnlin@gmail.com>
2026-01-24 16:30:49 -0500

    Updating versions

--- module/pack/support/rollup/build.ts
+++ module/pack/support/rollup/build.ts
@@ -1,4 +1,3 @@
-import commonjsRequire from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import jsonImport from '@rollup/plugin-json';
@@ -21,7 +20,6 @@ export default function buildConfig(): RollupOptions {
       jsonImport(),
       travettoEntryPlugin(config),
       travettoImportPlugin(config),
-      commonjsRequire({ ignore: id => config.ignore.has(id) }),
       nodeResolve({ preferBuiltins: true }),
       travettoSourcemaps(config),
       ...(config.output.compact ? [terser(config.minify)] : [])
