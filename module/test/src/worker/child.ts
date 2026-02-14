8c3bbf467365a8287cbbbf1f90fac57df31d31be
arciisine <timothy.soehnlin@gmail.com>
2026-02-14 08:47:55 -0500

    Cleaning up error conveyance

--- module/test/src/worker/child.ts
+++ module/test/src/worker/child.ts
@@ -24,7 +24,7 @@ export class TestChildWorker extends IpcChannel<TestRun> {
         throw error;
       }
       // Mark as errored out
-      this.send(type, CodecUtil.toJSONObject({ error }));
+      this.send(type, CodecUtil.toJSONObject({ error }, { reviver: false }));
     }
   }
 
