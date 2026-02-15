63fe3a85cfe1b9e284a982e5afb3117670febd2c
arciisine <timothy.soehnlin@gmail.com>
2026-02-15 10:04:18 -0500

    What

--- module/web-rpc/support/client/rpc-node.ts
+++ module/web-rpc/support/client/rpc-node.ts
@@ -2,13 +2,9 @@ import { consumeError } from './rpc.ts';
 
 export async function toNodeError(payload: unknown): Promise<Error> {
   try {
-    let result = undefined;
     const { AppError } = await import('@travetto/runtime');
-    if (AppError.isJSON(result)) {
-      result = AppError.fromJSON(result) ?? result;
-    }
-    if (result) {
-      return result;
+    if (AppError.isJSON(payload)) {
+      return AppError.fromJSON(payload);
     }
   } catch { }
   return consumeError(payload);
