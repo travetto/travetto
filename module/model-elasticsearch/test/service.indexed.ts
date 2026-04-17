8d244374ddf249039e448be042d96f1886f99af4
arciisine <timothy.soehnlin@gmail.com>
2026-04-17 08:18:12 -0400

    Reorging

--- module/model-elasticsearch/test/service.indexed.ts
+++ module/model-elasticsearch/test/service.indexed.ts
@@ -2,17 +2,10 @@ import { Suite } from '@travetto/test';
 import { ElasticsearchModelConfig, ElasticsearchModelService } from '@travetto/model-elasticsearch';
 
 import { ModelIndexedSuite } from '@travetto/model-indexed/support/test/indexed.ts';
-import { ModelIndexedPolymorphismSuite } from '@travetto/model-indexed/support/test/polymorphism.ts';
 
 @Suite()
 class ElasticsearchIndexedSuite extends ModelIndexedSuite {
   serviceClass = ElasticsearchModelService;
   configClass = ElasticsearchModelConfig;
   supportsUniqueIndexes = false;
-}
-
-@Suite()
-class ElasticsearchIndexedPolymorphicSuite extends ModelIndexedPolymorphismSuite {
-  serviceClass = ElasticsearchModelService;
-  configClass = ElasticsearchModelConfig;
 }
