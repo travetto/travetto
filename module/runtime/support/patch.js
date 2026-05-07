0b033a675f14df1a2bf16c40ce68d6fca8f0d1dd
arciisine <timothy.soehnlin@gmail.com>
2026-05-07 08:12:01 -0400

    chore(node): Setting up version 26 support

--- module/runtime/support/patch.js
+++ module/runtime/support/patch.js
@@ -1,4 +1,3 @@
-import 'temporal-polyfill-lite/global';
 import fs from 'node:fs/promises';
 
 if (process.env.NODE_ENV !== 'production') {
@@ -23,25 +22,28 @@ Object.defineProperty(Error, 'isError', {
 // polyfills
 
 const [majorVersion, minorVersion] = process.version.replace(/^v/, '').split('.').map(text => parseInt(text, 10));
+if (majorVersion < 26) {
+  Map.prototype.getOrInsert ??= function (key, value) {
+    return (this.has(key) || this.set(key, value), this.get(key));
+  };
 
-Map.prototype.getOrInsert ??= function (key, value) {
-  return (this.has(key) || this.set(key, value), this.get(key));
-};
+  Map.prototype.getOrInsertComputed ??= function (key, compute) {
+    return (this.has(key) || this.set(key, compute()), this.get(key));
+  };
 
-Map.prototype.getOrInsertComputed ??= function (key, compute) {
-  return (this.has(key) || this.set(key, compute()), this.get(key));
-};
+  // Allow for the throwIfNoEntry if on a version of node that is less than 25.7
+  if (majorVersion < 25 || (majorVersion === 25 && minorVersion < 7)) {
+    void import('temporal-polyfill-lite/global');
 
-// Allow for the throwIfNoEntry if on a version of node that is less than 25.7
-if (majorVersion < 25 || (majorVersion === 25 && minorVersion < 7)) {
-  const og = fs.stat;
-  Object.defineProperty(fs, 'stat', {
-    value: (...args) => {
-      const out = og.call(fs, ...args);
-      if (typeof args[1] === 'object' && args[1].throwIfNoEntry === false) {
-        return out.catch(() => { });
+    const og = fs.stat;
+    Object.defineProperty(fs, 'stat', {
+      value: (...args) => {
+        const out = og.call(fs, ...args);
+        if (typeof args[1] === 'object' && args[1].throwIfNoEntry === false) {
+          return out.catch(() => { });
+        }
+        return out;
       }
-      return out;
-    }
-  });
+    });
+  }
 }
