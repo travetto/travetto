edbbb9eed1c1709a28619f5eefcfc448a52936b4
arciisine <timothy.soehnlin@gmail.com>
2026-06-01 22:35:53 -0400

    Step next

--- module/repo/DOC.tsx
+++ module/repo/DOC.tsx
@@ -20,17 +20,16 @@ export const text = <>
     <li>Running commands on all workspace modules</li>
   </ul>
 
-  <c.CliHelp commandClass={RepoVersionCommand} />
+  <c.CliHelp commandClass={RepoVersionCommand}>
+    The versioning operation will find all the changed modules (and the modules that depend on the changed), and will update the versions in accordance with the user preferences.  The versioning logic is backed by {PACKAGE_MANAGER_LIST}'s versioning functionality and so it is identical to using the tool manually. The determination of what has or hasn't changed is relative to the last versioning commit.
 
-  The versioning operation will find all the changed modules (and the modules that depend on the changed), and will update the versions in accordance with the user preferences.  The versioning logic is backed by {PACKAGE_MANAGER_LIST}'s versioning functionality and so it is identical to using the tool manually. The determination of what has or hasn't changed is relative to the last versioning commit.
+    Level is a standard semver level of: major, minor, patch or prerelease.  The prefix argument only applies to the prerelease and allows for determining the prerelease level.  For example:
 
-  Level is a standard semver level of: major, minor, patch or prerelease.  The prefix argument only applies to the prerelease and allows for determining the prerelease level.  For example:
+    <c.Terminal title='Cutting a release candidate' src={`${d.trv} repo:version prerelease rc`} />
 
-  <c.Terminal title='Cutting a release candidate' src={`${d.trv} repo:version prerelease rc`} />
+    After a release is cut, a new commit will be created that marks the next stable point in the commit history.
 
-  After a release is cut, a new commit will be created that marks the next stable point in the commit history.
-
-  <c.Code title='Versioning Commit' src={`
+    <c.Code title='Versioning Commit' src={`
 commit e9dc1a1de9625ab47398997fee6a95dd5a426900
 Author: Travetto Framework <travetto.framework@gmail.com>
 Date:   Thu Feb 23 17:51:37 2023 -0500
@@ -38,31 +37,32 @@ Date:   Thu Feb 23 17:51:37 2023 -0500
 
     Publish @travetto/asset,@travetto/asset-web,@travetto/auth,@travetto/auth-model,@travetto/auth-web,@travetto/auth-web-jwt,@travetto/auth-web-passport,@travetto/auth-web-session,...
 `} language='bash' />
+  </c.CliHelp>
 
 
-  <c.CliHelp commandClass={RepoPublishCommand} />
+  <c.CliHelp commandClass={RepoPublishCommand}>
+    The publish functionality is relatively naive, but consistent.  The code will look at all modules in the mono-repo and check the listed version against what is available in the npm registry.  If the local version is newer, it is a candidate for publishing.
 
-  The publish functionality is relatively naive, but consistent.  The code will look at all modules in the mono-repo and check the listed version against what is available in the npm registry.  If the local version is newer, it is a candidate for publishing.
+    By default the tool will execute a dry run only, and requires passing a flag to disable the dry run.
 
-  By default the tool will execute a dry run only, and requires passing a flag to disable the dry run.
+    <c.Terminal title='Publishing changes' src={`${d.trv} repo:publish --no-dry-run`} />
 
-  <c.Terminal title='Publishing changes' src={`${d.trv} repo:publish --no-dry-run`} />
+    If no modules are currently changed, then the command will indicate there is no work to do, and exit gracefully.
+  </c.CliHelp>
 
-  If no modules are currently changed, then the command will indicate there is no work to do, and exit gracefully.
+  <c.CliHelp commandClass={ListModuleCommand}>
+    The listing functionality provides the ability to get the workspace modules in the following formats:
+    <ul>
+      <li>{d.input('list')} - Standard text list, each module on its own line</li>
+      <li>{d.input('graph')} - Modules as a digraph, mapping interdependencies</li>
+      <li>{d.input('json')} - Graph of modules in JSON form, with additional data (useful for quickly building a dependency graph)</li>
+    </ul>
 
-  <c.CliHelp commandClass={ListModuleCommand} />
+    <c.Execution title='List execution of Monorepo' cmd='trv' args={['repo:list']} config={{ workingDirectory: Runtime.workspace.path }} />
+  </c.CliHelp>
 
-  The listing functionality provides the ability to get the workspace modules in the following formats:
-  <ul>
-    <li>{d.input('list')} - Standard text list, each module on its own line</li>
-    <li>{d.input('graph')} - Modules as a digraph, mapping interdependencies</li>
-    <li>{d.input('json')} - Graph of modules in JSON form, with additional data (useful for quickly building a dependency graph)</li>
-  </ul>
-
-  <c.Execution title='List execution of Monorepo' cmd='trv' args={['repo:list']} config={{ workingDirectory: Runtime.workspace.path }} />
-
-  <c.CliHelp commandClass={RepoExecCommand} />
-
-  The standard format includes prefixed output to help identify which module produced which output.
-  <c.Execution title='List execution of Monorepo' cmd='trv' args={['repo:exec', '-w', '1', 'pwd']} config={{ workingDirectory: Runtime.workspace.path }} />
+  <c.CliHelp commandClass={RepoExecCommand}>
+    The standard format includes prefixed output to help identify which module produced which output.
+    <c.Execution title='List execution of Monorepo' cmd='trv' args={['repo:exec', '-w', '1', 'pwd']} config={{ workingDirectory: Runtime.workspace.path }} />
+  </c.CliHelp>
 </>;
